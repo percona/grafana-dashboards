82cc88d8d0804b432b984e6af97c622d9cbdc8df
Lymanska Daria <daria.lymanska@gmail.com>
2017-12-11 04:40:34 +0200

    [PMM-1829] Code refactoring of qan panel module

--- pmm-app/src/pmm-qan-app-panel/module.ts
+++ pmm-app/src/pmm-qan-app-panel/module.ts
@@ -1,74 +1,70 @@
-/// <reference path="../../headers/common.d.ts" />
-
-import { MetricsPanelCtrl } from 'app/plugins/sdk';
+import {MetricsPanelCtrl} from 'app/plugins/sdk';
 import config from 'app/core/config';
 
 export class PanelCtrl extends MetricsPanelCtrl {
+    static template = `<iframe ng-src="{{trustSrc(url)}}" style="width: 100%; height: 400px; border: 0;" scrolling="no" />`;
 
-  static template = `
-    <iframe ng-src="{{ trustSrc(url) }}"
-      style="width: 100%; height: 400px; border: 0;" scrolling="no" />
-  `;
-  base_url = '/qan/profile';
+    constructor($scope, $injector, templateSrv, $sce) {
+        super($scope, $injector);
 
-	constructor($scope, $injector, templateSrv, $sce, $http) {
-		super($scope, $injector);
-    $scope.trustSrc = (src) => $sce.trustAsResourceUrl(src);
+        $scope.qanParams = {
+            'var-host': null,
+            'from': null,
+            'queryID': null,
+            'type': 'mySQL',
+            'to': null,
+            'tz': config.bootData.user.timezone,
+            'theme': config.bootData.user.lightTheme ? 'light' : 'dark'
+        };
+        $scope.trustSrc = (src) => $sce.trustAsResourceUrl(src);
 
-    $scope.qanParams = {
-			'var-host': null,
-			'from': null,
-			'to': null,
-			'tz': config.bootData.user.timezone,
-			'theme': config.bootData.user.lightTheme ? 'light' : 'dark'
-		};
+        this.setUrl($scope, templateSrv);
 
-    const setUrl = () => { // translates Grafana's variables into iframe's URL;
-      $scope.qanParams['var-host'] = templateSrv.variables[0].current.value;
-		};
-		$scope.$root.onAppEvent('template-variable-value-updated', setUrl);
-		setUrl();
+        $scope.$root.onAppEvent('template-variable-value-updated', this.setUrl.bind(this, $scope, templateSrv));
 
-		$scope.$watch('ctrl.range', (newValue, oldValue) => {
-			if (newValue) {
-				$scope.qanParams.from = newValue.from.valueOf();
-				$scope.qanParams.to = newValue.to.valueOf();
-			}
-		}, true);
-  }
+        $scope.$watch('ctrl.range', newValue => {
+            if (!newValue) return;
 
-  public encodeData(data: any): string {
-		return Object.keys(data).map((key) => {
-			if (data[key]) {
-				return [key, data[key]].map(encodeURIComponent).join('=');
-			}
-		}).join('&');
-	}
+            $scope.qanParams.from = newValue.from.valueOf();
+            $scope.qanParams.to = newValue.to.valueOf();
+        }, true);
+    }
 
-	link($scope, elem, attrs) {
-		const frame = elem.find('iframe');
-		const panel = elem.find('div.panel-container');
-		const bgcolor = $scope.qanParams.theme === 'light' ? '#ffffff' : '#141414';
-		panel.css({
-			'background-color': bgcolor,
-			'border': 'none'
-		});
+    link($scope, elem) {
+        const frame = elem.find('iframe');
+        const panel = elem.find('div.panel-container');
+        const bgcolor = $scope.qanParams.theme === 'light' ? '#ffffff' : '#141414';
+        panel.css({
+            'background-color': bgcolor,
+            'border': 'none'
+        });
 
-		const setHeight = () => {
-			const h = frame.contents().find('body').height() || 400;
-			frame.height(h + 100 + 'px');
-			panel.height(h + 150 + 'px');
-		};
+        // init url
+        this.resetUrl($scope);
+        // updated url
+        $scope.$watch('qanParams', this.resetUrl.bind(this, $scope), true);
 
-		frame[0].onload = (event) => frame.contents().bind(
-			'DOMSubtreeModified', () => setTimeout(setHeight, 100)
-		);
+        frame.on('load', () => frame.contents().bind('DOMSubtreeModified', event => {
+                const h = frame.contents().find('body').height() || 400;
+                frame.height(`${h + 100}px`);
+                panel.height(`${h + 150}px`);
+            }
+        ));
+    }
 
-    // initial url
-		$scope.url = this.base_url + '?' + this.encodeData($scope.qanParams);
-		// updated url
-		$scope.$watch('qanParams', (newValue, oldValue) => {
-			$scope.url = this.base_url + '?' + this.encodeData($scope.qanParams);
-		}, true);
-	}
+
+    private encodeData(data: Object): string {
+        return Object.keys(data)
+            .map(key => data[key] ? `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}` : null)
+            .join('&');
+    }
+
+    // translates Grafana's variables into iframe's URL;
+    private setUrl($scope, templateSrv) {
+        $scope.qanParams['var-host'] = templateSrv.variables[0].current.value;
+    }
+
+    private resetUrl($scope) {
+        $scope.url = `/qan/profile?${this.encodeData($scope.qanParams)}`;
+    }
 }
