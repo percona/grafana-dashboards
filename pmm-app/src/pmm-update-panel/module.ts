31d70d9296d9fb9589fb753fdd9861a5df9c6ae1
Lymanska Daria <daria.lymanska@gmail.com>
2018-01-17 16:49:07 +0200

    [PMM-1859] Fix jsdocs

--- pmm-app/src/pmm-update-panel/module.ts
+++ pmm-app/src/pmm-update-panel/module.ts
@@ -6,46 +6,37 @@ import AppEvents from 'app/core/app_events';
 import config from 'app/core/config';
 
 export class PanelCtrl extends MetricsPanelCtrl {
-    /*
-    *
-    * Urls to define panels templates
-    *
-    * */
+    /**
+     * Urls to define panels templates
+     */
     static TEMPLATES = {
         MAIN: 'pmm-update-panel/index.html',
         MODAL: 'public/plugins/pmm-update-panel/modal.html',
     };
 
-    /*
-    *
-    * Urls to define API endpoints
-    *
-    * */
+    /**
+     * Urls to define API endpoints
+     */
     static API = {
         CHECK_FOR_UPDATE: '/configurator/v1/check-update',
         UPDATE: '/configurator/v1/updates'
     };
 
-    /*
-    *
-    * Possible statuses of update version process (returned by backend)
-    *
-    * */
+    /**
+     * Possible statuses of update version process (returned by backend)
+     */
     static PROCESS_STATUSES = {
         IN_PROGRESS: 'running',
         DONE: 'succeeded',
         ERROR: 'error'
     };
 
-    /*
-    *
-    * Grafana param, define url of template that will be used for panel
-    *
-    * */
+    /**
+     * Grafana param, define url of template that will be used for panel
+     */
     static templateUrl: string = PanelCtrl.TEMPLATES.MAIN;
 
-    constructor($scope, $injector, $http) {
-        // console.log('$window', $window, $scope)
+    constructor(public $scope, public $injector, public $http) {
         super($scope, $injector);
 
         // Re-init all scope params
@@ -61,11 +52,9 @@ export class PanelCtrl extends MetricsPanelCtrl {
         $scope.copyToClipboard = this.copyToClipboard.bind(this, $scope);
     }
 
-    /*
-    *
-    * Send request for update version
-    *
-    * */
+    /**
+     * Send request for update version
+     */
     private update($scope, $http): void {
         $scope.isLoaderShown = true;
 
@@ -81,26 +70,23 @@ export class PanelCtrl extends MetricsPanelCtrl {
         }).then(response => {
             $scope.logLocation = response.headers('Location');
             $scope.getLog($scope, $http);
-        }, error => {
         });
     }
 
-    /*
-    *
-    * Send request to check if update possible and re-init params
-    *
-    * */
+    /**
+     * Send request to check if update possible and re-init params
+     */
     private checkForUpdate($scope, $http): void {
         $scope.isLoaderShown = true;
 
         $http({
             method: 'GET',
             url: PanelCtrl.API.CHECK_FOR_UPDATE,
-        }).then(response => {
+        }).then(() => {
             $scope.isLoaderShown = false;
             $scope.shouldBeUpdated = true;
             $scope.isChecked = true;
-        }, () => {
+        }).catch(() => {
             $scope.isLoaderShown = false;
             $scope.isChecked = true;
             // TODO: Error handler should be clarified
@@ -113,11 +99,9 @@ export class PanelCtrl extends MetricsPanelCtrl {
         });
     }
 
-    /*
-    *
-    * Send request for get info about update status
-    *
-    * */
+    /**
+     * Send request for get info about update status
+     */
     private getLog($scope, $http): void {
         if (!$scope.logLocation.length) return;
 
@@ -128,37 +112,33 @@ export class PanelCtrl extends MetricsPanelCtrl {
             $scope.output = response.data.detail;
 
             if (response.data.title === PanelCtrl.PROCESS_STATUSES.IN_PROGRESS) window.setTimeout($scope.getLog, 1000);
+
             if (response.data.title === PanelCtrl.PROCESS_STATUSES.DONE) {
                 $scope.shouldBeUpdated = false;
                 $scope.isUpdated = true;
                 $scope.isLoaderShown = false;
             }
-        }, error => {
+        }).catch(() => {
             $scope.isLoaderShown = false;
         });
     }
 
-    /*
-    *
-    * Send request to get info about new version
-    *
-    * */
+    /**
+     * Send request to get info about new version
+     */
     private showReleaseNotes($scope) {
+        // TODO: will be implemented after API release
     }
 
-    /*
-    *
-    * Send request to get info about new version
-    *
-    * */
+    /**
+     * Send request to get info about new version
+     */
     private copyToClipboard($scope) {
     }
 
-    /*
-    *
-    * Re-init all inner parameters that can be changed during update
-    *
-    * */
+    /**
+     * Re-init all inner parameters that can be changed during update
+     */
     private reset($scope): void {
         $scope.output = '';
         $scope.isLoaderShown = false;
