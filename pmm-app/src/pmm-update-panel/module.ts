3a7bacfd8ab5a89e53fd5b8ea891fa6e7a6f181b
Lymanska Daria <daria.lymanska@gmail.com>
2018-01-17 14:24:36 +0200

    [PMM-1859] Add styles for minor update widget items

--- pmm-app/src/pmm-update-panel/module.ts
+++ pmm-app/src/pmm-update-panel/module.ts
@@ -45,17 +45,20 @@ export class PanelCtrl extends MetricsPanelCtrl {
     static templateUrl: string = PanelCtrl.TEMPLATES.MAIN;
 
     constructor($scope, $injector, $http) {
+        // console.log('$window', $window, $scope)
         super($scope, $injector);
+
         // Re-init all scope params
         this.reset($scope);
 
-        let location = '';
+        $scope.logLocation = '';
         $scope.version = '1.0.0';
 
-        $scope.checkForUpdate = this.update.bind(this, $scope, $http);
+        $scope.checkForUpdate = this.checkForUpdate.bind(this, $scope, $http);
         $scope.update = this.update.bind(this, $scope, $http);
-        $scope.getLog = this.getLog.bind(this, $scope, $http, location);
+        $scope.getLog = this.getLog.bind(this, $scope, $http);
         $scope.showReleaseNotes = this.showReleaseNotes.bind(this, $scope);
+        $scope.copyToClipboard = this.copyToClipboard.bind(this, $scope);
     }
 
     /*
@@ -64,14 +67,20 @@ export class PanelCtrl extends MetricsPanelCtrl {
     *
     * */
     private update($scope, $http): void {
-        $scope.waitingForResponse = true;
+        $scope.isLoaderShown = true;
+
+        AppEvents.emit('show-modal', {
+            src: PanelCtrl.TEMPLATES.MODAL,
+            modalClass: 'confirm-modal',
+            scope: $scope
+        });
 
         $http({
             method: 'POST',
             url: PanelCtrl.API.UPDATE,
         }).then(response => {
-            location = response.headers('Location');
-            $scope.getLog();
+            $scope.logLocation = response.headers('Location');
+            $scope.getLog($scope, $http);
         }, error => {
         });
     }
@@ -82,17 +91,24 @@ export class PanelCtrl extends MetricsPanelCtrl {
     *
     * */
     private checkForUpdate($scope, $http): void {
-        AppEvents.emit('show-modal', {
-            src: PanelCtrl.TEMPLATES.MODAL,
-            modalClass: 'confirm-modal'
-        });
+        $scope.isLoaderShown = true;
 
         $http({
             method: 'GET',
             url: PanelCtrl.API.CHECK_FOR_UPDATE,
         }).then(response => {
+            $scope.isLoaderShown = false;
             $scope.shouldBeUpdated = true;
-        }, error => {
+            $scope.isChecked = true;
+        }, () => {
+            $scope.isLoaderShown = false;
+            $scope.isChecked = true;
+            // TODO: Error handler should be clarified
+            setTimeout(() => {
+                $scope.isChecked = false;
+                $scope.$apply();
+            }, 5000);
+
             $scope.shouldBeUpdated = false;
         });
     }
@@ -102,20 +118,23 @@ export class PanelCtrl extends MetricsPanelCtrl {
     * Send request for get info about update status
     *
     * */
-    private getLog($scope, $http, location): void {
-        if (!location.length) return;
+    private getLog($scope, $http): void {
+        if (!$scope.logLocation.length) return;
 
         $http({
             method: 'GET',
-            url: location,
+            url: $scope.logLocation,
         }).then(response => {
             $scope.output = response.data.detail;
 
             if (response.data.title === PanelCtrl.PROCESS_STATUSES.IN_PROGRESS) window.setTimeout($scope.getLog, 1000);
             if (response.data.title === PanelCtrl.PROCESS_STATUSES.DONE) {
                 $scope.shouldBeUpdated = false;
+                $scope.isUpdated = true;
+                $scope.isLoaderShown = false;
             }
         }, error => {
+            $scope.isLoaderShown = false;
         });
     }
 
@@ -129,12 +148,23 @@ export class PanelCtrl extends MetricsPanelCtrl {
 
     /*
     *
+    * Send request to get info about new version
+    *
+    * */
+    private copyToClipboard($scope) {
+    }
+
+    /*
+    *
     * Re-init all inner parameters that can be changed during update
     *
     * */
     private reset($scope): void {
         $scope.output = '';
-        $scope.showOutput = false;
+        $scope.isLoaderShown = false;
+        $scope.isChecked = false;
+        $scope.isUpdated = false;
+        $scope.isOutputShown = true;
         $scope.shouldBeUpdated = false;
     }
 }
