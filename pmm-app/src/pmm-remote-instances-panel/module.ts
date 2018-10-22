00b8733f8a4115231c8aa85546c4aee77f1e311c
Daria Lymanska <daria.lymanska@gmail.com>
2018-10-22 20:05:53 +0300

    PMM-3062 Add remote intances list panel

---
+++ pmm-app/src/pmm-remote-instances-panel/module.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../headers/common.d.ts" />
+
+import {MetricsPanelCtrl} from 'app/plugins/sdk';
+import config from 'app/core/config';
+
+export class PanelCtrl extends MetricsPanelCtrl {
+
+    static template = `
+		<iframe ng-src="{{ trustSrc(url) }}"
+			style="width: 100%; height: 400px; border: 0;" scrolling="no" />
+	`;
+
+    base_url = '/qan/remote-instances-list';
+
+    constructor($scope, $injector, templateSrv, $sce, $http) {
+        super($scope, $injector);
+        $scope.trustSrc = (src) => $sce.trustAsResourceUrl(src);
+        $scope.qanParams = {
+            'theme': config.bootData.user.lightTheme ? 'light' : 'dark'
+        };
+
+        $scope.url = this.base_url + '?theme=' + $scope.qanParams.theme;
+    }
+}
