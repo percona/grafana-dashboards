81dd5c5ca7bdd5f6753488c2690efdc42696c10f
Roman Misyurin <roman.misyurin@percona.com>
2019-11-25 11:54:24 +0300

    Moved code to old panel directory

--- pmm-app/build/webpack.dev.conf.js
+++
@@ -1,8 +0,0 @@
-const baseWebpackConfig = require('./webpack.base.conf');
-
-var conf = baseWebpackConfig;
-conf.devtool = "source-map";
-conf.watch = true;
-conf.mode = 'development';
-
-module.exports = conf;
--- pmm-app/build/webpack.prod.conf.js
+++
@@ -1,6 +0,0 @@
-const baseWebpackConfig = require('./webpack.base.conf');
-
-var conf = baseWebpackConfig;
-conf.mode = 'production';
-
-module.exports = baseWebpackConfig;
