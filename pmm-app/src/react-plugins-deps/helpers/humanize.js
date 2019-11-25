8bb2ac6fd9c4c06e423982eb5bfffdd8fa7a4463
Roman Misyurin <roman.misyurin@percona.com>
2019-11-25 15:55:22 +0300

    update build

--- pmm-app/src/react-plugins-deps/helpers/humanize.js
+++ pmm-app/src/react-plugins-deps/helpers/humanize.js
@@ -1,22 +1,20 @@
-import * as numeral from "numeral";
+import * as numeral from 'numeral';
 
 class Humanize {
   static formatPercent(input) {
     let res;
     if (input !== 0 && input < 0.0001) {
-      res = "<0.01";
+      res = '<0.01';
     } else if (input === 1) {
-      res = "100%";
+      res = '100%';
     } else {
-      res = numeral(input).format("0.00%");
+      res = numeral(input).format('0.00%');
     }
 
     return res;
   }
 
-  formatMetricValue(metric, type) {
-
-  }
+  formatMetricValue(metric, type) {}
 }
 
 export default Humanize;
