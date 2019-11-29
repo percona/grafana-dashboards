cad4964acf6aae6ece4e7a30e36cad884e48637f
Roman Misyurin <roman.misyurin@percona.com>
2019-11-29 11:50:22 +0300

    fix: remove unused components

--- pmm-app/src/pmm-add-instance-app-panel/module.tsx
+++
@@ -1,4 +0,0 @@
-import { PanelPlugin } from '@grafana/ui';
-import AddInstancePanel from './panel';
-
-export const plugin = new PanelPlugin(AddInstancePanel);
--- pmm-app/src/pmm-qan-app-panel-new/module.tsx
+++
@@ -1,5 +0,0 @@
-import { PanelPlugin } from '@grafana/ui';
-import SettingsPanel from './panel';
-import '../react-plugins-deps/styles.scss';
-import '../react-plugins-deps/style.less';
-export const plugin = new PanelPlugin(SettingsPanel);
