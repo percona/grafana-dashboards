6f04e95047871db86915bc3726ca9d5edb781007
Roman Misyurin <roman.misyurin@percona.com>
2019-12-02 23:39:23 +0300

    feat: add inventory service

--- pmm-app/src/pmm-inventory-new/panel.tsx
+++ pmm-app/src/pmm-inventory-new/panel.tsx
@@ -1,52 +1,171 @@
-import React from 'react';
-import { Collapse } from 'antd';
+import React, { useState } from 'react';
+import { Collapse, Table, Tabs } from 'antd';
 import '../react-plugins-deps/styles.scss';
 import '../react-plugins-deps/style.less';
 import './panel.scss';
-import AlertManager from './Parts/AlertManager';
-import UploadSSHKey from './Parts/UploadSSHKey';
-import Diagnostics from './Parts/Diagnostics';
-import SettingsPart from './Parts/Settings';
 
-const { Panel } = Collapse;
+const { TabPane } = Tabs;
 
-const customPanelStyle = {
-  background: 'rgb(51, 51, 51)',
-  marginBottom: 10,
-  border: 1,
-  borderColor: 'white',
-  color: 'white',
-  textColor: 'white',
+function callback(key) {
+  console.log(key);
+}
+
+const Services = props => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Service name',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Node ID',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Addresses',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Port',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Other Details',
+      dataIndex: 'age',
+      key: 'age',
+    },
+  ];
+
+  return <Table dataSource={dataSource} columns={columns} />;
 };
 
-const customCollapseStyle = {
-  background: 'transparent',
-  marginBottom: 10,
-  border: 1,
-  borderColor: 'black',
-  color: 'white',
-  textColor: 'white',
+const Agents = props => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Other Details',
+      dataIndex: 'age',
+      key: 'age',
+    },
+  ];
+
+  return <Table dataSource={dataSource} columns={columns} />;
+};
+
+const Nodes = props => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Node Name',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Addresses',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Other Details',
+      dataIndex: 'age',
+      key: 'age',
+    },
+  ];
+
+  return <Table dataSource={dataSource} columns={columns} />;
 };
 
 const InventoryPanel = () => {
   return (
     <div id={'antd'}>
-      <div className={'app-theme-dark pmm-settings-panel'}>
-        <Collapse bordered={false} defaultActiveKey={['1']} onChange={() => {}} style={customCollapseStyle}>
-          <Panel header="Settings" key="1" style={customPanelStyle}>
-            <SettingsPart />
-          </Panel>
-          <Panel header="SSH Key Details" key="3" style={customPanelStyle}>
-            <UploadSSHKey />
-          </Panel>
-          <Panel header="AlertManager integration" key="4" style={customPanelStyle}>
-            <AlertManager />
-          </Panel>
-          <Panel header="Diagnostics" key="5" style={customPanelStyle}>
-            <Diagnostics />
-          </Panel>
-        </Collapse>
-      </div>
+      <Tabs defaultActiveKey="1" onChange={callback}>
+        <TabPane tab="Services" key="1">
+          <Services />
+        </TabPane>
+        <TabPane tab="Agents" key="2">
+          <Agents />
+        </TabPane>
+        <TabPane tab="Nodes" key="3">
+          <Nodes />
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
