8f5a1ea90088ef77b8269aecf12b81c2056dcf00
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2019-12-12 17:16:59 +0300

    PMM-5061: inventory refactoring (#537)
    * added pmm react inventory
    
    * feat: add inventory service
    
    * feat: add inventory columns processing
    
    * feat: loading state added
    
    * fix: fixed remote rds object key
    
    * fix: typo removed
    
    * fix: cleanup
    
    * refactor: structure refactoring
    
    * fix: object name
    
    * fix: style leak fix
    
    * fix: structure and model fixes
    
    * fix: added tslint ignore to model
    
    * fix: fixed links style and base font size changed to 14
    
    * feat: added gradient
    
    * feat: improved other details overview and cleanup
    
    * fix: moved ink-bart to the top
    
    * fix: review issues

---
+++ pmm-app/src/pmm-inventory/DataService.ts
@@ -0,0 +1,65 @@
+/* tslint:disable */
+export class CustomLabelsModel {
+  key: string;
+  value: string;
+
+  constructor(customLabel) {
+    this.key = customLabel[0];
+    this.value = customLabel[1];
+  }
+}
+
+export class CommonModel {
+  custom_labels: CustomLabelsModel[];
+  agentType: string;
+  isDeleted: boolean;
+
+  constructor(params, type) {
+    const { custom_labels, ...rest } = params;
+    this.custom_labels =
+      custom_labels && Object.keys(custom_labels).length ? Object.entries(custom_labels).map(item => new CustomLabelsModel(item)) : [];
+    this.agentType = type;
+    this.isDeleted = false;
+    Object.keys(rest).forEach(param => {
+      this[param] = rest[param];
+    });
+  }
+}
+
+const inventoryTypes = {
+  external_exporter: 'External exporter',
+  mongodb_exporter: 'MongoDB exporter',
+  mysqld_exporter: 'MySQL exporter',
+  postgres_exporter: 'Postgres exporter',
+  proxysql_exporter: 'ProxySQL exporter',
+  node_exporter: 'Node exporter',
+  pmm_agent: 'PMM Agent',
+  qan_mongodb_profiler_agent: 'Qan MongoDB Profiler Agent',
+  qan_mysql_perfschema_agent: 'Qan MySQL Perfschema Agent',
+  qan_mysql_slowlog_agent: 'Qan MySQL Slowlog Agent',
+  qan_postgresql_pgstatements_agent: 'QAN PostgreSQL PgStatements Agent',
+  rds_exporter: 'RDS exporter',
+  container: 'Container',
+  generic: 'Generic',
+  remote: 'Remote',
+  remote_rds: 'Remote Amazon RDS',
+  amazon_rds_mysql: 'Amazon RDS MySQL',
+  mongodb: 'MongoDB',
+  mysql: 'MySQL',
+  postgresql: 'PostgreSQL',
+  proxysql: 'ProxySQL',
+};
+export class InventoryDataService {
+  constructor() {}
+
+  static generateStructure(item) {
+    const addAgentType = Object.keys(item).map(agentType => new Object({ agentType: agentType, params: item[agentType] }));
+    const createParams = addAgentType.map(agent =>
+      agent['params'].map(arrItem => {
+        const type = inventoryTypes[agent['agentType']] || '';
+        new CommonModel(arrItem, type);
+      })
+    );
+    return [].concat(...createParams);
+  }
+}
---
+++ pmm-app/src/pmm-inventory/panel.tsx
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Tabs } from 'antd';
+import '../react-plugins-deps/styles.scss';
+import '../react-plugins-deps/style.less';
+import './panel.scss';
+import InventoryService from './InventoryService';
+import { InventoryDataService } from './DataService';
+
+const { TabPane } = Tabs;
+
+const getCustomLabels = customLabels => customLabels.map(label => <span>{`${label.key}: ${label.value}`}</span>);
+
+const servicesColumns = [
+  {
+    title: 'ID',
+    dataIndex: 'service_id',
+    key: 'service_id',
+  },
+  {
+    title: 'Type',
+    dataIndex: 'agentType',
+    key: 'agentType',
+  },
+  {
+    title: 'Service name',
+    dataIndex: 'service_name',
+    key: 'service_name',
+  },
+  {
+    title: 'Node ID',
+    dataIndex: 'node_id',
+    key: 'node_id',
+  },
+  {
+    title: 'Addresses',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Port',
+    dataIndex: 'port',
+    key: 'port',
+  },
+  {
+    title: 'Other Details',
+    dataIndex: 'age',
+    key: 'age',
+    render: (text, element) => {
+      const mainColumns = ['service_id', 'agentType', 'service_name', 'custom_labels', 'node_id', 'address', 'port'];
+      const labels = Object.keys(element).filter(label => !mainColumns.includes(label));
+      return (
+        <div className={'other-details-wrapper'}>
+          {labels.map(label => (element[label] ? <span>{`${label}: ${element[label]}`}</span> : null))}
+          {element.custom_labels && getCustomLabels(element.custom_labels)}
+        </div>
+      );
+    },
+  },
+];
+const agentsColumns = [
+  {
+    title: 'ID',
+    dataIndex: 'agent_id',
+    key: 'agent_id',
+  },
+  {
+    title: 'Type',
+    dataIndex: 'agentType',
+    key: 'agentType',
+  },
+  {
+    title: 'Other Details',
+    dataIndex: 'age',
+    key: 'age',
+    render: (text, element) => {
+      const mainColumns = ['agent_id', 'agentType', 'isDeleted', 'service_ids', 'custom_labels'];
+      const labels = Object.keys(element).filter(label => !mainColumns.includes(label));
+      return (
+        <div className={'other-details-wrapper'}>
+          {labels.map(label => (element[label] ? <span>{`${label}: ${element[label]}`}</span> : null))}
+          {element.username ? <span>{`password: ******`}</span> : null}
+          {/* TODO: know more about isString*/}
+          {element.service_ids && element.service_ids.length ? (
+            <>
+              service_ids: <span>{element.service_ids.map(serviceId => serviceId)}</span>
+            </>
+          ) : null}
+          {element.custom_labels && getCustomLabels(element.custom_labels)}
+        </div>
+      );
+    },
+  },
+];
+const nodesColumns = [
+  {
+    title: 'ID',
+    dataIndex: 'node_id',
+    key: 'node_id',
+  },
+  {
+    title: 'Node Name',
+    dataIndex: 'node_name',
+    key: 'node_name',
+  },
+  {
+    title: 'Addresses',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Other Details',
+    dataIndex: 'age',
+    key: 'age',
+    render: (text, element) => {
+      const mainColumns = ['node_id', 'node_name', 'address', 'custom_labels', 'agentType', 'isDeleted'];
+      const labels = Object.keys(element).filter(label => !mainColumns.includes(label));
+      return (
+        <div className={'other-details-wrapper'}>
+          {labels.map(label => (element[label] ? <span>{`${label}: ${element[label]}`}</span> : null))}
+          {element.custom_labels && getCustomLabels(element.custom_labels)}
+        </div>
+      );
+    },
+  },
+];
+
+const Services = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      try {
+        const result = await InventoryService.getServices({});
+        setData(InventoryDataService.generateStructure(result));
+      } catch (e) {
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return <Table dataSource={data} columns={servicesColumns} pagination={false} bordered loading={loading} />;
+};
+
+const Agents = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      try {
+        const result = await InventoryService.getAgents({});
+        setData(InventoryDataService.generateStructure(result));
+      } catch (e) {
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return <Table dataSource={data} columns={agentsColumns} pagination={false} bordered loading={loading} />;
+};
+
+const Nodes = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      try {
+        const result = await InventoryService.getNodes({});
+        setData(InventoryDataService.generateStructure(result));
+      } catch (e) {
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return <Table dataSource={data} columns={nodesColumns} pagination={false} bordered loading={loading} />;
+};
+
+const InventoryPanel = () => {
+  return (
+    <div id={'antd'}>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Services" key="1">
+          <Services />
+        </TabPane>
+        <TabPane tab="Agents" key="2">
+          <Agents />
+        </TabPane>
+        <TabPane tab="Nodes" key="3">
+          <Nodes />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
+};
+
+export default InventoryPanel;
