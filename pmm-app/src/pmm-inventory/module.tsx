6d4f2beb656e2d067ed42792325dc69e03c82f69
Roman Misyurin <roman.misyurin@percona.com>
2019-12-13 10:46:00 +0300

    test: added render tests for pmm inventory

--- pmm-app/src/pmm-inventory/module.tsx
+++ pmm-app/src/pmm-inventory/module.tsx
@@ -1,4 +1,7 @@
 import { PanelPlugin } from '@grafana/ui';
+import '../react-plugins-deps/styles.scss';
+import '../react-plugins-deps/style.less';
+import './panel.scss';
 import InventoryPanel from './panel';
 
 export const plugin = new PanelPlugin(InventoryPanel);
