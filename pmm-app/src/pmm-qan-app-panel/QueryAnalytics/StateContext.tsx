d18bfa0f5a9b419853ded21e8217456c4def9a3c
Roman Misyurin <roman.misyurin@percona.com>
2019-12-16 18:55:25 +0300

    feat: improved layout

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/StateContext.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/StateContext.tsx
@@ -17,69 +17,100 @@ const initialState = {} as ContextInterface;
 export const StateContext = React.createContext(initialState);
 
 const DEFAULT_COLUMNS = ['load', 'num_queries', 'query_time'];
-export const UrlParametersProvider = ({ children }) => {
-  const query = new URLSearchParams(window.location.search);
-  const [hack, setHack] = useState({});
-  console.log(hack);
-  const reload = () => setHack({});
-  const addFilter = filter => changeFilter(filter, true);
-  const removeFilter = filter => changeFilter(filter, false);
-  const selectQuery = queryId => {
-    // setState({ filterBy: queryId });
-    updateUrl();
-    // reload();
-  };
 
-  const setFilters = query => {
+class ContextActions {
+  public selectedVariables: {};
+  public columns: any[];
+  public filterBy: any;
+  constructor(query) {
+    this.selectedVariables = this.setFilters(query);
+    this.columns = DEFAULT_COLUMNS;
+    this.filterBy = query.get('filter_by');
+  }
+
+  private setFilters(query) {
     const filtersListNames = ['host', 'city', 'cluster', 'az'];
 
     return filtersListNames.reduce((result, filterName) => {
       result[filterName] = query.getAll(`var-${filterName}`);
       return result;
     }, {});
-  };
+  }
 
-  const updateUrl = () => {
-    const filtersResult = Object.keys(state.selectedVariables)
+  private generateURL() {
+    // read parameters and create new url
+    const filtersResult = Object.keys(this.selectedVariables)
       .map(key => {
-        const variables = state.selectedVariables[key];
+        const variables = this.selectedVariables[key];
         return variables.map(variable => `var-${key}=${variable}`).join('&');
       })
       .filter(Boolean)
       .join('&');
     const filtersQuery = filtersResult || '';
-    const columnsQuery = state.columns ? `columns=${JSON.stringify(state.columns)}` : '';
-    const filterByQuery = state.filterBy ? `filter_by=${state.filterBy}` : '';
+    const columnsQuery = this.columns ? `columns=${JSON.stringify(this.columns)}` : '';
+    const filterByQuery = this.filterBy ? `filter_by=${this.filterBy}` : '';
     // TODO: replace crutch with right redirect
-    console.log(`${window.location.pathname}?${[filtersQuery, columnsQuery, filterByQuery].filter(Boolean).join('&')}`);
-    // window.location.href = `${window.location.pathname}?${[filtersQuery, columnsQuery, filterByQuery].filter(Boolean).join('&')}`;
-  };
+    return `${window.location.pathname}?${[filtersQuery, columnsQuery, filterByQuery].filter(Boolean).join('&')}`;
+  }
 
-  const changeFilter = (filter, isAdding) => {
+  private reload() {
+    const newUrl = this.generateURL();
+    window.location.href = newUrl;
+  }
+
+  private changeFilter(filter, isAdding) {
     const [group, value] = filter.split(':');
     function onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
     }
-    let filtersList = state.selectedVariables[group];
+    let filtersList = this.selectedVariables[group];
     if (isAdding) {
       filtersList.push(value);
     } else {
       filtersList = filtersList.filter(item => item !== value);
     }
-    state.selectedVariables[group] = filtersList.filter(onlyUnique);
-    setState({ ...state, selectedVariables: state.selectedVariables });
-    updateUrl();
-    reload();
-  };
+    this.selectedVariables[group] = filtersList.filter(onlyUnique);
+    this.reload();
+  }
 
+  addFilter(filter) {
+    this.changeFilter(filter, true);
+    this.reload();
+  }
+
+  removeFilter(filter) {
+    this.changeFilter(filter, false);
+    this.reload();
+  }
+
+  selectQuery(queryId) {
+    this.filterBy = queryId;
+    this.reload();
+  }
+
+  addColumn() {}
+  removeColumn() {}
+
+  changePagination() {}
+
+  addDetailsFilter() {}
+  removeDetailsFilter() {}
+
+  changeSort() {}
+}
+
+export const UrlParametersProvider = ({ children }) => {
+  const query = new URLSearchParams(window.location.search);
+
+  const contextData = new ContextActions(query);
   // Initial setup
   const [state, setState] = useState({
-    addFilter: addFilter,
-    removeFilter: removeFilter,
-    selectQuery: selectQuery,
-    columns: DEFAULT_COLUMNS,
-    filterBy: query.get('filter_by'),
-    selectedVariables: setFilters(query),
+    addFilter: contextData.addFilter.bind(contextData),
+    removeFilter: contextData.removeFilter.bind(contextData),
+    selectQuery: contextData.selectQuery.bind(contextData),
+    columns: contextData.columns,
+    filterBy: contextData.filterBy,
+    selectedVariables: contextData.selectedVariables,
   } as ContextInterface);
 
   return <StateContext.Provider value={state}>{children}</StateContext.Provider>;
