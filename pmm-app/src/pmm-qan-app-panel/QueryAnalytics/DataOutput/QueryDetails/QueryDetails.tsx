7e20b3d0a6c87eae27aaa585d8c4402edc4d1477
Roman Misyurin <roman.misyurin@percona.com>
2019-12-17 21:28:13 +0300

    fead: added polygon chart

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryDetails/QueryDetails.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryDetails/QueryDetails.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import Labels from './Labels/Labels';
-import { Tabs } from 'antd';
+import { Divider, Tabs } from 'antd';
 import './QueryDetails.scss';
 import Fingerprint from './Fingerprint';
 import Explain from './Explain/Explain.container';
 import Example from './Example/Example';
 import Metrics from './Metrics/Metrics';
 import TableCreate from './Table/Table';
+import { StateContext } from '../../StateContext';
 
 const { TabPane } = Tabs;
 
 const QueryDetails = props => {
+  const context = useContext(StateContext);
+
   const MetricsProps = {
-    filterBy: '1087289585964552152',
+    filterBy: context.filterBy,
     groupBy: 'queryid',
     labels: [],
-    periodStartFrom: '2019-12-16T12:09:16+00:00',
-    periodStartTo: '2019-12-17T00:09:16+00:00',
+    periodStartFrom: '2019-12-17T05:02:10+00:00',
+    periodStartTo: '2019-12-17T17:02:10+00:00',
     tables: [],
   };
+
   return (
     <div className={'query-analytics-details-grid'} id={'query-analytics-details'}>
       <Fingerprint
         query={'COLLSTATS sbtest3 clusterTime,collStats,hash,id,keyId,lsid,mode,scale,signature'}
         controlSum={'4675a0d6d2e18bdce08b7c7aa83f88ff'}
       />
-
       <div className="details-tabs">
+        <Divider />
         <Tabs defaultActiveKey="1" onChange={() => {}} tabPosition={'left'}>
           <TabPane tab="Details" key="1">
             <div className="details-table">
