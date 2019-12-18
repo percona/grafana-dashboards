4ac42ce668b97bee29a0f7add0180f776c258a93
Roman Misyurin <roman.misyurin@percona.com>
2019-12-18 15:16:15 +0300

    fix: fixed port validator

--- pmm-app/src/react-plugins-deps/components/validators/validators.ts
+++ pmm-app/src/react-plugins-deps/components/validators/validators.ts
@@ -1,6 +1,6 @@
 class Validators {
   static validatePort(value) {
-    if (!/^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/.test(value)) {
+    if (!/^([1-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/.test(value)) {
       return 'Port should be a number and between the range of 0 and 65535';
     }
     return '';
