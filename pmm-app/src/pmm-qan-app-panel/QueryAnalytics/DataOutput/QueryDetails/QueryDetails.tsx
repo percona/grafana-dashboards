5ba3ce51038dcf4033889296cd8a6bb463904f3a
Roman Misyurin <roman.misyurin@percona.com>
2020-02-03 20:37:22 +0300

    refactor: code simplifications

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryDetails/QueryDetails.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryDetails/QueryDetails.tsx
@@ -1,36 +1,33 @@
-import React, {useContext} from 'react';
-import {Divider, Tabs} from 'antd';
+import React, { useContext } from 'react';
+import { Divider, Tabs } from 'antd';
 import './QueryDetails.scss';
 import Fingerprint from './Fingerprint';
 import Explain from './Explain/Explain.container';
 import Example from './Example/Example';
 import Metrics from './Metrics/Metrics';
 import TableCreate from './Table/Table';
-import {StateContext} from '../../StateContext';
+import { StateContext } from '../../StateContext';
 
 const { TabPane } = Tabs;
 
-const QueryDetails = props => {
-  const context = useContext(StateContext);
+const HARDCODED_FINGERPRINT = 'COLLSTATS sbtest3 clusterTime,collStats,hash,id,keyId,lsid,mode,scale,signature';
+const HARDCODED_CONTROL_SUM = '4675a0d6d2e18bdce08b7c7aa83f88ff';
+const QueryDetails = () => {
+  const {
+    state: { queryId, groupBy, from, to },
+  } = useContext(StateContext);
 
-  if (!context.state.queryId) {
-    return null
+  if (!queryId) {
+    return null;
   }
 
   const MetricsProps = {
-    filterBy: context.state.queryId,
-    groupBy: context.state.groupBy,
     labels: [],
-    periodStartFrom: context.state.from,
-    periodStartTo: context.state.to,
     tables: [],
   };
   return (
     <div className={'query-analytics-details-grid'} id={'query-analytics-details'}>
-      <Fingerprint
-        query={'COLLSTATS sbtest3 clusterTime,collStats,hash,id,keyId,lsid,mode,scale,signature'}
-        controlSum={'4675a0d6d2e18bdce08b7c7aa83f88ff'}
-      />
+      <Fingerprint query={HARDCODED_FINGERPRINT} controlSum={HARDCODED_CONTROL_SUM} />
       <div className="details-tabs">
         <Divider />
         <Tabs defaultActiveKey="1" onChange={() => {}} tabPosition={'left'}>
@@ -38,10 +35,10 @@ const QueryDetails = props => {
             <div className="details-table">
               <div className="details-table-content">
                 <Metrics
-                  queryId={context.state.queryId}
-                  groupBy={MetricsProps.groupBy}
-                  periodStartFrom={MetricsProps.periodStartFrom}
-                  periodStartTo={MetricsProps.periodStartTo}
+                  queryId={queryId}
+                  groupBy={groupBy}
+                  periodStartFrom={from}
+                  periodStartTo={to}
                   labels={MetricsProps.labels}
                   tables={MetricsProps.tables}
                 />
@@ -50,20 +47,20 @@ const QueryDetails = props => {
           </TabPane>
           <TabPane tab="Examples" key="2">
             <Example
-              queryId={context.state.queryId}
-              groupBy={MetricsProps.groupBy}
-              periodStartFrom={MetricsProps.periodStartFrom}
-              periodStartTo={MetricsProps.periodStartTo}
+              queryId={queryId}
+              groupBy={groupBy}
+              periodStartFrom={from}
+              periodStartTo={to}
               labels={MetricsProps.labels}
               tables={MetricsProps.tables}
             />
           </TabPane>
           <TabPane tab="Explain" key="3">
             <Explain
-              queryId={context.state.queryId}
-              groupBy={MetricsProps.groupBy}
-              periodStartFrom={MetricsProps.periodStartFrom}
-              periodStartTo={MetricsProps.periodStartTo}
+              queryId={queryId}
+              groupBy={groupBy}
+              periodStartFrom={from}
+              periodStartTo={to}
               labels={MetricsProps.labels}
               tables={MetricsProps.tables}
             />
