275bb4a04e5bf79dedd4d293178c0badc23dbeeb
Roman Misyurin <roman.misyurin@percona.com>
2020-02-05 17:33:19 +0300

    refactor: replaced anchors with buttons

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/DataOutput.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/DataOutput.tsx
@@ -1,10 +1,10 @@
 import OverviewTable from './QueryAnalyticsOverview/OverviewTable/OverviewTable';
 import QueryDetails from './QueryDetails/QueryDetails';
-import React, { useContext, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import Split from 'react-split';
 import { StateContext } from '../StateContext';
 import './DataOutput.scss';
-import { Pagination } from 'antd';
+import { Button, Pagination } from 'antd';
 import AddColumn from './AddColumn';
 
 const PAGE_SIZE_OPTIONS = ['10', '50', '100'];
@@ -14,25 +14,41 @@ const DataOutput = () => {
     state: { pageNumber, pageSize, firstSeen },
   } = useContext(StateContext);
   const [total, setTotal] = useState(30);
+
+  const changeFirstSeen = useCallback(() => {
+    dispatch({
+      type: 'CHANGE_FIRST_SEEN',
+      payload: {
+        firstSeen: !firstSeen,
+      },
+    });
+  }, [firstSeen]);
+
+  const changePageNumber = useCallback(pageNumber => {
+    dispatch({
+      type: 'CHANGE_PAGE',
+      payload: {
+        pageNumber,
+      },
+    });
+  }, []);
+
+  const changePageSize = useCallback((current, pageSize) => {
+    dispatch({
+      type: 'CHANGE_PAGE_SIZE',
+      payload: {
+        pageSize,
+      },
+    });
+  }, []);
+
   return (
     <div>
       <div className={'filters-header'} style={{ padding: '5px 0px', height: '50px' }}>
         <h5 style={{ margin: '3px', marginRight: '15px' }}>Queries overview</h5>
-        <a
-          href="#"
-          className={'filter-switchers'}
-          style={{ marginLeft: '20px' }}
-          onClick={() => {
-            dispatch({
-              type: 'CHANGE_FIRST_SEEN',
-              payload: {
-                firstSeen: !firstSeen,
-              },
-            });
-          }}
-        >
+        <Button type="link" style={{ padding: 0, height: 'auto', marginLeft: '20px' }} onClick={changeFirstSeen}>
           {firstSeen ? `Show all Queries` : `Show first Seen`}
-        </a>
+        </Button>
         <div style={{ marginLeft: 'auto' }}>
           <Pagination
             showSizeChanger={true}
@@ -42,22 +58,8 @@ const DataOutput = () => {
             current={pageNumber}
             pageSize={pageSize}
             total={total}
-            onShowSizeChange={(current, pageSize) => {
-              dispatch({
-                type: 'CHANGE_PAGE_SIZE',
-                payload: {
-                  pageSize,
-                },
-              });
-            }}
-            onChange={pageNumber => {
-              dispatch({
-                type: 'CHANGE_PAGE',
-                payload: {
-                  pageNumber,
-                },
-              });
-            }}
+            onShowSizeChange={changePageSize}
+            onChange={changePageNumber}
           />
         </div>
         <div style={{ marginLeft: '10px' }}>
