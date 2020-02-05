2bf6c86aa7542c21c230487a7524d114cab5d41a
Roman Misyurin <roman.misyurin@percona.com>
2020-02-05 17:19:01 +0300

    refator: switchers simplification

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/DataOutput.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/DataOutput.tsx
@@ -7,78 +7,54 @@ import './DataOutput.scss';
 import { Pagination } from 'antd';
 import AddColumn from './AddColumn';
 
-const FirstSeen = () => {
-  const context = useContext(StateContext);
-
-  return (
-    <>
-      {context.state.firstSeen ? (
-        <a
-          href="#"
-          className={'filter-switchers'}
-          style={{ marginLeft: '20px' }}
-          onClick={() => {
-            context.dispatch({
-              type: 'CHANGE_FIRST_SEEN',
-              payload: {
-                firstSeen: false,
-              },
-            });
-          }}
-        >
-          Show all Queries
-        </a>
-      ) : (
-        <a
-          href="#"
-          className={'filter-switchers'}
-          style={{ marginLeft: '20px' }}
-          onClick={() => {
-            context.dispatch({
-              type: 'CHANGE_FIRST_SEEN',
-              payload: {
-                firstSeen: true,
-              },
-            });
-          }}
-        >
-          Show first Seen
-        </a>
-      )}
-    </>
-  );
-};
-
+const PAGE_SIZE_OPTIONS = ['10', '50', '100'];
 const DataOutput = () => {
-  const context = useContext(StateContext);
+  const {
+    dispatch,
+    state: { pageNumber, pageSize, firstSeen },
+  } = useContext(StateContext);
   const [total, setTotal] = useState(30);
   return (
     <div>
       <div className={'filters-header'} style={{ padding: '5px 0px', height: '50px' }}>
         <h5 style={{ margin: '3px', marginRight: '15px' }}>Queries overview</h5>
-        <FirstSeen />
+        <a
+          href="#"
+          className={'filter-switchers'}
+          style={{ marginLeft: '20px' }}
+          onClick={() => {
+            dispatch({
+              type: 'CHANGE_FIRST_SEEN',
+              payload: {
+                firstSeen: !firstSeen,
+              },
+            });
+          }}
+        >
+          {firstSeen ? `Show all Queries` : `Show first Seen`}
+        </a>
         <div style={{ marginLeft: 'auto' }}>
           <Pagination
             showSizeChanger={true}
-            pageSizeOptions={['10', '50', '100']}
+            pageSizeOptions={PAGE_SIZE_OPTIONS}
             defaultCurrent={1}
             defaultPageSize={10}
-            current={context.state.pageNumber}
-            pageSize={context.state.pageSize}
+            current={pageNumber}
+            pageSize={pageSize}
             total={total}
             onShowSizeChange={(current, pageSize) => {
-              context.dispatch({
+              dispatch({
                 type: 'CHANGE_PAGE_SIZE',
                 payload: {
-                  pageSize: pageSize,
+                  pageSize,
                 },
               });
             }}
             onChange={pageNumber => {
-              context.dispatch({
+              dispatch({
                 type: 'CHANGE_PAGE',
                 payload: {
-                  pageNumber: pageNumber,
+                  pageNumber,
                 },
               });
             }}
