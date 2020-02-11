3b456e49eb38e4a1b509e4c96a922aee88443679
Roman Misyurin <roman.misyurin@percona.com>
2020-02-12 01:03:27 +0300

    fix: metric units display fix

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryAnalyticsOverview/OverviewTable/Column.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryAnalyticsOverview/OverviewTable/Column.tsx
@@ -8,6 +8,7 @@ import './OverviewTable.scss';
 import { METRIC_CATALOGUE } from '../../metric-catalogue';
 import Tooltip from 'antd/es/tooltip';
 import { Card, List } from 'antd';
+import './Column.scss';
 
 export const getOverviewColumn = (metricName, columnIndex, totalValues, orderBy) => {
   const metric = METRIC_CATALOGUE[metricName];
@@ -32,18 +33,18 @@ export const getOverviewColumn = (metricName, columnIndex, totalValues, orderBy)
         { header: 'From total', value: ((stats.sum_per_sec / totalValues.metrics[metricName].stats.sum_per_sec) * 100).toFixed(2) + '%' },
       ];
 
-      const { min = 0, max = 0, avg = 0, p99 = 0 } = stats;
-      const minStr = `⌜ Min: ${Humanize.transform(min, this.measurement)}`;
-      const maxStr = `⌟ Max: ${Humanize.transform(max, this.measurement)}`;
-      const avgStr = `◦ Avg: ${Humanize.transform(avg, this.measurement)}`;
-      const p99Str = `${p99 ? `• 99%: ${Humanize.transform(p99, this.measurement)}` : ''}`;
+      // const { min = 0, max = 0, avg = 0, p99 = 0 } = stats;
+      const minStr = `${Humanize.transform(stats.min)}`;
+      const maxStr = `${Humanize.transform(stats.max)}`;
+      const avgStr = `${Humanize.transform(stats.avg)}`;
+      const p99Str = `${stats.p99 ? `${Humanize.transform(stats.p99)}` : ''}`;
 
       const latencyTooltipData = [
-        { header: '⌜ Min: ', value: minStr },
-        { header: '⌟ Max: ', value: maxStr },
-        { header: '◦ Avg: ', value: avgStr },
-        { header: '• 99%: ', value: p99Str },
-      ];
+        { header: '⌜ Min', value: minStr },
+        { header: '⌟ Max', value: maxStr },
+        { header: '◦ Avg', value: avgStr },
+        { header: '• 99%', value: p99Str },
+      ].filter(element => element.value);
 
       // @ts-ignore
       const polygonChartProps = { width: 150, data: item.sparkline };
@@ -53,6 +54,7 @@ export const getOverviewColumn = (metricName, columnIndex, totalValues, orderBy)
           <Tooltip
             getPopupContainer={() => document.querySelector('#antd') || document.body}
             placement="topLeft"
+            overlayClassName={'overview-column-toolkit'}
             title={
               <Card title={`${metric.humanizeName} details`}>
                 <List size="small" dataSource={tooltipData} renderItem={item => <List.Item>{`${item.header} : ${item.value}`}</List.Item>} />
