f0e8069860599aeb75fb02996a65140c5f012cbc
Roman Misyurin <roman.misyurin@percona.com>
2020-02-11 17:06:09 +0300

    refactor: getColumName -> getOverviewColumn

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryAnalyticsOverview/OverviewTable/Column.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/QueryAnalyticsOverview/OverviewTable/Column.tsx
@@ -9,7 +9,7 @@ import { METRIC_CATALOGUE } from '../../metric-catalogue';
 import Tooltip from 'antd/es/tooltip';
 import { Card, List } from 'antd';
 
-export const getColumnName = (metricName, columnIndex, totalValues, orderBy) => {
+export const getOverviewColumn = (metricName, columnIndex, totalValues, orderBy) => {
   const metric = METRIC_CATALOGUE[metricName];
   let sortOrder: boolean | string = false;
   if (orderBy === metricName) {
@@ -31,6 +31,20 @@ export const getColumnName = (metricName, columnIndex, totalValues, orderBy) => 
         { header: 'Sum', value: stats.sum && Humanize.transform(stats.sum, metric.pipeTypes.sumPipe) },
         { header: 'From total', value: ((stats.sum_per_sec / totalValues.metrics[metricName].stats.sum_per_sec) * 100).toFixed(2) + '%' },
       ];
+
+      const { min = 0, max = 0, avg = 0, p99 = 0 } = stats;
+      const minStr = `⌜ Min: ${Humanize.transform(min, this.measurement)}`;
+      const maxStr = `⌟ Max: ${Humanize.transform(max, this.measurement)}`;
+      const avgStr = `◦ Avg: ${Humanize.transform(avg, this.measurement)}`;
+      const p99Str = `${p99 ? `• 99%: ${Humanize.transform(p99, this.measurement)}` : ''}`;
+
+      const latencyTooltipData = [
+        { header: '⌜ Min: ', value: minStr },
+        { header: '⌟ Max: ', value: maxStr },
+        { header: '◦ Avg: ', value: avgStr },
+        { header: '• 99%: ', value: p99Str },
+      ];
+
       // @ts-ignore
       const polygonChartProps = { width: 150, data: item.sparkline };
       return (
@@ -43,6 +57,7 @@ export const getColumnName = (metricName, columnIndex, totalValues, orderBy) => 
               <Card title={`${metric.humanizeName} details`}>
                 <List size="small" dataSource={tooltipData} renderItem={item => <List.Item>{`${item.header} : ${item.value}`}</List.Item>} />
                 {metricName === 'query_time' && <LatencyChart {...{ data: stats }} />}
+                <List size="small" dataSource={latencyTooltipData} renderItem={item => <List.Item>{`${item.header} : ${item.value}`}</List.Item>} />
               </Card>
             }
             overlayStyle={{ backgroundColor: 'transparent' }}
