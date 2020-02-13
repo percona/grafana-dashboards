4c18eb5b87e506396e24e5834cc386024a98f340
Roman Misyurin <roman.misyurin@percona.com>
2020-02-13 22:26:38 +0300

    test: cleanup

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/Filters/Filters.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/Filters/Filters.tsx
@@ -11,60 +11,60 @@ import Search from 'antd/es/input/Search';
 
 const TOP_LIMIT = 5;
 
-const FILTERS_GROUPS = [
-  {
-    name: 'Environment',
-    dataKey: 'environment',
-  },
-  {
-    name: 'Cluster',
-    dataKey: 'cluster',
-  },
-  {
-    name: 'Replication Set',
-    dataKey: 'replication_set',
-  },
-  {
-    name: 'Database',
-    dataKey: 'database',
-  },
-  {
-    name: 'Schema',
-    dataKey: 'schema',
-  },
-  {
-    name: 'Node Name',
-    dataKey: 'node_name',
-  },
-  {
-    name: 'Service Name',
-    dataKey: 'service_name',
-  },
-  {
-    name: 'Client Host',
-    dataKey: 'client_host',
-  },
-  {
-    name: 'User Name',
-    dataKey: 'username',
-  },
-  {
-    name: 'Service Type',
-    dataKey: 'service_type',
-  },
-  {
-    name: 'Node Type',
-    dataKey: 'node_type',
-  },
-  {
-    name: 'City',
-    dataKey: 'city',
-  },
-  {
-    name: 'Availability Zone',
-    dataKey: 'az',
-  },
-];
+export const FILTERS_GROUPS = [
+         {
+           name: 'Environment',
+           dataKey: 'environment',
+         },
+         {
+           name: 'Cluster',
+           dataKey: 'cluster',
+         },
+         {
+           name: 'Replication Set',
+           dataKey: 'replication_set',
+         },
+         {
+           name: 'Database',
+           dataKey: 'database',
+         },
+         {
+           name: 'Schema',
+           dataKey: 'schema',
+         },
+         {
+           name: 'Node Name',
+           dataKey: 'node_name',
+         },
+         {
+           name: 'Service Name',
+           dataKey: 'service_name',
+         },
+         {
+           name: 'Client Host',
+           dataKey: 'client_host',
+         },
+         {
+           name: 'User Name',
+           dataKey: 'username',
+         },
+         {
+           name: 'Service Type',
+           dataKey: 'service_type',
+         },
+         {
+           name: 'Node Type',
+           dataKey: 'node_type',
+         },
+         {
+           name: 'City',
+           dataKey: 'city',
+         },
+         {
+           name: 'Availability Zone',
+           dataKey: 'az',
+         },
+       ];
 
 interface GroupInterface {
   dataKey: string;
