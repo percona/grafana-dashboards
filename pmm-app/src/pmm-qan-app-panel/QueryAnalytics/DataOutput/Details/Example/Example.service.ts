fe1f8aa95ec586e1919b8995ddb6791c75c3b3a8
Roman Misyurin <roman.misyurin@percona.com>
2020-02-14 03:20:04 +0300

    refactor: details cleanup

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Example/Example.service.ts
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Example/Example.service.ts
@@ -1,14 +1,14 @@
 import { apiRequestQAN } from '../../../../../react-plugins-deps/components/helpers/api';
 
 class ExampleService {
-  static async getExample({ filterBy, groupBy, labels, periodStartFrom, periodStartTo, tables }) {
+  static async getExample({ filterBy, groupBy, labels = [], from, to, tables = [] }) {
     const data = {
       filter_by: filterBy,
       group_by: groupBy,
-      labels: labels || [],
-      period_start_from: periodStartFrom,
-      period_start_to: periodStartTo,
-      tables: tables || [],
+      labels: labels,
+      period_start_from: from,
+      period_start_to: to,
+      tables: tables,
     };
 
     return apiRequestQAN.post<any, any>('/ObjectDetails/GetQueryExample', data);
