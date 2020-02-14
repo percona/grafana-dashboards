7e0a7734db0c07696b0a72d7013afd2a9cad24ed
Roman Misyurin <roman.misyurin@percona.com>
2020-02-14 20:56:58 +0300

    refactor: react json view cleanup

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Explain/Explain.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Explain/Explain.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import ReactJson from 'react-json-view';
 import { Collapse } from 'antd';
+import { ReactJSON } from '../../../../../react-plugins-deps/components/ReactJSON/ReactJSON';
 const { Panel } = Collapse;
-
-// TODO: remove stryling code duplicates
-const THEME_JSON_VIEW = {
-  base00: 'transparent',
-  base01: 'transparent',
-  base02: 'lightgray',
-  base03: '#bfbfbf',
-  base04: '#1890ff',
-  base05: '#bfbfbf',
-  base06: '#bfbfbf',
-  base07: '#bfbfbf',
-  base08: '#bfbfbf',
-  base09: 'white',
-  base0A: 'white',
-  base0B: 'white',
-  base0C: 'white',
-  base0D: 'white',
-  base0E: 'white',
-  base0F: 'white',
-};
 
 const customPanelStyle = {
   background: '#1f1d1d',
@@ -49,7 +29,7 @@ const Explain = ({ json, classic }) => {
           {classic}
         </Panel>
         <Panel header="JSON" key="2" style={customPanelStyle}>
-          <ReactJson src={json || {}} theme={THEME_JSON_VIEW} />
+          <ReactJSON json={json} />
         </Panel>
       </Collapse>
     </div>
