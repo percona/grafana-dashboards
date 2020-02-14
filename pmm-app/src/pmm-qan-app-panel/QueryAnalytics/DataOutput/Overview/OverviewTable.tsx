93f108dbe445e32da532a9e038fce0ad7c28e0df
Roman Misyurin <roman.misyurin@percona.com>
2020-02-14 03:05:23 +0300

    refactor: getColumns structure cleanup

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Overview/OverviewTable.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Overview/OverviewTable.tsx
@@ -1,77 +1,9 @@
-import { Select, Table } from 'antd';
+import { Table } from 'antd';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import './OverviewTable.scss';
 import { StateContext } from '../../StateContext';
-import { getOverviewColumn } from './Column';
+import { getDefaultColumns, getOverviewColumn, TABLE_X_SCROLL_WIDTH, TABLE_Y_SCROLL_WIDTH } from './Columns';
 import OverviewTableService from './OverviewTable.service';
-
-const { Option } = Select;
-const GROUP_BY_OPTIONS = [
-  { value: 'queryid', label: 'Query' },
-  { value: 'service_name', label: 'Service Name' },
-  { value: 'database', label: 'Database' },
-  { value: 'schema', label: 'Schema' },
-  { value: 'username', label: 'User Name' },
-  { value: 'client_host', label: 'Client Host' },
-];
-
-const MAIN_METRIC_MIN_WIDTH = 470;
-const TABLE_X_SCROLL_WIDTH = 1250;
-const TABLE_Y_SCROLL_WIDTH = 400;
-const COLUMN_WIDTH = 250;
-const ROW_NUMBER_COLUMN_WIDTH = 30;
-const getDefaultColumns = (groupBy, setGroupBy, pageNumber, pageSize, columns) => {
-  const mainMetricColumnWidth = Math.max(TABLE_X_SCROLL_WIDTH - columns * COLUMN_WIDTH - ROW_NUMBER_COLUMN_WIDTH, MAIN_METRIC_MIN_WIDTH);
-  return [
-    {
-      title: '#',
-      dataIndex: 'rowNumber',
-      key: 'rowNumber',
-      fixed: 'left',
-      width: ROW_NUMBER_COLUMN_WIDTH,
-      render: (text, record, index) => (
-        <div style={{ wordWrap: 'normal', wordBreak: 'normal' }}>{index === 0 ? '' : (pageNumber - 1) * pageSize + index}</div>
-      ),
-    },
-    {
-      dataIndex: 'mainMetric',
-      fixed: 'left',
-      width: mainMetricColumnWidth,
-      title: () => {
-        return (
-          <Select optionLabelProp="label" defaultValue={groupBy} style={{ width: '120px' }} onChange={setGroupBy}>
-            {GROUP_BY_OPTIONS.map(option => (
-              <Option value={option.value} label={option.label}>
-                {option.label}
-              </Option>
-            ))}
-          </Select>
-        );
-      },
-      ellipsis: true,
-      className: 'overview-main-column',
-      render: (text, record) => {
-        //  overflow: hidden;
-        //  text-overflow: ellipsis;
-        //  white-space: nowrap;
-        return (
-          <div
-            style={{
-              wordWrap: 'break-word',
-              wordBreak: 'break-word',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              maxWidth: mainMetricColumnWidth,
-            }}
-          >
-            {record.fingerprint || record.dimension || 'TOTAL'}
-          </div>
-        );
-      },
-    },
-  ];
-};
 
 interface RowInterface {
   dimension?: string;
@@ -147,7 +79,7 @@ const OverviewTable = props => {
 
         props.setTotal(result.total_rows);
         const calculatedColumns = getDefaultColumns(groupBy, setGroupBy, pageNumber, pageSize, columns.length).concat(
-          columns.map((key, index) => getOverviewColumn(key, index, result.rows[0], orderBy, COLUMN_WIDTH))
+          columns.map((key, index) => getOverviewColumn(key, index, result.rows[0], orderBy))
         );
         // @ts-ignore
         setData({ rows: result.rows, columns: calculatedColumns });
