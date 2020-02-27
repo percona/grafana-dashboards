4bd1d00b83b43e2461e9742ff60f9d8539c6f182
Roman Misyurin <roman.misyurin@percona.com>
2020-02-27 11:03:50 +0300

    feat: add filters and table resize features

---
+++ pmm-app/src/react-plugins-deps/components/helpers/WindowSize.hooks.ts
@@ -0,0 +1,16 @@
+import { useLayoutEffect, useState } from 'react';
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([document.documentElement.clientWidth, document.documentElement.clientHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
+
+export default useWindowSize;
