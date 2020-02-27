2b0de858b1d026eb1442194c5d5328b354e0e8ff
Roman Misyurin <roman.misyurin@percona.com>
2020-02-28 00:54:24 +0300

    refactor: add filters disable state

--- pmm-app/src/react-plugins-deps/components/FormComponents/Checkbox/Checkbox.tsx
+++ pmm-app/src/react-plugins-deps/components/FormComponents/Checkbox/Checkbox.tsx
@@ -4,7 +4,6 @@ import './Checkbox.scss';
 import { Typography } from 'antd';
 
 const { Text } = Typography;
-// import { useField } from 'react-final-form';
 interface CheckboxFieldInterface {
   required?: boolean;
   name: string;
@@ -12,13 +11,15 @@ interface CheckboxFieldInterface {
   form: any;
   label?: string;
   checked?: boolean;
+  disabled?: boolean;
 }
-export const CheckboxField = ({ name, label, required, form, ...rest }: CheckboxFieldInterface) => {
+
+export const CheckboxField = ({ name, label, required, form, disabled = false, ...rest }: CheckboxFieldInterface) => {
   const field = useField(name, form);
 
   return (
     <label className="checkbox-container checkbox-container--main">
-      <input {...field.input} type="checkbox" checked={rest.checked || field.input.value} />
+      <input {...field.input} type="checkbox" checked={rest.checked || field.input.value} disabled={disabled} />
       <span className="checkbox-container__checkmark"></span>
       <Text className="checkbox-container__label-text" ellipsis={true}>
         {label}
