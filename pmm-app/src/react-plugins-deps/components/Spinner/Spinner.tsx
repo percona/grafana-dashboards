98c5027f4efd733addc4fda87809de29989fe041
Roman Misyurin <roman.misyurin@percona.com>
2020-03-01 11:57:01 +0300

    feat: add setDefaultSpinner

--- pmm-app/src/react-plugins-deps/components/Spinner/Spinner.tsx
+++ pmm-app/src/react-plugins-deps/components/Spinner/Spinner.tsx
@@ -1,26 +1,8 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Spin } from 'antd';
 
 library.add(fas);
 
-interface SpinnerInterface {
-  size: 'small' | 'medium' | 'large';
-}
-const Spinner = ({ size = 'medium' }: SpinnerInterface): ReactElement => {
-  const getSizePX = size => {
-    switch (size) {
-      case 'large':
-        return '50px';
-      case 'medium':
-        return '36px';
-      case 'small':
-        return '20px';
-      default:
-        return '36px';
-    }
-  };
-  return <i className="fa fa-spinner fa-spin spinner" style={{ color: 'white', fontSize: getSizePX(size) }}></i>;
-};
-
-export default Spinner;
+Spin.setDefaultIndicator(<i className="fa fa-spinner fa-spin spinner" style={{ color: 'rgb(211,211,211)', fontSize: '36px' }}></i>);
