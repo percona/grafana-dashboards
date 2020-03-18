862d41f52cd55b717b9143c103b002d98f0e967f
Roman Misyurin <roman.misyurin@percona.com>
2020-03-18 22:23:29 +0300

    refactor: custom-panel-styling fixed

--- pmm-app/src/pmm-qan-app-panel/panel.tsx
+++ pmm-app/src/pmm-qan-app-panel/panel.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './panel.scss';
 import '../react-plugins-deps/styles.scss';
 import '../react-plugins-deps/style.less';
 import { UrlParametersProvider } from './QueryAnalytics/StateContext';
 import FiltersContainer from './QueryAnalytics/Filters/Filters';
 import QueryAnalyticsContainer from './QueryAnalytics/DataOutput/QueryAnalyticsContainer';
+import Styling from '../react-plugins-deps/components/helpers/styling';
 
 const SettingsPanel = () => {
+  useEffect(() => Styling.addPluginPanelClass(), []);
   return (
     <UrlParametersProvider>
       <div className={'query-analytics-grid'} id={'antd'}>
