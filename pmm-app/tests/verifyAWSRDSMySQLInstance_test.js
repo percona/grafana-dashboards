5b9ef56d4f0c4a1b572377e709ff69bec5cbebfd
Roman Misyurin <roman.misyurin@percona.com>
2020-03-26 11:32:23 +0300

    PMM-5649 skip AWS RDS tests

--- pmm-app/tests/verifyAWSRDSMySQLInstance_test.js
+++ pmm-app/tests/verifyAWSRDSMySQLInstance_test.js
@@ -5,7 +5,7 @@ Before((I, loginPage) => {
   loginPage.login('admin', 'admin');
 });
 
-Scenario(
+xScenario(
   'Verify Discovery and adding AWS RDS MySQL 5.6 instance for monitoring',
   async (I, remoteInstancesPage, pmmInventoryPage) => {
     const instanceIdToMonitor = 'rds-mysql56';
@@ -22,7 +22,7 @@ Scenario(
   }
 );
 
-Scenario(
+xScenario(
   'Verify AWS RDS MySQL 5.6 instance has status running @pmm-post-update',
   async (I, remoteInstancesPage, pmmInventoryPage) => {
     const serviceName = 'rds-mysql56';
@@ -32,7 +32,7 @@ Scenario(
   }
 );
 
-Scenario(
+xScenario(
   'Verify QAN Filters contain AWS RDS MySQL 5.6 after it was added for monitoring',
   async (I, qanPage, adminPage) => {
     const environment = 'RDS MySQL 5.6';
