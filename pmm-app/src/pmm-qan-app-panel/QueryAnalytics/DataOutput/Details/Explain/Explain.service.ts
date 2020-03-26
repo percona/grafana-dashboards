237f6841f945f09442994f0a3c6e30548877fe4f
Roman Misyurin <roman.misyurin@percona.com>
2020-03-27 00:33:21 +0300

    PMM-5649 Mongodb explain added

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Explain/Explain.service.ts
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Explain/Explain.service.ts
@@ -1,7 +1,7 @@
 import { apiRequestManagement } from '../../../../../react-plugins-deps/components/helpers/api';
 
 class ExplainService {
-  static getTraditionalExplainJSON({ database, service_id, query }) {
+  static getTraditionalExplainJSONMysql({ database, service_id, query }) {
     const body = {
       database,
       service_id,
@@ -9,13 +9,22 @@ class ExplainService {
     };
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLExplainJSON', body);
   }
-  static getTraditionalExplain({ database, service_id, query }) {
+  static getTraditionalExplainMysql({ database, service_id, query }) {
     const body = {
       database,
       service_id,
       query,
     };
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLExplain', body);
+  }
+
+  static getTraditionalExplainJSONMongo({ pmm_agent_id, service_id, query }) {
+    const body = {
+      pmm_agent_id,
+      service_id,
+      query,
+    };
+    return apiRequestManagement.post<any, any>('/Actions/StartMongoDBExplain', body);
   }
 
   static getActionResult({ action_id }) {
