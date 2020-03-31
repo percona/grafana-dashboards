4d0d4b5bb344a60fda5c53c2d92171759f770a55
Roman Misyurin <roman.misyurin@percona.com>
2020-04-01 00:41:27 +0300

    PMM-5649 add postgres show create table display

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Table/Table.service.ts
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Table/Table.service.ts
@@ -1,13 +1,22 @@
 import { apiRequestManagement } from '../../../../../react-plugins-deps/components/helpers/api';
 
 class TableService {
-  static getShowCreateTable({ database, service_id, table_name }) {
+  static getShowCreateTableMySQL({ database, service_id, table_name }) {
     const body = {
       database,
       service_id,
       table_name,
     };
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLShowCreateTable', body);
+  }
+
+  static getShowCreateTablePostgreSQL({ database, service_id, table_name }) {
+    const body = {
+      database,
+      service_id,
+      table_name,
+    };
+    return apiRequestManagement.post<any, any>('/Actions/StartPostgreSQLShowCreateTable', body);
   }
 
   static getActionResult({ action_id }) {
