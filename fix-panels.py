c5cf358c2834f5b8c8ca95db8556ae385e1b0e8b
adivinho <vadim.yalovets@percona.com>
2020-04-01 13:29:58 +0300

    PMM-5655 remove leftovers of Grafana plugins

---
+++ fix-panels.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Grafana dashboard importer script.
+
+import json
+import os
+import shutil
+import sys
+
+GRAFANA_PLUGINS_DR = '/var/lib/grafana/plugins/'
+
+
+def rename_panels():
+    for app in ['panels']:
+        print '  * Renaming %r' % (app,)
+        panels_list = os.listdir(GRAFANA_PLUGINS_DR)
+        for panel in panels_list:
+            panel_path = os.path.join(GRAFANA_PLUGINS_DR, panel, 'dist/plugin.json')
+            if os.path.exists(panel_path):
+                print '   * %r -> ' % (panel,),
+                with open(panel_path, 'r') as f:
+                    panel_params = json.loads(f.read())
+                    print '%r' % (panel_params['id'],)
+                    if (not(os.path.exists(os.path.join(GRAFANA_PLUGINS_DR, panel_params['id'])))):
+                        print '    * Renaming panel'
+                        shutil.copytree(os.path.join(GRAFANA_PLUGINS_DR, panel), os.path.join(GRAFANA_PLUGINS_DR, panel_params['id']))
+                    if (panel != panel_params['id']):
+                        print '    * Removing leftovers'
+                        shutil.rmtree(os.path.join(GRAFANA_PLUGINS_DR, panel))
+
+print "Grafana database directory: %s" % (GRAFANA_PLUGINS_DR,)
+rename_panels()
