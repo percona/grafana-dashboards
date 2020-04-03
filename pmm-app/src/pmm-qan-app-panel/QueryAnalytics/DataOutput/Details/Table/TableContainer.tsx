96cf658daf708538d33c96938f9bb3951e16f8d7
Roman Misyurin <roman.misyurin@percona.com>
2020-04-03 16:54:49 +0300

    PMM-5677 add spinner to details section

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Table/TableContainer.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Table/TableContainer.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ExampleService from '../Example/Example.service';
 import TableService from './Table.service';
 import TableCreate from './Table';
-import { Tabs } from 'antd';
+import { Spin, Tabs } from 'antd';
 
 const { TabPane } = Tabs;
 
@@ -10,10 +10,12 @@ const TableCreateContainer = props => {
   const { queryId, groupBy, from, to, labels, tables, databaseType } = props;
 
   const [example, setExample] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
+        setLoading(true);
         const result = await ExampleService.getExample({
           filterBy: queryId,
           groupBy,
@@ -24,32 +26,38 @@ const TableCreateContainer = props => {
         });
         const queryExample = result['query_examples'][0];
         setExample(queryExample);
+        setLoading(false);
       } catch (e) {
+        setLoading(false);
         //TODO: add error handling
       }
     })();
   }, [queryId]);
 
   // return <div>{errorText ? <pre>{errorText}</pre> : <pre>{showCreateTable}</pre>}</div>;
-  return example && example.tables ? (
-    <Tabs defaultActiveKey="1" onChange={() => {}} tabPosition="top">
-      {example.tables.map((table, index) => {
-        return (
-          <TabPane tab={<span>{table}</span>} key={index}>
-            <TableCreate
-              tableName={table}
-              example={example}
-              databaseType={databaseType}
-              schema={example.schema}
-            />
-          </TabPane>
-        );
-      })}
-    </Tabs>
-  ) : (
-    <div>
-      <pre>Sorry, no tables found =(</pre>
-    </div>
+  return (
+    <Spin spinning={loading}>
+      {example && example.tables ? (
+        <Tabs defaultActiveKey="1" onChange={() => {}} tabPosition="top">
+          {example.tables.map((table, index) => {
+            return (
+              <TabPane tab={<span>{table}</span>} key={index}>
+                <TableCreate
+                  tableName={table}
+                  example={example}
+                  databaseType={databaseType}
+                  schema={example.schema}
+                />
+              </TabPane>
+            );
+          })}
+        </Tabs>
+      ) : (
+        <div>
+          <pre>Sorry, no tables found =(</pre>
+        </div>
+      )}
+    </Spin>
   );
 };
 
