dcc6d533215edd1f01cc337d218a40745d59d0ae
yurkovychv <vasyl.yurkovych@percona.com>
2020-04-04 00:35:50 +0300

    commented on snapshot plugin

--- pmm-app/cypress/plugins/index.js
+++ pmm-app/cypress/plugins/index.js
@@ -17,6 +17,6 @@
  */
 
 // const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin')
-// module.exports = (on, config) => {
+module.exports = (on, config) => {
 //   	addMatchImageSnapshotPlugin(on, config)
-// }
+}
