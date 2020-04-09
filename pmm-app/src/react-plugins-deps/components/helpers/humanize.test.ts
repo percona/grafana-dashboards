06d143554c119ead1ed11e7e664fe0dd213695ad
Roman Misyurin <roman.misyurin@percona.com>
2020-04-09 21:28:33 +0300

    add humanize

--- pmm-app/src/react-plugins-deps/components/helpers/humanize.test.ts
+++ pmm-app/src/react-plugins-deps/components/helpers/humanize.test.ts
@@ -1,14 +1,116 @@
-import Humanize from './humanize';
-describe('Humanize module', () => {
-  it('should return <0.01', () => {
-    expect(Humanize.formatPercent(0.00001)).toBe('<0.01');
+import { Humanize } from './Humanize';
+
+describe('Humanize', () => {
+  it('create an instance', () => {
+    const pipe = new Humanize();
+    expect(pipe).toBeTruthy();
   });
 
-  it('should return 100%', () => {
-    expect(Humanize.formatPercent(1)).toBe('100%');
+  it('should return 0 if input is zero', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(0);
+    expect(result).toBe('0');
   });
 
-  xit('should return 21.5%', () => {
-    expect(Humanize.formatPercent(21.575)).toBe('21.5%');
+  it('should return 20.00 sec if input is more than 1 and less than 60', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(20);
+    expect(result).toBe('20.00 sec');
+  });
+
+  it('should return 0:01:00 if input is more or equal 60', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(60);
+    expect(result).toBe('0:01:00');
+  });
+
+  it('should return 1 days, 0:01:40 if input is more than 86400', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(86500);
+    expect(result).toBe('1 days, 0:01:40');
+  });
+
+  it('should return 20.00 ms if input is less than 1', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(0.02);
+    expect(result).toBe('20.00 ms');
+  });
+
+  it('should return -1000000.00 µs if input is negative value', () => {
+    const pipe = new Humanize();
+    const result = pipe.parceTime(-1);
+    expect(result).toBe('-1000000.00 µs');
+  });
+
+  it('should return 0 if input is null', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(null, 'time');
+    expect(result).toBe('0');
+  });
+
+  it('should return 2.00 sec  if input is 2 and name is undefined', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, undefined);
+    expect(result).toBe('2.00 sec');
+  });
+
+  it('should return -1000000.00 µs if input is negative value', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, 'time');
+    expect(result).toBe('2.00 sec');
+  });
+
+  it('should return <1.00 µs if input is less than 0.00001', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(0.000001, 'time');
+    expect(result).toBe('<1.00 µs');
+  });
+
+  it('should return 2.00 Bytes if input is 2 and name is size', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, 'size');
+    expect(result).toBe('2.00 Bytes');
+  });
+
+  it('should return <0.01 Bytes if input is 0.001 and name is size', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(0.001, 'size');
+    expect(result).toBe('<0.01 Bytes');
+  });
+
+  it('should return <0.01 if input is 0.001 and name is number', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(0.001, 'number');
+    expect(result).toBe('<0.01');
+  });
+
+  it('should return 2.00 if input is 2 and name is number', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, 'number');
+    expect(result).toBe('2.00');
+  });
+
+  it('should return 200.00% if input is 2 and name is percent', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, 'percent');
+    expect(result).toBe('200.00%');
+  });
+
+  it('should return <0.01 if input is 0.0000001 and name is percent', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(0.0000001, 'percent');
+    expect(result).toBe('<0.01');
+  });
+
+  it('should return <0.01 if input is 0.001', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(0.001, '');
+    expect(result).toBe('<0.01');
+  });
+
+  it('should return 2.00  if input is 2', () => {
+    const pipe = new Humanize();
+    const result = pipe.transform(2, '');
+    expect(result).toBe('2.00 ');
   });
 });
--- pmm-app/src/react-plugins-deps/components/helpers/humanize.ts
+++ pmm-app/src/react-plugins-deps/components/helpers/humanize.ts
@@ -1,18 +1,95 @@
+import * as moment from 'moment';
 import * as numeral from 'numeral';
 
-class Humanize {
-  static formatPercent(input) {
-    let res;
-    if (input !== 0 && input < 0.0001) {
-      res = '<0.01';
-    } else if (input === 1) {
-      res = '100%';
-    } else {
-      res = numeral(input).format('0.00%');
+export class Humanize {
+  parceTime(input: number) {
+    let dur = '';
+    const dur_sec = moment.duration(input, 's');
+    switch (true) {
+      case input === 0:
+        dur = '0';
+        break;
+      case dur_sec.as('s') > 1 && dur_sec.as('s') < 60:
+        dur = dur_sec.as('s').toFixed(2) + ' sec';
+        break;
+      case dur_sec.as('s') >= 60:
+        let secs = dur_sec.as('s');
+        const secondsInDay = 24 * 60 * 60;
+        if (secs >= secondsInDay) {
+          const days = Math.floor(secs / secondsInDay);
+          dur = `${days} days, `;
+          secs = secs % secondsInDay;
+        }
+        dur += numeral(secs).format('00:00:00');
+        break;
+      case dur_sec.as('ms') < 1:
+        dur = (dur_sec.as('ms') * 1000).toFixed(2) + ' µs';
+        break;
+      default:
+        dur = dur_sec.as('ms').toFixed(2) + ' ms';
+        break;
+    }
+    return dur;
+  }
+
+  transform(input: number, name: string): string {
+    if (input === null) {
+      return '0';
     }
 
-    return res;
+    let res = '0';
+    switch (name) {
+      // "top 10"/profile queries no name parameters
+      case undefined:
+        res = this.parceTime(input);
+        break;
+      // time
+      case 'time':
+        res = input !== 0 && input < 0.00001 ? '<' : '';
+        res += this.parceTime(input);
+        break;
+      // size
+      case 'size':
+        if (input !== 0 && input < 0.01) {
+          res = '<0.01 B';
+        } else {
+          res = numeral(input).format('0.00 b');
+        }
+        res = res.replace(/([\d]) B/, '$1 Bytes');
+        break;
+      // ops
+      case 'number':
+        if (input !== 0 && input < 0.01) {
+          res = '<0.01';
+        } else {
+          res = numeral(input).format('0.00a');
+        }
+        break;
+      case 'percent':
+        if (input !== 0 && input < 0.0001) {
+          res = '<0.01';
+        } else if (input === 1) {
+          res = '100%';
+        } else {
+          res = numeral(input).format('0.00%');
+        }
+        break;
+      case 'percentRounded':
+        if (input !== 0 && input < 0.0001) {
+          res = '<0.01';
+        } else {
+          res = numeral(input).format('0%');
+        }
+        break;
+      // ops
+      default:
+        if (input !== 0 && input < 0.01) {
+          res = '<0.01';
+        } else {
+          res = numeral(input).format('0.00 a');
+        }
+        break;
+    }
+    return String(res).replace('<0.00', '<0.01');
   }
 }
-
-export default Humanize;
