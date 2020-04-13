f1d229128c9c10b39f3db2bfe33bc08925b4a055
Roman Misyurin <roman.misyurin@percona.com>
2020-04-13 13:08:59 +0300

    PMM-5718 state context cleanup

--- pmm-app/src/pmm-qan-app-panel/QueryAnalytics/StateContext.tsx
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/StateContext.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ParseQueryParamDate } from '../../react-plugins-deps/components/helpers/time-parameters-parser';
 import { getDataSourceSrv } from '@grafana/runtime';
-import _ from 'lodash';
+import { find } from 'lodash';
 
 const initialState = {} as any;
 
@@ -24,112 +24,105 @@ const FILTERS_NAMES = [
   'az',
 ];
 
-class ContextActions {
-  constructor() {}
-
-  static setFilters(query) {
-    return FILTERS_NAMES.reduce((result, filterName) => {
-      const filters = query.getAll(`var-${filterName}`);
-      if (!filters.length) {
-        return result;
-      }
-
-      if (filters[0] === 'All' || filters[0] === '') {
-        return result;
-      }
-
-      result[filterName] = filters;
+const setFilters = query =>
+  FILTERS_NAMES.reduce((result, filterName) => {
+    const filters = query.getAll(`var-${filterName}`);
+    if (!filters.length) {
       return result;
-    }, {});
-  }
+    }
 
-  static refreshGrafanaVariables(state) {
-    const dataSource = getDataSourceSrv();
-    // @ts-ignore
-    const templateVariables = dataSource.templateSrv.variables;
-    FILTERS_NAMES.forEach(filter => {
-      const variables = _.find(templateVariables, { name: filter.replace('var-', '') });
-      if (!variables) {
-        return;
-      }
-      variables.current = {
-        text: state.labels[filter] || ['All'],
-        value: state.labels[filter] || ['All'],
-      };
-    });
-    templateVariables[0].variableSrv.variableUpdated(templateVariables[0]);
-  }
+    if (filters[0] === 'All' || filters[0] === '') {
+      return result;
+    }
 
-  static generateURL(state) {
-    // read parameters and create new url
-    // @ts-ignore
-    const labels =
-      state.labels &&
-      Object.keys(state.labels)
-        .map(key => {
-          // @ts-ignore
-          const variables = state.labels[key];
-          return variables.map(variable => `var-${key}=${variable}`).join('&');
-        })
-        .filter(Boolean)
-        .join('&');
-    const columnsQuery = state.columns ? `columns=${JSON.stringify(state.columns)}` : '';
-    const groupBy = state.groupBy ? `group_by=${state.groupBy}` : '';
-    const filterByQuery = state.queryId ? `filter_by=${state.queryId}` : '';
-    const orderBy = state.orderBy ? `order_by=${state.orderBy}` : '';
-    const from = state.rawTime && state.rawTime.from ? `from=${state.rawTime.from}` : '';
-    const to = state.rawTime && state.rawTime.to ? `to=${state.rawTime.to}` : '';
-    // TODO: replace crutch with right redirect
-    return `${window.location.pathname}?${[columnsQuery, filterByQuery, labels, orderBy, groupBy, from, to]
+    result[filterName] = filters;
+    return result;
+  }, {});
+
+const refreshGrafanaVariables = state => {
+  const dataSource = getDataSourceSrv();
+  // @ts-ignore
+  const templateVariables = dataSource.templateSrv.variables;
+  FILTERS_NAMES.forEach(filter => {
+    const variables = find(templateVariables, { name: filter.replace('var-', '') });
+    if (!variables) {
+      return;
+    }
+    variables.current = {
+      text: state.labels[filter] || ['All'],
+      value: state.labels[filter] || ['All'],
+    };
+  });
+  templateVariables[0].variableSrv.variableUpdated(templateVariables[0]);
+};
+
+const generateURL = state => {
+  // read parameters and create new url
+  const { labels, columns, groupBy, queryId, orderBy } = state;
+  // @ts-ignore
+  const urlLabels =
+    labels &&
+    Object.keys(labels)
+      .map(key => {
+        // @ts-ignore
+        const variables = labels[key];
+        return variables.map(variable => `var-${key}=${variable}`).join('&');
+      })
       .filter(Boolean)
-      .join('&')}`;
-  }
+      .join('&');
+  const urlColumnsQuery = columns ? `columns=${JSON.stringify(columns)}` : '';
+  const urlGroupBy = groupBy ? `group_by=${groupBy}` : '';
+  const urlFilterByQuery = queryId ? `filter_by=${queryId}` : '';
+  const urlOrderBy = orderBy ? `order_by=${orderBy}` : '';
+  const urlFrom = state.rawTime && state.rawTime.from ? `from=${state.rawTime.from}` : '';
+  const urlTo = state.rawTime && state.rawTime.to ? `to=${state.rawTime.to}` : '';
+  // TODO: replace crutch with right redirect
+  return `${window.location.pathname}?${[
+    urlColumnsQuery,
+    urlFilterByQuery,
+    urlLabels,
+    urlOrderBy,
+    urlGroupBy,
+    urlFrom,
+    urlTo,
+  ]
+    .filter(Boolean)
+    .join('&')}`;
+};
 
-  static parseURL(query) {
-    const urlParams = {} as any;
-    urlParams.from = ParseQueryParamDate.transform(query.get('from') || 'now-12h', 'from')
-      .utc()
-      .format('YYYY-MM-DDTHH:mm:ssZ');
-    urlParams.to = ParseQueryParamDate.transform(query.get('to') || 'now', 'to')
-      .utc()
-      .format('YYYY-MM-DDTHH:mm:ssZ');
-    // urlParams.rawTime = {
-    //   from: query.get('from'),
-    //   to: query.get('to'),
-    // };
-    urlParams.columns = JSON.parse(query.get('columns')) || DEFAULT_COLUMNS;
-    urlParams.labels = ContextActions.setFilters(query);
-    urlParams.pageNumber = 1;
-    urlParams.pageSize = 10;
-    urlParams.orderBy = query.get('order_by') || `-${urlParams.columns[0]}`;
-    urlParams.queryId = query.get('filter_by');
-    urlParams.querySelected = !!query.get('filter_by');
-    urlParams.groupBy = query.get('group_by') || 'queryid';
-    // console.log(Object.assign({}, urlParams));
-    return urlParams;
-  }
+const parseURL = query => ({
+  from: ParseQueryParamDate.transform(query.get('from') || 'now-12h', 'from'),
+  to: ParseQueryParamDate.transform(query.get('to') || 'now', 'to')
+    .utc()
+    .format('YYYY-MM-DDTHH:mm:ssZ'),
+  columns: JSON.parse(query.get('columns')) || DEFAULT_COLUMNS,
+  labels: setFilters(query),
+  pageNumber: 1,
+  pageSize: 10,
+  orderBy: query.get('order_by') || `-${( JSON.parse(query.get('columns')) || DEFAULT_COLUMNS)[0]}`,
+  queryId: query.get('filter_by'),
+  querySelected: !!query.get('filter_by'),
+  groupBy: query.get('group_by') || 'queryid',
+});
 
-  static setLabels(filters) {
-    const labels = {};
-    Object.keys(filters)
-      .filter(filter => filters[filter])
-      .forEach(filter => {
-        const [group, value] = filter.split(':');
-        // TODO: using '--' because final form think that it is a nested fields
-        //  need to replace it with something better
-        if (labels[group]) {
-          labels[group].push(value.replace(/\-\-/gi, '.'));
-        } else {
-          labels[group] = [value.replace(/\-\-/gi, '.')];
-        }
-      });
-    return labels;
-  }
-}
+const setLabels = filters =>
+  Object.keys(filters)
+    .filter(filter => filters[filter])
+    .reduce((labels, filter) => {
+      const [group, value] = filter.split(':');
+      // TODO: using '--' because final form think that it is a nested fields
+      //  need to replace it with something better
+      if (labels[group]) {
+        labels[group].push(value.replace(/\-\-/gi, '.'));
+      } else {
+        labels[group] = [value.replace(/\-\-/gi, '.')];
+      }
+      return labels;
+    }, {});
 
 const actions = {
   setLabels: value => state => {
-    const newState = { ...state, labels: ContextActions.setLabels(value), pageNumber: 1 };
+    const newState = { ...state, labels: setLabels(value), pageNumber: 1 };
     delete newState.queryId;
     delete newState.querySelected;
     return newState;
@@ -144,34 +137,31 @@ const actions = {
     return { ...state, queryId: value || 'TOTAL', querySelected: true };
   },
   addColumn: value => state => {
-    const columns = state.columns.slice();
+    const columns = [...state.columns];
     columns.push(value);
-    const newState = {
+    return {
       ...state,
-      columns: columns,
+      columns,
     };
-    return newState;
   },
   changeColumn: value => state => {
-    const columns = state.columns.slice();
+    const columns = [...state.columns];
     columns[columns.indexOf(value.oldColumn.simpleName)] = value.column;
-    const newState = {
+    return {
       ...state,
-      columns: columns,
+      columns,
       orderBy:
         value.oldColumn.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
     };
-    return newState;
   },
   removeColumn: value => state => {
-    const columns = state.columns.slice();
+    const columns = [...state.columns];
     columns.splice(columns.indexOf(value.simpleName), 1);
-    const newState = {
+    return {
       ...state,
-      columns: columns,
+      columns,
       orderBy: value.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
     };
-    return newState;
   },
   changePage: value => state => {
     const newState = {
@@ -227,17 +217,6 @@ const actions = {
       fingerprint: value,
     };
   },
-  updateTimeRange: value => state => {
-    const newState = {
-      ...state,
-      from: value.from,
-      to: value.to,
-    };
-    delete newState.queryId;
-    delete newState.querySelected;
-    return newState;
-  },
-  refresh: value => state => {},
 };
 
 export const UrlParametersProvider = ({ grafanaProps, children }) => {
@@ -246,12 +225,12 @@ export const UrlParametersProvider = ({ grafanaProps, children }) => {
   const from = grafanaProps.timeRange.from.utc().format('YYYY-MM-DDTHH:mm:ssZ');
   const to = grafanaProps.timeRange.to.utc().format('YYYY-MM-DDTHH:mm:ssZ');
   const [panelState, setContext] = useState({
-    ...ContextActions.parseURL(query),
+    ...parseURL(query),
   });
 
   useEffect(() => {
-    ContextActions.refreshGrafanaVariables(panelState);
-    const newUrl = ContextActions.generateURL(panelState);
+    refreshGrafanaVariables(panelState);
+    const newUrl = generateURL(panelState);
     history.pushState({}, 'test', newUrl);
   }, [panelState]);
 
