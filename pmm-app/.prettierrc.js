20cc6b80a1c3de8977a61b1f361a0310aea17757
Alexander Tymchuk <alexander.tymchuk@percona.com>
2020-04-18 01:28:49 +0300

    SAAS-31 update eslint & readme

--- pmm-app/.prettierrc.js
+++ pmm-app/.prettierrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ...require('./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json'),
+  ...require('@grafana/toolkit/src/config/prettier.plugin.config.json'),
   printWidth: 110,
 };
