212a254eda3fb3c8b7621a0c4656f85008a9e890
Roman Misyurin <roman.misyurin@percona.com>
2020-04-18 10:52:03 +0300

    add test cases for filters

--- pmm-app/tests/QAN/filters_test.js
+++ pmm-app/tests/QAN/filters_test.js
@@ -9,17 +9,48 @@ Scenario('Open the QAN Dashboard and check existence of filters @new-qan', async
   await qanPage.verifyFiltersSectionIsPresent();
 });
 
-Scenario('Open the QAN Dashboard and check work of button "reset all" @new-qan', async (I, qanPage) => {
-  qanPage.selectFilter(qanPage.fields.filterCheckboxSelector);
-  qanPage.resetAllFilters();
-  I.dontSeeCheckboxIsChecked(qanPage.fields.filterCheckboxSelector);
-});
-
 xScenario(
   'Open the QAN Dashboard switch between show all/selected correct',
   async (I, adminPage, qanPage) => {}
 );
 
-xScenario('Open the QAN Dashboard filters search works correct', async (I, adminPage, qanPage) => {
-  // TODO: check changes in url
+xScenario(
+  'T-123 Verify User is able to search for filter value @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+xScenario(
+  'T-124 Verify User is able to show all and show top 5 values for filter section @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+xScenario(
+  'T-125 Verify user is able to Show only selected filter values and Show All filter values @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+Scenario('T-126 Open the QAN Dashboard and check work of button "reset all" @new-qan', async (I, qanPage) => {
+  qanPage.selectFilter(qanPage.fields.filterCheckboxSelector);
+  qanPage.resetAllFilters();
+  I.dontSeeCheckboxIsChecked(qanPage.fields.filterCheckboxSelector);
 });
+xScenario(
+  'T-172 Verify that selecting a filter updates the table data and URL @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+xScenario(
+  "T-175 Verify user is able to apply filter that has dots ('.') in label @new-qan ",
+  async (I, adminPage, qanPage) => {}
+);
+xScenario('T-190 Verify user is able to see n/a filter @new-qan ', async (I, adminPage, qanPage) => {});
+xScenario(
+  'T-191 Verify Reset all points the user to the default filters view @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+xScenario(
+  'T-192 Verify `show selected` and `reset all` disabled if nothing was selected @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
+xScenario('T-211 Verify filters headers style @visual @new-qan ', async (I, adminPage, qanPage) => {});
+xScenario('T-217 Verify filter by Service Name works @new-qan ', async (I, adminPage, qanPage) => {});
+xScenario(
+  'T-221 Verify that all filter options are always visible (but some disabled) after selecting an item and % value is changed @new-qan ',
+  async (I, adminPage, qanPage) => {}
+);
