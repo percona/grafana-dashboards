2c9ea2f43c8d61813fdb4541d7fdb257da31f9d2
Roman Misyurin <roman.misyurin@percona.com>
2020-04-23 21:19:14 +0300

    add useExample hooks

---
+++ pmm-app/src/pmm-qan-app-panel/QueryAnalytics/DataOutput/Details/Example/Example.hooks.ts
@@ -0,0 +1,33 @@
+import { useContext, useEffect, useState } from 'react';
+import ExampleService from './Example.service';
+import { StateContext } from '../../../StateContext';
+
+export const useExamples = () => {
+  const {
+    panelState: { queryId, groupBy, from, to, labels },
+  } = useContext(StateContext);
+  const [examples, setExamples] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const result = await ExampleService.getExample({
+          filterBy: queryId,
+          groupBy,
+          from,
+          to,
+          labels,
+        });
+        setExamples(result['query_examples'].filter(example => example.example));
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        //TODO: add error handling
+      }
+    })();
+  }, [queryId]);
+
+  return [examples, loading];
+};
