546264b7b81f30b22c94f2ff3aa40673fac0e3fb
Roman Misyurin <roman.misyurin@percona.com>
2020-04-27 16:36:05 +0300

    PMM-5766 cleanup

--- pmm-app/src/react-plugins-deps/components/Form/Checkbox/Checkbox.tsx
+++ pmm-app/src/react-plugins-deps/components/Form/Checkbox/Checkbox.tsx
@@ -5,12 +5,11 @@ import { Typography } from 'antd';
 import { CheckboxFieldInterface } from './Checkbox.types';
 
 const { Text } = Typography;
-export const CheckboxField = ({ name, label, disabled }: CheckboxFieldInterface) => {
-  const field = useField(name);
-
+export const CheckboxField = ({ name, label, disabled, checked }: CheckboxFieldInterface) => {
+  const field = useField(name, { type: 'checkbox' });
   return (
     <label className="checkbox-container checkbox-container--main">
-      <input {...field.input} type="checkbox" disabled={disabled} />
+      <input {...field.input} type="checkbox" checked={field.input.checked || checked} disabled={disabled} />
       <span className="checkbox-container__checkmark"></span>
       <Text className="checkbox-container__label-text" ellipsis>
         {label}
