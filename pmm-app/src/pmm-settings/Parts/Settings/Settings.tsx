08ef99b9d80bc045d94f1b4c74ffaf3375ddd19d
Roman Misyurin <roman.misyurin@percona.com>
2020-04-27 14:08:25 +0300

    PMM-5820 update snapshot

--- pmm-app/src/pmm-settings/Parts/Settings/Settings.tsx
+++ pmm-app/src/pmm-settings/Parts/Settings/Settings.tsx
@@ -252,7 +252,9 @@ const SettingsPart = props => {
                       <PluginTooltip
                         links={[
                           {
-                            url: 'https://www.percona.com/doc/percona-monitoring-and-management/2.x/manage/server-admin-gui.html#stt',
+                            url:
+                              // eslint-disable-next-line max-len
+                              'https://www.percona.com/doc/percona-monitoring-and-management/2.x/manage/server-admin-gui.html#stt',
                             text: 'Read more',
                           },
                         ]}
