3ea4c9f8fd08ef06f45336d1e8ddc65727b16468
Roman Misyurin <roman.misyurin@percona.com>
2020-04-29 19:11:55 +0300

    PMM-5790 fingerprint improvements

--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Example/Example.tsx
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Example/Example.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Highlight from 'react-highlight.js';
 import { ReactJSON } from '../../../../../../react-plugins-deps/components/Elements/ReactJSON/ReactJSON';
 import { useExamples } from './Example.hooks';
 import { useDatabaseType } from '../Details.hooks';
 import { DATABASE } from '../Details.constants';
+import { PanelProvider } from '../../../../../panel/panel.provider';
 
 const getExample = databaseType => (example: any): any => {
   if (databaseType === DATABASE.mongodb) {
-    return <ReactJSON json={JSON.parse(example.example)} />;
+    return <ReactJSON json={JSON.parse(example)} />;
   }
 
-  return <Highlight language="sql">{example.example}</Highlight>;
+  return <Highlight language="sql">{example}</Highlight>;
 };
 
 const Example = () => {
-  const [examples] = useExamples();
+  const {
+    panelState: { fingerprint },
+  } = useContext(PanelProvider);
   const databaseType = useDatabaseType();
+  let [examples] = useExamples();
 
   return (
     <div>
-      {examples && examples.filter(example => example.example).length ? (
+      {databaseType === DATABASE.postgresql ? (
+        getExample(databaseType)(fingerprint)
+      ) : examples && examples.filter(example => example.example).length ? (
         examples.filter(example => example.example).map(getExample(databaseType))
       ) : (
         <pre>Sorry, no examples found for this query</pre>
