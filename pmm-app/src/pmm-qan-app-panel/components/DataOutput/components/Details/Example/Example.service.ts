4867253758fc2e7915edc290ecf62341f58b6828
Roman Misyurin <roman.misyurin@percona.com>
2020-04-29 16:04:42 +0300

    PMM-5830 set fixed codecept version

---
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Example/Example.service.ts
@@ -0,0 +1,24 @@
+import { apiRequestQAN } from '../../../../../../react-plugins-deps/helpers/api';
+
+class ExampleService {
+  static async getExample({ filterBy, groupBy, labels = [], from, to, tables = [] }) {
+    const data = {
+      filter_by: filterBy,
+      group_by: groupBy,
+      labels:
+        Object.keys(labels).map(key => {
+          return {
+            key: key,
+            value: labels[key],
+          };
+        }) || [],
+      period_start_from: from,
+      period_start_to: to,
+      tables: tables,
+    };
+
+    return apiRequestQAN.post<any, any>('/ObjectDetails/GetQueryExample', data);
+  }
+}
+
+export default ExampleService;
---
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Metrics/Metrics.hooks.ts
@@ -0,0 +1,38 @@
+import { useContext, useEffect, useState } from 'react';
+import { PanelProvider } from '../../../../../panel/panel.provider';
+import MetricsService from './Metrics.service';
+import { processMetrics } from '../../../../../../react-plugins-deps/helpers/processMetrics';
+import { METRIC_CATALOGUE } from '../../../../../panel/panel.constants';
+
+export const useMetricsDetails = (): [any[], boolean] => {
+  const {
+    contextActions,
+    panelState: { queryId, groupBy, from, to, labels },
+  } = useContext(PanelProvider);
+  const [metrics, setMetrics] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const getMetrics = async () => {
+      try {
+        setLoading(true);
+        const result = await MetricsService.getMetrics({
+          filterBy: queryId,
+          groupBy,
+          from,
+          to,
+          labels,
+          // tables,
+        });
+        setMetrics(processMetrics(METRIC_CATALOGUE, result));
+        contextActions.setFingerprint(groupBy === 'queryid' ? result.fingerprint : queryId);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        //TODO: add error handling
+      }
+    };
+    getMetrics();
+  }, [queryId]);
+
+  return [metrics, loading];
+};
