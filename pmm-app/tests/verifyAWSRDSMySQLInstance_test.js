83d57c93c38bcc031f607922670e617db420877b
puneet0191 <puneet.0191@gmail.com>
2020-04-30 13:21:07 +0530

    PMM-5767 Minor fixes

--- pmm-app/tests/verifyAWSRDSMySQLInstance_test.js
+++ pmm-app/tests/verifyAWSRDSMySQLInstance_test.js
@@ -4,12 +4,11 @@ Before( async (I) => {
   I.Authorize();
 });
 
-Scenario(
-  'Verify Discovery and adding AWS RDS MySQL 5.6 instance for monitoring @not-pr-pipeline',
-  async (I, remoteInstancesPage, pmmInventoryPage) => {
-    const instanceIdToMonitor = 'rds-mysql56';
+Scenario('Verify Discovery and adding AWS RDS MySQL 5.6 instance for monitoring @not-pr-pipeline', async (I, remoteInstancesPage, pmmInventoryPage) => {
+    let instanceIdToMonitor = "rds-mysql56";
     I.amOnPage(remoteInstancesPage.url);
-    remoteInstancesPage.waitUntilNewRemoteInstancesPageLoaded().openAddAWSRDSMySQLPage();
+    remoteInstancesPage.waitUntilNewRemoteInstancesPageLoaded()
+        .openAddAWSRDSMySQLPage();
     remoteInstancesPage.discoverRDS();
     remoteInstancesPage.verifyInstanceIsDiscovered(instanceIdToMonitor);
     remoteInstancesPage.startMonitoringOfInstance(instanceIdToMonitor);
@@ -18,39 +17,30 @@ Scenario(
     remoteInstancesPage.createNewRemoteMySQL();
     pmmInventoryPage.verifyMySQLRemoteServiceIsDisplayed(instanceIdToMonitor);
     await pmmInventoryPage.verifyAgentHasStatusRunning(instanceIdToMonitor);
-  }
-);
+});
 
-Scenario(
-  'Verify AWS RDS MySQL 5.6 instance has status running @pmm-post-update @not-pr-pipeline',
-  async (I, remoteInstancesPage, pmmInventoryPage) => {
-    const serviceName = 'rds-mysql56';
+xScenario('Verify AWS RDS MySQL 5.6 instance has status running @pmm-post-update @not-pr-pipeline', async (I, remoteInstancesPage, pmmInventoryPage) => {
+    let serviceName = 'rds-mysql56';
     I.amOnPage(pmmInventoryPage.url);
     pmmInventoryPage.verifyMySQLRemoteServiceIsDisplayed(serviceName);
     await pmmInventoryPage.verifyAgentHasStatusRunning(serviceName);
-  }
-);
+});
 
-Scenario(
-  'Verify QAN Filters contain AWS RDS MySQL 5.6 after it was added for monitoring @not-pr-pipeline',
-  async (I, qanPage, adminPage) => {
-    const environment = 'RDS MySQL 5.6';
-    const filter = qanPage.getFilterLocator(environment);
+Scenario('Verify QAN Filters contain AWS RDS MySQL 5.6 after it was added for monitoring @not-pr-pipeline', async (I, qanPage, adminPage) => {
+    let environment = 'RDS MySQL 5.6';
+    let filter = qanPage.getFilterLocator(environment);
     I.amOnPage(qanPage.url);
     await I.waitForElement(qanPage.fields.iframe, 60);
-    adminPage.applyTimer('5m');
+    adminPage.applyTimer("5m");
     await I.switchTo(qanPage.fields.iframe);
     qanPage.waitForQANPageLoaded();
+    await qanPage.expandAllFilter();
     I.seeElement(filter);
-  }
-);
+});
 
-xScenario(
-  'Verify MySQL Instances Overview Dashboard for AWS RDS MySQL 5.6 data after it was added for monitoring @not-pr-pipeline',
-  async (I, remoteInstancesPage, dashboardPage) => {
+xScenario('Verify MySQL Instances Overview Dashboard for AWS RDS MySQL 5.6 data after it was added for monitoring', async (I, remoteInstancesPage, dashboardPage) => {
     I.amOnPage(remoteInstancesPage.dashboardMySQLOverviewWithFilters);
     dashboardPage.waitForDashboardOpened();
     await dashboardPage.expandEachDashboardRow();
     dashboardPage.verifyThereIsNoGraphsWithNA();
-  }
-);
+});
