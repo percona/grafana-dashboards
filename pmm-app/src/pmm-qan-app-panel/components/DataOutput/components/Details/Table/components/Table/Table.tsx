32c4164adb792493d39942450775d1a2c9bae19b
Roman Misyurin <roman.misyurin@percona.com>
2020-05-12 00:33:12 +0300

    PMM-5790 refactor tables tab structure

--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Table/Table.tsx
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Table/Table.tsx
@@ -4,52 +4,51 @@ import { DATABASE } from '../../../Details.constants';
 import { useActionResult } from '../../../Details.hooks';
 
 // TODO: refactor example parameters passing
+
+const getMysqlTablesData = async ({ example, tableName, setErrorText, setActionId }) => {
+  if (!tableName) {
+    setErrorText('Cannot display table info without query example, schema or table name at this moment.');
+    return;
+  }
+  const { action_id } = await TableService.getShowCreateTableMySQL({
+    table_name: tableName,
+    service_id: example.service_id,
+  });
+  setActionId(action_id as string);
+};
+
+const getPostgreSQLTablesData = async ({ example, tableName, setErrorText, setActionId }) => {
+  if (!tableName) {
+    setErrorText('Cannot display table info without query example, schema or table name at this moment.');
+    return;
+  }
+  const { action_id } = await TableService.getShowCreateTablePostgreSQL({
+    table_name: tableName,
+    service_id: example.service_id,
+  });
+  setActionId(action_id as string);
+};
+
+const getDatabaseTablesFactory = databaseType => ({ example, tableName, setErrorText, setActionId }) => {
+  switch (databaseType) {
+    case DATABASE.mysql:
+      return getMysqlTablesData({ example, tableName, setErrorText, setActionId });
+    case DATABASE.postgresql:
+      return getPostgreSQLTablesData({ example, tableName, setErrorText, setActionId });
+    default:
+      throw new Error('Unknown database type');
+  }
+};
+
 const TableCreate = props => {
   const { tableName, databaseType, example } = props;
   const [errorText, setErrorText] = useState('');
   const [showCreateTable, setActionId] = useActionResult();
-  const showCreateTableAction = example => {
-    setErrorText('');
-    switch (databaseType) {
-      case DATABASE.mysql:
-        if (!tableName) {
-          setErrorText(
-            'Cannot display table info without query example, schema or table name at this moment.'
-          );
-          return;
-        }
-        getMySQL(example);
-        break;
-      case DATABASE.postgresql:
-        if (!tableName) {
-          setErrorText(
-            'Cannot display table info without query example, schema or table name at this moment.'
-          );
-          return;
-        }
-        getPostgreSQL(example);
-        break;
-    }
-  };
-
-  const getMySQL = async example => {
-    const { action_id } = await TableService.getShowCreateTableMySQL({
-      table_name: tableName,
-      service_id: example.service_id,
-    });
-    setActionId(action_id as string);
-  };
-
-  const getPostgreSQL = async example => {
-    const { action_id } = await TableService.getShowCreateTablePostgreSQL({
-      table_name: tableName,
-      service_id: example.service_id,
-    });
-    setActionId(action_id as string);
-  };
 
   useEffect(() => {
-    showCreateTableAction(example);
+    setErrorText('');
+    const getDatabaseData = getDatabaseTablesFactory(databaseType);
+    getDatabaseData({ example, tableName, setErrorText, setActionId });
   }, [databaseType]);
 
   return <div>{errorText ? <pre>{errorText}</pre> : <pre>{showCreateTable}</pre>}</div>;
