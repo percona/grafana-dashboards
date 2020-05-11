bbe5f0d87ec3afb2f68d428b874bf13fc2aa2369
Roman Misyurin <roman.misyurin@percona.com>
2020-05-12 00:33:12 +0300

    PMM-5790 refactor tables tab structure

--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Indexes/Indexes.tsx
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Indexes/Indexes.tsx
@@ -5,53 +5,52 @@ import { Table } from 'antd';
 import { useActionResult } from '../../../Details.hooks';
 import { processTableData } from '../../../Details.tools';
 
+const getMysqlIndexesData = async ({ example, tableName, setErrorText, setActionId }) => {
+  if (!tableName) {
+    setErrorText('Cannot display indexes info without query example, schema or table name at this moment.');
+    return;
+  }
+  const { action_id } = await IndexesService.getMysqlIndex({
+    database: example.schema,
+    table_name: tableName,
+    service_id: example.service_id,
+  });
+  setActionId(action_id as string);
+};
+
+const getPostgreSQLIndexesData = async ({ example, tableName, setErrorText, setActionId }) => {
+  if (!tableName) {
+    setErrorText('Cannot display indexes info without table name at this moment.');
+    return;
+  }
+  const { action_id } = await IndexesService.getPostgreSQLIndex({
+    table_name: tableName,
+    service_id: example.service_id,
+  });
+  setActionId(action_id as string);
+};
+
+const getDatabaseIndexesFactory = databaseType => ({ example, tableName, setErrorText, setActionId }) => {
+  switch (databaseType) {
+    case DATABASE.mysql:
+      return getMysqlIndexesData({ example, tableName, setErrorText, setActionId });
+    case DATABASE.postgresql:
+      return getPostgreSQLIndexesData({ example, tableName, setErrorText, setActionId });
+    default:
+      throw new Error('Unknown database type');
+  }
+};
+
 export const Indexes = props => {
   const { tableName, databaseType, example } = props;
   const [data, setData] = useState({ columns: [], rows: [] });
   const [errorText, setErrorText] = useState('');
   const [indexes, setActionId] = useActionResult();
 
-  const getIndexes = example => {
-    setErrorText('');
-    switch (databaseType) {
-      case DATABASE.mysql:
-        if (!tableName) {
-          setErrorText(
-            'Cannot display indexes info without query example, schema or table name at this moment.'
-          );
-          return;
-        }
-        getMySQL(example);
-        break;
-      case DATABASE.postgresql:
-        if (!tableName) {
-          setErrorText('Cannot display indexes info without table name at this moment.');
-          return;
-        }
-        getPostgreSQL(example);
-        break;
-    }
-  };
-
-  const getMySQL = async example => {
-    const { action_id } = await IndexesService.getMysqlIndex({
-      database: example.schema,
-      table_name: tableName,
-      service_id: example.service_id,
-    });
-    setActionId(action_id as string);
-  };
-
-  const getPostgreSQL = async example => {
-    const { action_id } = await IndexesService.getPostgreSQLIndex({
-      table_name: tableName,
-      service_id: example.service_id,
-    });
-    setActionId(action_id as string);
-  };
-
   useEffect(() => {
-    getIndexes(example);
+    setErrorText('');
+    const getDatabaseData = getDatabaseIndexesFactory(databaseType);
+    getDatabaseData({ example, tableName, setErrorText, setActionId });
   }, [databaseType]);
 
   useEffect(() => {
--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Status/Status.tsx
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Status/Status.tsx
@@ -5,38 +5,38 @@ import { Table } from 'antd';
 import { useActionResult } from '../../../Details.hooks';
 import { processTableData } from '../../../Details.tools';
 
+const getMysqlStatusesData = async ({ example, tableName, setErrorText, setActionId }) => {
+  if (!tableName) {
+    setErrorText('Cannot display status info without query example, schema or table name at this moment.');
+    return;
+  }
+  const { action_id } = await StatusService.getMysqlTableStatus({
+    database: example.schema,
+    table_name: tableName,
+    service_id: example.service_id,
+  });
+  setActionId(action_id as string);
+};
+
+const getDatabaseStatusesFactory = databaseType => ({ example, tableName, setErrorText, setActionId }) => {
+  switch (databaseType) {
+    case DATABASE.mysql:
+      return getMysqlStatusesData({ example, tableName, setErrorText, setActionId });
+    default:
+      throw new Error('Unknown database type');
+  }
+};
+
 export const Status = props => {
   const { tableName, databaseType, example } = props;
   const [data, setData] = useState({ columns: [], rows: [] });
   const [errorText, setErrorText] = useState('');
   const [status, setActionId] = useActionResult();
 
-  const getstatus = example => {
-    setErrorText('');
-    switch (databaseType) {
-      case DATABASE.mysql:
-        if (!tableName) {
-          setErrorText(
-            'Cannot display status info without query example, schema or table name at this moment.'
-          );
-          return;
-        }
-        getMySQL(example);
-        break;
-    }
-  };
-
-  const getMySQL = async example => {
-    const { action_id } = await StatusService.getMysqlTableStatus({
-      database: example.schema,
-      table_name: tableName,
-      service_id: example.service_id,
-    });
-    setActionId(action_id as string);
-  };
-
   useEffect(() => {
-    getstatus(example);
+    setErrorText('');
+    const getStatusesData = getDatabaseStatusesFactory(databaseType);
+    getStatusesData({ example, tableName, setErrorText, setActionId });
   }, [databaseType]);
 
   useEffect(() => {
