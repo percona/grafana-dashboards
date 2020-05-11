6c0af9d9485ea59f6c01296c3a0e1756184012f1
Roman Misyurin <roman.misyurin@percona.com>
2020-05-12 00:48:33 +0300

    PMM-5790 move indexes to factory

--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Indexes/Indexes.tsx
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Table/components/Indexes/Indexes.tsx
@@ -1,45 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { DATABASE } from '../../../Details.constants';
-import IndexesService from './Indexes.service';
 import { Table } from 'antd';
 import { useActionResult } from '../../../Details.hooks';
 import { processTableData } from '../../../Details.tools';
-
-const getMysqlIndexesData = async ({ example, tableName, setErrorText, setActionId }) => {
-  if (!tableName) {
-    setErrorText('Cannot display indexes info without query example, schema or table name at this moment.');
-    return;
-  }
-  const { action_id } = await IndexesService.getMysqlIndex({
-    database: example.schema,
-    table_name: tableName,
-    service_id: example.service_id,
-  });
-  setActionId(action_id as string);
-};
-
-const getPostgreSQLIndexesData = async ({ example, tableName, setErrorText, setActionId }) => {
-  if (!tableName) {
-    setErrorText('Cannot display indexes info without table name at this moment.');
-    return;
-  }
-  const { action_id } = await IndexesService.getPostgreSQLIndex({
-    table_name: tableName,
-    service_id: example.service_id,
-  });
-  setActionId(action_id as string);
-};
-
-const getDatabaseIndexesFactory = databaseType => ({ example, tableName, setErrorText, setActionId }) => {
-  switch (databaseType) {
-    case DATABASE.mysql:
-      return getMysqlIndexesData({ example, tableName, setErrorText, setActionId });
-    case DATABASE.postgresql:
-      return getPostgreSQLIndexesData({ example, tableName, setErrorText, setActionId });
-    default:
-      throw new Error('Unknown database type');
-  }
-};
+import { databaseFactory } from '../../database-models';
 
 export const Indexes = props => {
   const { tableName, databaseType, example } = props;
@@ -49,8 +12,8 @@ export const Indexes = props => {
 
   useEffect(() => {
     setErrorText('');
-    const getDatabaseData = getDatabaseIndexesFactory(databaseType);
-    getDatabaseData({ example, tableName, setErrorText, setActionId });
+    const database = databaseFactory(databaseType);
+    database.getIndexes({ example, tableName, setErrorText, setActionId });
   }, [databaseType]);
 
   useEffect(() => {
