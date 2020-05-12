38b10404bb68b5be6437d47e7e9250e517a52fb9
Roman Misyurin <roman.misyurin@percona.com>
2020-05-12 20:29:06 +0300

    Revert "PMM-5727 replaced Filters api with generated"
    This reverts commit f0a416bc

--- pmm-app/src/pmm-qan-app-panel/components/Filters/Filters.service.ts
+++ pmm-app/src/pmm-qan-app-panel/components/Filters/Filters.service.ts
@@ -1,12 +1,23 @@
-import Api from 'typescript-api';
+import { apiRequestQAN } from '../../../react-plugins-deps/helpers/api';
 
-const filtersApi = new Api.FiltersApi();
+class FiltersService {
+  static async getQueryOverviewFiltersList(paramLabels, from, to, mainMetric) {
+    const requestLabels = Object.keys(paramLabels).map(key => {
+      return {
+        key: key,
+        value: paramLabels[key],
+      };
+    });
 
-const setCheckedLabels = (paramLabels, labels: any) => {
-  const result = Object.assign(labels);
-  if (labels) {
-    Object.keys(result).forEach(label => {
-      result[label].name.forEach(metric => {
+    const { labels } = await apiRequestQAN.post<any, any>('/Filters/Get', {
+      labels: requestLabels,
+      main_metric_name: mainMetric,
+      period_start_from: from,
+      period_start_to: to,
+    });
+
+    Object.keys(labels).forEach(label => {
+      labels[label].name.forEach(metric => {
         const passedVariables = paramLabels[label];
         metric.checked =
           passedVariables &&
@@ -18,29 +29,8 @@ const setCheckedLabels = (paramLabels, labels: any) => {
           });
       });
     });
-  }
-  return result;
-};
 
-class FiltersService {
-  static async getQueryOverviewFiltersList(paramLabels, from, to, mainMetric) {
-    const requestLabels = Object.keys(paramLabels).map(key => {
-      return {
-        key: key,
-        value: paramLabels[key],
-      };
-    });
-
-    const { labels } = await filtersApi.get({
-      body: {
-        labels: requestLabels,
-        main_metric_name: mainMetric,
-        period_start_from: from,
-        period_start_to: to,
-      },
-    });
-
-    return setCheckedLabels(paramLabels, labels);
+    return labels;
   }
 }
 
