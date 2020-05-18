646730c62d345f83f12058656797e2add58fca41
Alexander Tymchuk <alexander.tymchuk@percona.com>
2020-05-18 13:00:05 +0300

    PMM-5936 fix the checks panel url

--- pmm-app/src/pmm-check-home/CheckPanel.constants.ts
+++ pmm-app/src/pmm-check-home/CheckPanel.constants.ts
@@ -1,7 +1,7 @@
 import { CheckPanelOptions, SeverityMap } from 'pmm-check/types';
 
 export const PMM_SETTINGS_URL = '/graph/d/pmm-settings/pmm-settings';
-export const PMM_DATABASE_CHECKS_PANEL_URL = 'graph/d/pmm-checks/pmm-database-checks';
+export const PMM_DATABASE_CHECKS_PANEL_URL = '/graph/d/pmm-checks/pmm-database-checks';
 
 export const DEFAULTS: CheckPanelOptions = {
   title: 'Failed Database Checks',
