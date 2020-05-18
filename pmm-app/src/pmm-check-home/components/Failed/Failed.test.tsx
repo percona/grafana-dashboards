024445df64649117e408458a680ad2945728e647
Alexander Tymchuk <alexander.tymchuk@percona.com>
2020-05-18 12:11:24 +0300

    PMM-5936 fixed the failing unit test

--- pmm-app/src/pmm-check-home/components/Failed/Failed.test.tsx
+++ pmm-app/src/pmm-check-home/components/Failed/Failed.test.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Icon, Tooltip } from '@grafana/ui';
+import { Tooltip } from '@grafana/ui';
 import { Failed } from './Failed';
 
 describe('Failed::', () => {
   it('should render a sum of total failed checks with severity details', () => {
     const root = shallow(<Failed failed={[1, 0, 1]} isSttEnabled />);
 
-    expect(
-      root
-        .find('div > span')
-        .at(0)
-        .text()
-    ).toEqual('1 / 0 / 1');
+    const spans = root.find('div a > span');
+    expect(spans.at(0).text()).toEqual('1');
+    expect(spans.at(2).text()).toEqual('0');
+    expect(spans.at(4).text()).toEqual('1');
   });
 
-  it('should render inner components', () => {
+  it('should render an inner tooltip component', () => {
     const root = shallow(<Failed failed={[1, 0, 1]} isSttEnabled />);
 
-    expect(root.find(Icon).length).toEqual(1);
     expect(root.find(Tooltip).length).toEqual(1);
   });
 });
