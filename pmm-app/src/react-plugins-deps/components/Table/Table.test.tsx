2bd434f16890573b1aede5289428a02f1395897f
Roman Misyurin <roman.misyurin@percona.com>
2020-05-25 20:49:58 +0300

    PMM-544 fix test

--- pmm-app/src/react-plugins-deps/components/Table/Table.test.tsx
+++ pmm-app/src/react-plugins-deps/components/Table/Table.test.tsx
@@ -42,7 +42,7 @@ describe('Table', () => {
     const ActionPanel = ({ selected }) => <div data-qa="action-panel"></div>;
     const root = mount(<Table columns={columns} data={rows} ActionPanel={ActionPanel} />);
     expect(root.find('[data-qa="select-all"]').length).toEqual(1);
-    expect(root.find('[data-qa="select-row"]').length).toEqual(rows.length + 1);
+    expect(root.find('[data-qa="select-row"]').length).toEqual(rows.length);
   });
 
   it('Render custom no data section if its passed', () => {
