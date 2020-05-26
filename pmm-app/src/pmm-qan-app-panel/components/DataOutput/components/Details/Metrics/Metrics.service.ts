3d6974263d74fbee9e1ebbe9a959463bca8da7d7
Roman Misyurin <roman.misyurin@percona.com>
2020-05-27 00:49:05 +0300

    PMM-5999 filters state fix

--- pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Metrics/Metrics.service.ts
+++ pmm-app/src/pmm-qan-app-panel/components/DataOutput/components/Details/Metrics/Metrics.service.ts
@@ -6,12 +6,14 @@ class MetricsService {
       filter_by: filterBy || '',
       group_by: groupBy,
       labels:
-        Object.keys(labels).map(key => {
-          return {
-            key: key,
-            value: labels[key],
-          };
-        }) || [],
+        Object.keys(labels)
+          .map(key => {
+            return {
+              key: key,
+              value: labels[key],
+            };
+          })
+          .filter(item => item.value.filter(element => element !== 'All').length) || [],
       period_start_from: from,
       period_start_to: to,
       tables: tables,
