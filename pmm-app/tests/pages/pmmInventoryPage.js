ca687b6b29533e379eb379a01520f1992dba0254
Roman Misyurin <roman.misyurin@percona.com>
2020-06-04 20:59:24 +0300

    PMM-544 tabs links fix

--- pmm-app/tests/pages/pmmInventoryPage.js
+++ pmm-app/tests/pages/pmmInventoryPage.js
@@ -5,17 +5,17 @@ module.exports = {
   // setting locators
   url: 'graph/d/pmm-inventory/pmm-inventory?orgId=1',
   fields: {
-    iframe: "//div[@class='panel-content']//iframe",
+    iframe: '//div[@class="panel-content"]//iframe',
     inventoryTable: '//table',
     inventoryTableRows: '//table//tr',
     inventoryTableColumn: '//table//td',
-    agentsLink: "//div[@role='tab'][contains(text(),'Agents')]",
-    nodesLink: "//div[@role='tab'][contains(text(),'Nodes')]",
-    agentsLinkOld: "//a[contains(text(), 'Agents')]",
-    nodesLinkOld: "//a[contains(text(), 'Nodes')]",
-    pmmAgentLocator: "//table//td[contains(text(), 'PMM Agent')]",
-    serviceIdLocatorPrefix: "//table//tr/td[3][contains(text(),'",
-    pmmServicesSelector: "//div[@role='tab'][contains(text(),'Services')]",
+    agentsLink: '//div[@class="inventory-wrapper"]//li[contains(text(),"Agents")]',
+    nodesLink: '//div[@class="inventory-wrapper"]//li[contains(text(),"Nodes")]',
+    pmmServicesSelector: '//div[@class="inventory-wrapper"]//li[contains(text(),"Services")]',
+    agentsLinkOld: '//a[contains(text(), "Agents")]',
+    nodesLinkOld: '//a[contains(text(), "Nodes")]',
+    pmmAgentLocator: '//table//td[contains(text(), "PMM Agent")]',
+    serviceIdLocatorPrefix: '//table//tr/td[3][contains(text(),"',
   },
 
   verifyOldMySQLRemoteServiceIsDisplayed(serviceName) {
@@ -39,9 +39,7 @@ module.exports = {
     I.waitForElement(this.fields.pmmAgentLocator, 60);
     I.waitForElement(this.fields.inventoryTable, 60);
     const numberOfServices = await I.grabNumberOfVisibleElements(
-      "//span[contains(text(), '" +
-        serviceId +
-        "')]/following-sibling::span[contains(text(),'status: RUNNING')]"
+      `//span[contains(text(), "${serviceId}")]/following-sibling::span[contains(text(),"status: RUNNING")]`
     );
     if (
       service_name === 'rds-mysql56' ||
@@ -49,9 +47,12 @@ module.exports = {
       service_name === 'postgresql_remote_new' ||
       service_name === 'mysql_remote_new'
     ) {
-      I.waitForVisible("//span[contains(text(), '" +
+      I.waitForVisible(
+        '//span[contains(text(), "' +
           serviceId +
-          "')]/following-sibling::span[contains(text(),'status: RUNNING')]", 30);
+          '")]/following-sibling::span[contains(text(),"status: RUNNING")]',
+        30
+      );
       assert.equal(
         numberOfServices,
         2,
@@ -69,26 +70,22 @@ module.exports = {
     I.click(servicesLink);
     const nodeId = await this.getNodeId(serviceName);
     I.click(agentLinkLocator);
-    let flagExists =
-      "//span[contains(text(), '" +
-      nodeId +
-      "')]/following-sibling::span[contains(text(),'enhanced_metrics_disabled: true')]";
+    // eslint-disable-next-line max-len
+    let flagExists = `//span[contains(text(), "${nodeId}")]/following-sibling::span[contains(text(),"enhanced_metrics_disabled: true")]`;
     I.seeElement(flagExists);
-    flagExists =
-      "//span[contains(text(), '" +
-      nodeId +
-      "')]/following-sibling::span[contains(text(),'basic_metrics_disabled: true')]";
+    // eslint-disable-next-line max-len
+    flagExists = `//span[contains(text(), "${nodeId}")]/following-sibling::span[contains(text(),"basic_metrics_disabled: true")]`;
     I.seeElement(flagExists);
   },
 
   async getNodeId(serviceName) {
-    const nodeIdLocator = this.fields.serviceIdLocatorPrefix + serviceName + "')]/following-sibling::td[1]";
+    const nodeIdLocator = this.fields.serviceIdLocatorPrefix + serviceName + '")]/following-sibling::td[1]';
     return await I.grabTextFrom(nodeIdLocator);
   },
 
   async getServiceId(serviceName) {
     const serviceIdLocator =
-      this.fields.serviceIdLocatorPrefix + serviceName + "')]/preceding-sibling::td[2]";
+      this.fields.serviceIdLocatorPrefix + serviceName + '")]/preceding-sibling::td[2]';
     const matchedServices = await I.grabNumberOfVisibleElements(serviceIdLocator);
     await assert.equal(
       matchedServices,
