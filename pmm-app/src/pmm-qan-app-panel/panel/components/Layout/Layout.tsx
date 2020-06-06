b80986a9e2f3bf2a2dcd6fb87c074dfc2cad3d62
Roman Misyurin <roman.misyurin@percona.com>
2020-06-06 14:40:43 +0300

    PMM-6077 Change structure

--- pmm-app/src/pmm-qan-app-panel/panel/components/Layout/Layout.tsx
+++ pmm-app/src/pmm-qan-app-panel/panel/components/Layout/Layout.tsx
@@ -1,36 +1,17 @@
-import React, {
-  useCallback, useContext, useEffect, useState,
-} from 'react';
-import { Pagination } from 'antd';
+import React, { useContext, useEffect, useState } from 'react';
 import SplitPane from 'react-split-pane';
-import OverviewTable from '../Overview/OverviewTable';
+import { Overview } from '../Overview';
 import Details from '../Details/Details';
 import { PanelProvider } from 'pmm-qan-app-panel/panel/panel.provider';
 import ManageColumns from '../Overview/components/ManageColumns/ManageColumns';
 import { styles } from './Layout.styles';
-import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE, PAGE_SIZE_OPTIONS } from './Layout.constants';
 import './Layout.scss';
+import FiltersContainer from '../Filters/Filters';
 
 const Layout = () => {
   const {
-    contextActions,
-    panelState: { pageNumber, pageSize, querySelected },
+    panelState: { querySelected },
   } = useContext(PanelProvider);
-  const [total, setTotal] = useState(30);
-  const [showTotal, setShowTotal] = useState('');
-  const changePageNumber = useCallback((page) => {
-    contextActions.changePage(page);
-  }, []);
-
-  const changePageSize = useCallback((current, size) => {
-    contextActions.changePageSize(size);
-  }, []);
-
-  const renderShowTotal = useCallback((totalAmount, range) => {
-    setShowTotal(`${range[0]}-${range[1]} of ${totalAmount} items`);
-    return null;
-  }, []);
-
   // TODO: replace with something more elegant & fast
   const container = document.querySelector('#antd');
   const size = container && container.clientWidth;
@@ -42,46 +23,34 @@ const Layout = () => {
   }, [querySelected]);
 
   return (
-    <div className={styles.getContainerWrapper(size)}>
-      <div className={styles.overviewHeader}>
-        <div className={styles.manageColumnsWrapper}>
-          <ManageColumns onlyAdd />
-        </div>
+    <div className="query-analytics-grid" id="antd">
+      <div className="overview-filters" id="query-analytics-filters">
+        <FiltersContainer />
       </div>
-      <div style={{ position: 'relative' }} className={styles.splitterWrapper}>
-        <SplitPane
-          split="horizontal"
-          onDragFinished={() => setReload({})}
-          className={styles.splitterWrapper}
-          resizerStyle={{ display: querySelected ? '' : 'none' }}
-          pane1Style={{
-            minHeight: querySelected ? '20%' : '100%',
-            maxHeight: querySelected ? '60%' : '100%',
-          }}
-          pane2Style={{ minHeight: '20%', overflowY: 'scroll', zIndex: 999 }}
-        >
-          <div className="table-wrapper" style={{ width: '100%' }}>
-            <OverviewTable setTotal={setTotal} reload={reload} />
-            <div className={styles.overviewHeader}>
-              <div className={styles.paginationWrapper}>
-                <Pagination
-                  showSizeChanger
-                  pageSizeOptions={PAGE_SIZE_OPTIONS}
-                  defaultCurrent={DEFAULT_PAGE_NUMBER}
-                  defaultPageSize={DEFAULT_PAGE_SIZE}
-                  showTotal={renderShowTotal}
-                  current={pageNumber}
-                  pageSize={pageSize}
-                  total={total}
-                  onShowSizeChange={changePageSize}
-                  onChange={changePageNumber}
-                />
-                <span className={styles.showTotal}>{showTotal}</span>
-              </div>
+      <div id="query-analytics-data">
+        <div className={styles.getContainerWrapper(size)}>
+          <div className={styles.overviewHeader}>
+            <div className={styles.manageColumnsWrapper}>
+              <ManageColumns onlyAdd />
             </div>
           </div>
-          <div>{querySelected ? <Details /> : null}</div>
-        </SplitPane>
+          <div style={{ position: 'relative' }} className={styles.splitterWrapper}>
+            <SplitPane
+              split="horizontal"
+              onDragFinished={() => setReload({})}
+              className={styles.splitterWrapper}
+              resizerStyle={{ display: querySelected ? '' : 'none' }}
+              pane1Style={{
+                minHeight: querySelected ? '20%' : '100%',
+                maxHeight: querySelected ? '60%' : '100%',
+              }}
+              pane2Style={{ minHeight: '20%', overflowY: 'scroll', zIndex: 999 }}
+            >
+              <Overview reload={reload} />
+              <div>{querySelected ? <Details /> : null}</div>
+            </SplitPane>
+          </div>
+        </div>
       </div>
     </div>
   );
--- pmm-app/src/pmm-qan-app-panel/panel/panel.tsx
+++ pmm-app/src/pmm-qan-app-panel/panel/panel.tsx
@@ -4,22 +4,13 @@ import 'react-plugins-deps/styles.scss';
 import 'react-plugins-deps/style.less';
 import styles from 'react-plugins-deps/components/helpers/styling';
 import { UrlParametersProvider } from './panel.provider';
-
-import FiltersContainer from './components/Filters/Filters';
 import Layout from './components/Layout/Layout';
 
 const QueryAnalyticsPanel = (props) => {
   useEffect(() => styles.addPluginPanelClass(), []);
   return (
     <UrlParametersProvider grafanaProps={props}>
-      <div className="query-analytics-grid" id="antd">
-        <div className="overview-filters" id="query-analytics-filters">
-          <FiltersContainer />
-        </div>
-        <div id="query-analytics-data">
-          <Layout />
-        </div>
-      </div>
+      <Layout />
     </UrlParametersProvider>
   );
 };
