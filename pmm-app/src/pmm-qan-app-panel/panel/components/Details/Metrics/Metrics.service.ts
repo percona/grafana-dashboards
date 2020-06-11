6797081d01700f8236e3b342f3dae00f2602c9cb
Roman Misyurin <roman.misyurin@percona.com>
2020-06-12 01:09:53 +0300

    PMM-6077 classes and interfaces improvements

--- pmm-app/src/pmm-qan-app-panel/panel/components/Details/Metrics/Metrics.service.ts
+++ pmm-app/src/pmm-qan-app-panel/panel/components/Details/Metrics/Metrics.service.ts
@@ -1,22 +1,24 @@
 import { apiRequestQAN } from 'shared/components/helpers/api';
 import { getLabelQueryParams } from 'pmm-qan-app-panel/panel/panel.tools';
 
-class MetricsService {
-  static getMetrics({
-    filterBy, groupBy, labels = [], from, to, tables = [], totals,
-  }) {
-    const body = {
-      filter_by: filterBy || '',
-      group_by: groupBy,
-      labels: getLabelQueryParams(labels),
-      period_start_from: from,
-      period_start_to: to,
-      tables,
-      totals,
-    };
 
-    return apiRequestQAN.post<any, any>('/ObjectDetails/GetMetrics', body);
-  }
-}
+export const getMetrics = async ({
+  filterBy, groupBy, labels = [], from, to, tables = [], totals,
+}) => {
+  const body = {
+    filter_by: filterBy || '',
+    group_by: groupBy,
+    labels: getLabelQueryParams(labels),
+    period_start_from: from,
+    period_start_to: to,
+    tables,
+    totals,
+  };
 
-export default MetricsService;
+  return apiRequestQAN.post<any, any>('/ObjectDetails/GetMetrics', body);
+};
+
+
+export default {
+  getMetrics
+};
--- pmm-app/src/pmm-qan-app-panel/panel/components/Details/database-models/mysql/service.ts
+++ pmm-app/src/pmm-qan-app-panel/panel/components/Details/database-models/mysql/service.ts
@@ -1,23 +1,23 @@
 import { apiRequestManagement } from 'shared/components/helpers/api';
 
-export default class MysqlDatabaseService {
-  static getShowCreateTableMySQL(body) {
+export default {
+  getShowCreateTableMySQL(body) {
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLShowCreateTable', body);
-  }
+  },
 
-  static getMysqlTableStatus(body) {
+  getMysqlTableStatus(body) {
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLShowTableStatus', body);
-  }
+  },
 
-  static getMysqlIndex(body) {
+  getMysqlIndex(body) {
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLShowIndex', body);
-  }
+  },
 
-  static getTraditionalExplainJSONMysql(body) {
+  getTraditionalExplainJSONMysql(body) {
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLExplainJSON', body);
-  }
+  },
 
-  static getTraditionalExplainMysql(body) {
+  getTraditionalExplainMysql(body) {
     return apiRequestManagement.post<any, any>('/Actions/StartMySQLExplain', body);
-  }
-}
+  },
+};
