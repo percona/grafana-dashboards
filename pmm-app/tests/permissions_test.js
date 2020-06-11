8a4ec4cc55f4b4bb734250d0d4b98267b86f7d59
yurkovychv <vasyl.yurkovych@percona.com>
2020-06-11 11:20:23 +0300

    SAAS-155 - expanded scenarios, checking permissions for user with updated admin role

--- pmm-app/tests/permissions_test.js
+++ pmm-app/tests/permissions_test.js
@@ -1,60 +1,117 @@
 Feature('PMM Permission restrictions');
-const viewerUser = 'test_viewer';
-const viewerPassword = 'password';
-let user;
+let viewer;
+let admin;
+const users = {
+  viewer: {
+    username: 'test_viewer',
+    password: 'password',
+  },
+  admin: {
+    username: 'test_admin',
+    password: 'password',
+  },
+};
 
 BeforeSuite(async (I) => {
-      I.say('Creating users for the test suite');
-      const userId = await I.createUser(viewerUser, viewerPassword);
-      await I.setRole(userId);
-      user = userId;
+  I.say('Creating users for the permissions test suite');
+  const viewerId = await I.createUser(users.viewer.username, users.viewer.password);
+  const adminId = await I.createUser(users.admin.username, users.admin.password);
+  await I.setRole(viewerId);
+  await I.setRole(adminId, 'Admin');
+  viewer = viewerId;
+  admin = adminId;
 });
 
-AfterSuite(async(I) => {
-      I.say('Removing users');
-      await I.deleteUser(user);
+AfterSuite(async (I) => {
+  I.say('Removing users');
+  await I.deleteUser(viewer);
+  await I.deleteUser(admin);
 });
 
 Scenario(
-    'Verify Failed checks panel at Home page for a viewer role (STT is enabled)',
+    'PMM-T358 Verify Failed checks panel at Home page for the viewer role (STT is enabled)',
     async (I, homePage, settingsAPI) => {
       await settingsAPI.apiEnableSTT();
-      await I.Authorize(viewerUser, viewerPassword);
+      await I.Authorize(users.viewer.username, users.viewer.password);
       I.amOnPage(homePage.url);
-      I.waitForVisible(homePage.fields.noAccessRightsSelector, 30);
+      I.waitForVisible(homePage.fields.checksPanelSelector, 30);
       I.see('Insufficient access rights.', homePage.fields.noAccessRightsSelector);
     }
 );
 
 Scenario(
-    'Verify Database Failed checks page for a viewer role (STT is enabled)',
-    async (I, databaseChecksPage, settingsAPI) => {
-          await settingsAPI.apiEnableSTT();
-          await I.Authorize(viewerUser, viewerPassword);
-          I.amOnPage(databaseChecksPage.url);
-          I.waitForVisible(databaseChecksPage.fields.noAccessRightsSelector, 30);
-          I.see('Insufficient access rights.', databaseChecksPage.fields.noAccessRightsSelector);
+    'PMM-T360 Verify Failed checks panel at Home page for the admin role (STT is enabled)',
+    async (I, homePage, settingsAPI) => {
+      await settingsAPI.apiEnableSTT();
+      await I.Authorize(users.admin.username, users.admin.password);
+      I.amOnPage(homePage.url);
+      I.waitForVisible(homePage.fields.checksPanelSelector, 30);
+      I.dontSeeElement(homePage.fields.noAccessRightsSelector);
     }
 );
 
 Scenario(
-    'Verify Failed checks panel at Home page for a viewer role (STT is disabled)',
+    'PMM-T358 Verify Database Failed checks page for the viewer role (STT is enabled)',
+    async (I, databaseChecksPage, settingsAPI) => {
+      await settingsAPI.apiEnableSTT();
+      await I.Authorize(users.viewer.username, users.viewer.password);
+      I.amOnPage(databaseChecksPage.url);
+      I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
+      I.see('Insufficient access rights.', databaseChecksPage.fields.noAccessRightsSelector);
+    }
+);
+
+Scenario(
+    'PMM-T360 Verify Database Failed checks page for the admin role (STT is enabled)',
+    async (I, databaseChecksPage, settingsAPI) => {
+      await settingsAPI.apiEnableSTT();
+      await I.Authorize(users.admin.username, users.admin.password);
+      I.amOnPage(databaseChecksPage.url);
+      I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
+      I.dontSeeElement(databaseChecksPage.fields.noAccessRightsSelector);
+    }
+);
+
+Scenario(
+    'PMM-T358 Verify Failed checks panel at Home page for the viewer role (STT is disabled)',
     async (I, homePage, settingsAPI) => {
       await settingsAPI.apiDisableSTT();
-      await I.Authorize(viewerUser, viewerPassword);
+      await I.Authorize(users.viewer.username, users.viewer.password);
       I.amOnPage(homePage.url);
-      I.waitForVisible(homePage.fields.noAccessRightsSelector, 30);
+      I.waitForVisible(homePage.fields.checksPanelSelector, 30);
       I.see('Insufficient access rights.', homePage.fields.noAccessRightsSelector);
     }
 );
 
 Scenario(
-    'Verify Database Failed checks page for a viewer role (STT is disabled)',
+    'PMM-T360 Verify Failed checks panel at Home page for the admin role (STT is disabled)',
+    async (I, homePage, settingsAPI) => {
+      await settingsAPI.apiDisableSTT();
+      await I.Authorize(users.admin.username, users.admin.password);
+      I.amOnPage(homePage.url);
+      I.waitForVisible(homePage.fields.checksPanelSelector, 30);
+      I.dontSeeElement(homePage.fields.noAccessRightsSelector);
+    }
+);
+
+Scenario(
+    'PMM-T358 Verify Database Failed checks page for the viewer role (STT is disabled)',
     async (I, databaseChecksPage, settingsAPI) => {
       await settingsAPI.apiDisableSTT();
-      await I.Authorize(viewerUser, viewerPassword);
+      await I.Authorize(users.viewer.username, users.viewer.password);
       I.amOnPage(databaseChecksPage.url);
-      I.waitForVisible(databaseChecksPage.fields.noAccessRightsSelector, 30);
+      I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
       I.see('Insufficient access rights.', databaseChecksPage.fields.noAccessRightsSelector);
+    }
+);
+
+Scenario(
+    'PMM-T360 Verify Database Failed checks page for the admin role (STT is disabled)',
+    async (I, databaseChecksPage, settingsAPI) => {
+      await settingsAPI.apiDisableSTT();
+      await I.Authorize(users.admin.username, users.admin.password);
+      I.amOnPage(databaseChecksPage.url);
+      I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
+      I.dontSeeElement(databaseChecksPage.fields.noAccessRightsSelector);
     }
 );
