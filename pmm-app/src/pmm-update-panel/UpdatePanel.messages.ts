6d9c09eb7d090b4acc675489bc7f8a6a8f2d0806
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-12 11:35:14 +0200

    PMM-6091 Add pmm-app/src/pmm-update-panel/UpdatePanel.messages.ts

---
+++ pmm-app/src/pmm-update-panel/UpdatePanel.messages.ts
@@ -0,0 +1,11 @@
+export const Messages = {
+  panelTitle: 'Updated',
+  noUpdates: 'No updates are available',
+  updatesNotice: 'PMM checks for updates once a day',
+  upToDate: 'You are up to date',
+  updateTo: 'Update to',
+  availableVersion: 'Available version',
+  whatsNew: "What's new",
+  lastCheck: 'Last check',
+  nothingToUpdate: 'Nothing to update',
+};
