828ad0de7d51031dd67d42aa8a73858f040063d9
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-15 21:55:26 +0200

    PMM-6091 Update pmm-update-panel/module.ts

--- pmm-app/src/pmm-update-panel/module.ts
+++ pmm-app/src/pmm-update-panel/module.ts
@@ -1,14 +1,4 @@
-// import { DEFAULTS } from './CheckPanel.constants';
-// import { UpdatePanelOptions } from './types';
-// import { UpdatePanelRouter } from './UpdatePanel';
-
-// export const plugin = new PanelPlugin<UpdatePanelOptions>(UpdatePanelRouter)
-//   .setDefaults(DEFAULTS)
-//   .setEditor(UpdatePanelEditor);
-
 import { PanelPlugin } from '@grafana/data';
-import '../react-plugins-deps/styles.scss';
-import '../react-plugins-deps/style.less';
 import { UpdatePanel } from './UpdatePanel';
 
 export const plugin = new PanelPlugin(UpdatePanel);
