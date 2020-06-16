5321209080f2b9c08e84f2c69e9383ef32252da9
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-16 14:47:07 +0200

    PMM-6091 Remove unused UpdateModalBackdrop

--- pmm-app/src/pmm-update/components/UpdateModalBackdrop/UpdateModalBackdrop.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
-
-import { Messages } from './UpdateModal.messages';
-import * as styles from './UpdateModal.styles';
-import { useClickOutside } from 'pmm-update-panel/hooks';
