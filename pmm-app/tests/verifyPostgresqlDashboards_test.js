58abd957ec17054ab814003eea05473841fb7cd2
puneet0191 <puneet.0191@gmail.com>
2020-06-19 14:02:35 +0530

    PMM-6140

--- pmm-app/tests/verifyPostgresqlDashboards_test.js
+++ pmm-app/tests/verifyPostgresqlDashboards_test.js
@@ -6,7 +6,7 @@ Before( async (I) => {
 
 Scenario(
   'Open the PostgreSQL Instance Summary Dashboard and verify Metrics are present and graphs are displayed @not-pr-pipeline',
-  async (I, dashboardPage) => {
+  async (I, dashboardPage, adminPage) => {
     I.amOnPage(dashboardPage.postgresqlInstanceSummaryDashboard.url);
     dashboardPage.waitForDashboardOpened();
     await dashboardPage.expandEachDashboardRow();
