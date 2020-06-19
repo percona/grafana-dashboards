9d162c0642ddee957edbc2c2fc9252e5e75ee333
Puneet Kala <puneet.0191@gmail.com>
2020-06-19 14:05:26 +0530

    PMM-6140 fixing the windowsize change issue (#786)
    * PMM-6140 Adding fix for Remote Instances and Inventory Page UI
    
    * PMM-6140 minor
    
    * PMM-6140 Fix for rds-mysql tests
    
    * PMM-6140 fixing RDS tests
    
    * PMM-6140 WindowSize
    
    * PMM-6140 Adding fix for window size change
    
    * PMM-6140

--- pmm-app/tests/verifyPostgresqlDashboards_test.js
+++ pmm-app/tests/verifyPostgresqlDashboards_test.js
@@ -6,10 +6,11 @@ Before( async (I) => {
 
 Scenario(
   'Open the PostgreSQL Instance Summary Dashboard and verify Metrics are present and graphs are displayed @not-pr-pipeline',
-  async (I, dashboardPage) => {
+  async (I, dashboardPage, adminPage) => {
     I.amOnPage(dashboardPage.postgresqlInstanceSummaryDashboard.url);
     dashboardPage.waitForDashboardOpened();
     await dashboardPage.expandEachDashboardRow();
+    adminPage.peformPageDown(5);
     dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
     await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
