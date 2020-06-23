49e555884f28a0315230357a68339f251eae6c1f
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-23 17:54:08 +0200

    PMM-6091 Add tests for UpdateButton

---
+++ pmm-app/src/pmm-update/components/UpdateButton/UpdateButton.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CenteredButton, UpdateButton } from 'pmm-update/components';
+
+jest.mock('../../../react-plugins-deps/components/helpers/notification-manager');
+
+describe('UpdateButton::', () => {
+  const nextVersion = 'x.y.z';
+  const fakeHandleClick = jest.fn();
+
+  it('should show the correct text', () => {
+    const wrapper = shallow(<UpdateButton onClick={fakeHandleClick} disabled nextVersion={nextVersion} />);
+
+    expect(wrapper.find(CenteredButton).props()).toHaveProperty('children', ['Update to ', nextVersion]);
+
+    wrapper.unmount();
+  });
+
+  it('should pass props to child component', () => {
+    const wrapper = shallow(<UpdateButton onClick={fakeHandleClick} disabled nextVersion={nextVersion} />);
+
+    expect(wrapper.find(CenteredButton).props()).toHaveProperty('disabled', true);
+
+    wrapper.unmount();
+  });
+
+  it('should call the passed onClick handler on Button click', () => {
+    const wrapper = shallow(<UpdateButton onClick={fakeHandleClick} disabled nextVersion={nextVersion} />);
+
+    wrapper.find(CenteredButton).simulate('click');
+
+    expect(fakeHandleClick).toBeCalledTimes(1);
+
+    wrapper.unmount();
+  });
+});
