a8f5acac7862de58ccbbac46a5f13dd6b61cdf00
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-23 17:53:47 +0200

    PMM-6091 Update tests for LastCheck

--- pmm-app/src/pmm-update/components/LastCheck/LastCheck.test.tsx
+++ pmm-app/src/pmm-update/components/LastCheck/LastCheck.test.tsx
@@ -8,6 +8,14 @@ describe('LastCheck::', () => {
   const lastCheckDate = '12345';
   const fakeHandleClick = jest.fn();
 
+  it('should show the passed last check date', () => {
+    const wrapper = shallow(<LastCheck onCheckForUpdates={fakeHandleClick} lastCheckDate={lastCheckDate} />);
+
+    expect(wrapper.find('div > p').text()).toEqual(`Last check: ${lastCheckDate}`);
+
+    wrapper.unmount();
+  });
+
   it('should call the passed onClick handler on Button click', () => {
     const wrapper = shallow(<LastCheck onCheckForUpdates={fakeHandleClick} lastCheckDate={lastCheckDate} />);
 
