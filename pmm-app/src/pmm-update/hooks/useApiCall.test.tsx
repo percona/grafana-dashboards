12fb529af234bc250261b5e73781ffeb91a961d5
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-24 13:31:56 +0200

    PMM-6091 Update tests for useApiCall

--- pmm-app/src/pmm-update/hooks/useApiCall.test.tsx
+++ pmm-app/src/pmm-update/hooks/useApiCall.test.tsx
@@ -13,9 +13,38 @@ const HookWrapper: FC<{ hook: () => any }> = ({ hook }) => {
 
 describe('useApiCall', () => {
   const fakeData = 42;
-  const fakeApi = jest.fn().mockImplementation(() => Promise.resolve(fakeData));
+  const fakeApi = jest.fn().mockImplementation(() => fakeData);
+  const fakeApiInvalid = jest.fn().mockImplementation(() => null);
+  const fakeApiWithTimeout = jest.fn().mockImplementation(async () => {
+    await new Promise(() => setTimeout(() => {}, 1000));
+    return Promise.resolve(fakeData);
+  });
 
-  it('should return the correct values if the api call is successfull', async () => {
+  it('should return the correct values if the api call is pending', async () => {
+    let wrapper: ReturnType<typeof mount> | undefined;
+
+    await act(async () => {
+      wrapper = mount(<HookWrapper hook={() => useApiCall(fakeApiWithTimeout)} />);
+    });
+
+    wrapper?.update();
+
+    const [data, errorMessage, isLoading, apiCall] = wrapper?.find('div').prop('data-hook');
+
+    expect(data).toEqual(undefined);
+    expect(errorMessage).toEqual('');
+    expect(isLoading).toEqual(true);
+
+    await act(async () => {
+      apiCall();
+    });
+
+    expect(fakeApiWithTimeout).toBeCalledTimes(2);
+
+    wrapper?.unmount();
+  });
+
+  it('should return the correct values if the api call has succeeded', async () => {
     let wrapper: ReturnType<typeof mount> | undefined;
 
     await act(async () => {
@@ -25,7 +54,6 @@ describe('useApiCall', () => {
     wrapper?.update();
 
     const [data, errorMessage, isLoading, apiCall] = wrapper?.find('div').prop('data-hook');
-
     expect(data).toEqual(fakeData);
     expect(errorMessage).toEqual('');
     expect(isLoading).toEqual(false);
@@ -35,5 +63,30 @@ describe('useApiCall', () => {
     });
 
     expect(fakeApi).toBeCalledTimes(2);
+
+    wrapper?.unmount();
+  });
+
+  it('should return the correct error if the api call has failed', async () => {
+    let wrapper: ReturnType<typeof mount> | undefined;
+
+    await act(async () => {
+      wrapper = mount(<HookWrapper hook={() => useApiCall(fakeApiInvalid)} />);
+    });
+
+    wrapper?.update();
+
+    const [data, errorMessage, isLoading, apiCall] = wrapper?.find('div').prop('data-hook');
+    expect(data).toBe(undefined);
+    expect(errorMessage).toEqual(Error('Invalid response received'));
+    expect(isLoading).toEqual(false);
+
+    await act(async () => {
+      apiCall();
+    });
+
+    expect(fakeApiInvalid).toBeCalledTimes(2);
+
+    wrapper?.unmount();
   });
 });
