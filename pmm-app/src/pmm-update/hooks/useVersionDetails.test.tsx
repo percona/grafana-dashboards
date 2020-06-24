675691304a223aa2957ad0ea105f3276bd35f6d4
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-25 01:29:46 +0200

    PMM-6091 Add tests for useVersionDetails

---
+++ pmm-app/src/pmm-update/hooks/useVersionDetails.test.tsx
@@ -0,0 +1,191 @@
+import React, { FC } from 'react';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+
+import { useApiCall, useVersionDetails } from 'pmm-update/hooks';
+
+jest.mock('../../react-plugins-deps/components/helpers/notification-manager');
+
+const HookWrapper: FC<{ hook: () => any }> = ({ hook }) => {
+  const dataHook = hook ? hook() : undefined;
+  return <div data-hook={dataHook} />;
+};
+
+// NOTE (nicolalamacchia): this mock is here because some test cases alter it
+jest.mock('./useApiCall', () => ({
+  useApiCall: jest.fn(),
+}));
+const mockedUseApiCall = useApiCall as jest.Mock;
+
+const fakeData = {
+  last_check: '2020-06-10T19:16:57Z',
+  latest: {
+    full_version: 'x.y.z-rc.j+1234567890',
+    timestamp: '2020-06-09T19:16:57Z',
+    version: 'x.y.z',
+  },
+  installed: {
+    full_version: 'a.b.c-rc.i+0123456789',
+    timestamp: '2020-06-08T19:16:57Z',
+    version: 'a.b.c',
+  },
+  latest_news_url: 'https://percona.com',
+  update_available: true,
+};
+
+const fakeDataUndefinedLeafs = {
+  last_check: undefined,
+  latest: {
+    full_version: undefined,
+    timestamp: undefined,
+    version: undefined,
+  },
+  installed: {
+    full_version: undefined,
+    timestamp: undefined,
+    version: undefined,
+  },
+  latest_news_url: undefined,
+  update_available: undefined,
+};
+
+const mockedApiCall = jest.fn();
+
+const defaultMockedUseApiCallReturn = [undefined, '', true, mockedApiCall];
+
+describe('useVersionDetails', () => {
+  let wrapper: ReturnType<typeof mount> | undefined;
+
+  beforeEach(async () => {
+    // default mock
+    mockedUseApiCall.mockImplementation(() => defaultMockedUseApiCallReturn);
+
+    await act(async () => {
+      wrapper = mount(<HookWrapper hook={() => useVersionDetails()} />);
+    });
+    wrapper?.update();
+  });
+
+  afterEach(() => {
+    mockedUseApiCall.mockRestore();
+    wrapper?.unmount();
+    mockedApiCall.mockClear();
+  });
+
+  it('should make an API call using useApiCall', () => {
+    expect(mockedUseApiCall).toBeCalledTimes(1);
+  });
+
+  it('should return sane defaults when data is undefined', async () => {
+    const [
+      { installedVersionDetails, lastCheckDate, nextVersionDetails, isUpdateAvailable },
+      errorMessage,
+      isLoading,
+      isDefaultView,
+      getVersionDetails,
+    ] = wrapper?.find('div').prop('data-hook');
+
+    expect(installedVersionDetails).toBe(undefined);
+    expect(lastCheckDate).toEqual('');
+    expect(nextVersionDetails).toBe(undefined);
+    expect(isUpdateAvailable).toEqual(false);
+    expect(errorMessage).toBe(defaultMockedUseApiCallReturn[1]);
+    expect(isLoading).toEqual(defaultMockedUseApiCallReturn[2]);
+    expect(isDefaultView).toBe(true);
+
+    await act(async () => {
+      getVersionDetails();
+    });
+    wrapper?.update();
+
+    expect(mockedApiCall).toBeCalledTimes(1);
+  });
+
+  it('should return the correct fallbacks when the API call returns some undefined values', async () => {
+    mockedUseApiCall.mockImplementation(() => [fakeDataUndefinedLeafs, '', false, mockedApiCall]);
+
+    await act(async () => {
+      wrapper = mount(<HookWrapper hook={() => useVersionDetails()} />);
+    });
+    wrapper?.update();
+
+    const [
+      { installedVersionDetails, lastCheckDate, nextVersionDetails, isUpdateAvailable },
+      errorMessage,
+      isLoading,
+      isDefaultView,
+      getVersionDetails,
+    ] = wrapper?.find('div').prop('data-hook');
+
+    const expectedNextVersionDetails = {
+      nextVersion: '',
+      nextFullVersion: '',
+      nextVersionDate: '',
+      newsLink: '',
+    };
+    const expectedInstalledVersionDetails = {
+      installedVersion: '',
+      installedFullVersion: '',
+      installedVersionDate: '',
+    };
+
+    expect(installedVersionDetails).toEqual(expectedInstalledVersionDetails);
+    expect(lastCheckDate).toEqual('');
+    expect(nextVersionDetails).toEqual(expectedNextVersionDetails);
+    expect(isUpdateAvailable).toEqual(false);
+    expect(errorMessage).toBe('');
+    expect(isLoading).toEqual(false);
+    expect(isDefaultView).toBe(false);
+
+    await act(async () => {
+      getVersionDetails();
+    });
+    wrapper?.update();
+
+    expect(mockedApiCall).toBeCalledTimes(1);
+  });
+
+  it('should return the correct values from the API call response', async () => {
+    mockedUseApiCall.mockImplementation(() => [fakeData, '', false, mockedApiCall]);
+
+    await act(async () => {
+      wrapper = mount(<HookWrapper hook={() => useVersionDetails()} />);
+    });
+    wrapper?.update();
+
+    const [
+      { installedVersionDetails, lastCheckDate, nextVersionDetails, isUpdateAvailable },
+      errorMessage,
+      isLoading,
+      isDefaultView,
+      getVersionDetails,
+    ] = wrapper?.find('div').prop('data-hook');
+
+    const expectedNextVersionDetails = {
+      nextFullVersion: fakeData.latest.full_version,
+      nextVersionDate: 'June 09',
+      nextVersion: fakeData.latest.version,
+      newsLink: 'https://percona.com',
+    };
+    const expectedInstalledVersionDetails = {
+      installedFullVersion: fakeData.installed.full_version,
+      installedVersionDate: 'June 08',
+      installedVersion: fakeData.installed.version,
+    };
+
+    expect(installedVersionDetails).toEqual(expectedInstalledVersionDetails);
+    expect(lastCheckDate).toEqual('June 10, 19:16');
+    expect(nextVersionDetails).toEqual(expectedNextVersionDetails);
+    expect(isUpdateAvailable).toEqual(fakeData.update_available);
+    expect(errorMessage).toBe('');
+    expect(isLoading).toEqual(false);
+    expect(isDefaultView).toBe(false);
+
+    await act(async () => {
+      getVersionDetails();
+    });
+    wrapper?.update();
+
+    expect(mockedApiCall).toBeCalledTimes(1);
+  });
+});
