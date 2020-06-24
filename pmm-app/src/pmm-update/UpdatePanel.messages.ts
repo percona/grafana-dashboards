2d4500b08b426994120c6fe5eecc1af6f8cc49b7
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-24 13:31:33 +0200

    PMM-6091 Remove unused file

--- pmm-app/src/pmm-update/UpdatePanel.messages.ts
+++
@@ -1,7 +0,0 @@
-export const Messages = {
-  panelTitle: 'Updated',
-  updateTo: 'Update to',
-  availableVersion: 'Available version',
-  lastCheck: 'Last check',
-  nothingToUpdate: 'Nothing to update',
-};
