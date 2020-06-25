bedddcdc969bf3b81906973b82a5f53bbfd889d7
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-06-25 15:57:04 +0200

    PMM-6091 Update src/pmm-update/components/AvailableUpdate/AvailableUpdate.test.tsx

--- pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.test.tsx
+++ pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.test.tsx
@@ -5,30 +5,30 @@ import { AvailableUpdate } from './AvailableUpdate';
 
 jest.mock('../../../react-plugins-deps/components/helpers/notification-manager');
 
-  const nextFullVersion = 'x.y.z-rc.j+1234567890';
-  const nextVersion = 'x.y.z';
-  const newsLink = 'https://percona.com';
-  const nextVersionDate = '23 Jun';
+const nextFullVersion = 'x.y.z-rc.j+1234567890';
+const nextVersion = 'x.y.z';
+const newsLink = 'https://percona.com';
+const nextVersionDate = '23 Jun';
 
-  const nextVersionDetails = { nextVersionDate, nextVersion, nextFullVersion, newsLink };
+const nextVersionDetails = { nextVersionDate, nextVersion, nextFullVersion, newsLink };
 
 describe('AvailableUpdate::', () => {
   let wrapper: ReturnType<typeof shallow> | undefined;
 
   beforeEach(() => {
-    wrapper = shallow( <AvailableUpdate nextVersionDetails={nextVersionDetails} />);
+    wrapper = shallow(<AvailableUpdate nextVersionDetails={nextVersionDetails} />);
   });
 
   afterEach(() => {
     wrapper?.unmount();
   });
 
-
   it('should show only the short version by default', () => {
     expect(wrapper?.find('section > div > p').length).toEqual(2);
     expect(wrapper?.find('section > div > p > em').length).toEqual(1);
     expect(
-      wrapper?.find('section > div > p')
+      wrapper
+        ?.find('section > div > p')
         .at(1)
         .text()
     ).toEqual(`${nextVersion} ${nextVersionDate}`);
@@ -37,13 +37,13 @@ describe('AvailableUpdate::', () => {
   it('should show the news link if present', () => {
     expect(wrapper?.find('section > div > p').length).toEqual(2);
     expect(wrapper?.find('section > div > p > a').length).toEqual(1);
-
   });
 
   it('should show the full version on alt-click', () => {
     wrapper?.find('section > div').simulate('click', { altKey: true });
     expect(
-      wrapper?.find('section > div > p')
+      wrapper
+        ?.find('section > div > p')
         .at(1)
         .text()
     ).toEqual(`${nextFullVersion} ${nextVersionDate}`);
