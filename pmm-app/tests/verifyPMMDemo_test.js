0e4833b9211369daa134cd0e0adb7bf8c6981883
Ivana Wzientková <ivana.wzientkova@percona.com>
2020-06-30 10:19:41 +0200

    Added tags

--- pmm-app/tests/verifyPMMDemo_test.js
+++ pmm-app/tests/verifyPMMDemo_test.js
@@ -1,15 +1,18 @@
 Feature('Verify PMMDemo side');
 
-Scenario('PMM-T363 - Verify Copyrights & Legal section elements', async (I, pmmDemoPage) => {
-  I.amOnPage(pmmDemoPage.url);
-  I.waitForVisible(pmmDemoPage.fields.title, 30);
-  pmmDemoPage.verifyCopyrightsAndLegal();
-  I.amOnPage(pmmDemoPage.url + pmmDemoPage.mongoDBDashbordUrl);
-  pmmDemoPage.verifyCopyrightsAndLegal();
-});
+Scenario(
+  'PMM-T363 - Verify Copyrights & Legal section elements @not-pr-pipeline @pmm-demo',
+  async (I, pmmDemoPage) => {
+    I.amOnPage(pmmDemoPage.url);
+    I.waitForVisible(pmmDemoPage.fields.title, 30);
+    pmmDemoPage.verifyCopyrightsAndLegal();
+    I.amOnPage(pmmDemoPage.url + pmmDemoPage.mongoDBDashbordUrl);
+    pmmDemoPage.verifyCopyrightsAndLegal();
+  }
+);
 
 Scenario(
-  'PMM-T364 - Verify PMM settings returns Access denied error',
+  'PMM-T364 - Verify PMM settings returns Access denied error @not-pr-pipeline @pmm-demo',
   async (I, pmmDemoPage, pmmSettingsPage) => {
     I.amOnPage(pmmDemoPage.url + pmmSettingsPage.url);
     I.waitForVisible(pmmDemoPage.fields.accessDenied, 30);
@@ -17,7 +20,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T364 - Verify PMM settings returns Access denied error',
+  'PMM-T364 - Verify PMM settings returns Access denied error @not-pr-pipeline @pmm-demo',
   async (I, pmmDemoPage, pmmInventoryPage) => {
     I.amOnPage(pmmDemoPage.url + pmmInventoryPage.url);
     I.waitForVisible(pmmDemoPage.fields.accessDenied, 30);
@@ -25,7 +28,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T366 - Verify Failed security checks doesnt display the result of check',
+  'PMM-T366 - Verify Failed security checks doesnt display the result of check @not-pr-pipeline @pmm-demo',
   async (I, pmmDemoPage) => {
     const text = 'Insufficient access rights.';
     I.amOnPage(pmmDemoPage.url);
@@ -36,7 +39,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T367 - Verify PMM Database checks page doesnt display checks',
+  'PMM-T367 - Verify PMM Database checks page doesnt display checks @not-pr-pipeline @pmm-demo',
   async (I, pmmDemoPage, databaseChecksPage) => {
     const text = 'Insufficient access rights.';
     I.amOnPage(pmmDemoPage.url + databaseChecksPage.url);
