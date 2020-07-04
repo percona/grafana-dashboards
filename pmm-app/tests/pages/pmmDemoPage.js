536b44058e2de927d30d3a61ee17501abd8a0b97
Puneet Kala <puneet.0191@gmail.com>
2020-07-04 09:39:38 +0530

    PMM-6088 adding missing url  (#827)
    * PMM-6088 Fixing failing UI tests
    
    * PMM-6088 Minor changes and support for filters
    
    * PMM-6088 Adding more sanity checks for PMM-Demo
    
    * PMM-6088 Adding test case number
    
    * PMM-6088
    
    * PMM-6088 dashboard and permissions
    
    * PMM-6088 Adding the url

--- pmm-app/tests/pages/pmmDemoPage.js
+++ pmm-app/tests/pages/pmmDemoPage.js
@@ -4,6 +4,10 @@ const assert = require('assert');
 module.exports = {
   url: 'https://pmmdemo.percona.com/',
   mongoDBDashbordUrl: 'graph/d/mongodb-instance-overview/mongodb-instances-overview?orgId=1&refresh=1m',
+  // eslint-disable-next-line max-len
+  advancedDataExplorationDashboardUrl:
+    // eslint-disable-next-line max-len
+    'graph/d/1oz9QMHmk/advanced-data-exploration?refresh=1m&var-metric=go_gc_duration_seconds&var-interval=$__auto_interval_interval&var-node_name=All',
   monitoredDB: [
     'pxc',
     6,
