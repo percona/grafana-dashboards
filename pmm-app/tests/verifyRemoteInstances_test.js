df7e6c006fea37917901902134a4f870b304febf
Ivana Wzientková <ivana.wzientkova@percona.com>
2020-07-08 14:43:40 +0200

    PMM-T343 done

--- pmm-app/tests/verifyRemoteInstances_test.js
+++ pmm-app/tests/verifyRemoteInstances_test.js
@@ -118,7 +118,7 @@ Scenario(
   }
 );
 */
-Scenario(
+xScenario(
   'PMM-T342 - Verify pmm-server node cannot be removed from PMM Inventory page @not-pr-pipeline',
   async (I, pmmInventoryPage) => {
     const node = 'pmm-server';
@@ -130,5 +130,29 @@ Scenario(
     I.click(pmmInventoryPage.fields.forceModeCheckbox);
     I.click(pmmInventoryPage.fields.proceedButton);
     pmmInventoryPage.checkNodeExists(node);
+  }
+);
+
+Scenario(
+  'PMM-T343 - Verify agent can be removed on PMM Inventory page @not-pr-pipeline',
+  async (I, pmmInventoryPage) => {
+    const agentType = 'MySQL exporter';
+    I.amOnPage(pmmInventoryPage.url);
+    I.waitForVisible(pmmInventoryPage.fields.nodesLink, 20);
+    I.click(pmmInventoryPage.fields.nodesLink);
+    const countOfNodesBefore = await pmmInventoryPage.getNodeCount();
+    I.waitForVisible(pmmInventoryPage.fields.agentsLink, 20);
+    I.click(pmmInventoryPage.fields.agentsLink);
+    const serviceId = await pmmInventoryPage.getAgentServiceID(agentType);
+    const agentId = await pmmInventoryPage.getAgentID(agentType);
+    pmmInventoryPage.selectAgent(agentType);
+    I.click(pmmInventoryPage.fields.deleteButton);
+    I.click(pmmInventoryPage.fields.proceedButton);
+    pmmInventoryPage.existsByid(agentId, true);
+    I.click(pmmInventoryPage.fields.nodesLink);
+    const countOfNodesAfter = await pmmInventoryPage.getNodeCount();
+    pmmInventoryPage.verifyNodesCount(countOfNodesBefore, countOfNodesAfter);
+    I.click(pmmInventoryPage.fields.pmmServicesSelector);
+    pmmInventoryPage.existsByid(serviceId, false);
   }
 );
