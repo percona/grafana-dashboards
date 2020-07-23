42767465b62dcae509307168b4646934d74787a6
Tiago Mota Santos <tiago.mota@percona.com>
2020-07-23 20:10:37 +0100

    PMM-6272 Add media queries to settings

--- pmm-app/src/shared/components/Form/Radio/RadioButtonGroup.tsx
+++ pmm-app/src/shared/components/Form/Radio/RadioButtonGroup.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
 import { SelectableValue } from '@grafana/data';
+import { useTheme } from '@grafana/ui';
 import { cx } from 'emotion';
 import { RadioButton } from './RadioButton';
-import { styles } from './RadioButtonGroup.styles';
+import { getStyles } from './RadioButtonGroup.styles';
 
 export interface RadioButtonGroupProps {
   selected: string;
@@ -20,18 +21,23 @@ export const RadioButtonGroup: FC<RadioButtonGroupProps> = ({
   className,
   dataQa,
   onChange
-}) => (
-  <div className={cx(styles.radioButtonGroup, className)} data-qa={dataQa}>
-    {options.map(({ key, value }) => (
-      <RadioButton
-        key={key}
-        id={key}
-        name={name}
-        active={key === selected}
-        onChange={(key) => onChange(key)}
-      >
-        {value}
-      </RadioButton>
-    ))}
-  </div>
-);
+}) => {
+  const theme = useTheme();
+  const styles = getStyles(theme);
+
+  return (
+    <div className={cx(styles.radioButtonGroup, className)} data-qa={dataQa}>
+      {options.map(({ key, value }) => (
+        <RadioButton
+          key={key}
+          id={key}
+          name={name}
+          active={key === selected}
+          onChange={(key) => onChange(key)}
+        >
+          {value}
+        </RadioButton>
+      ))}
+    </div>
+  );
+};
