f524508298e68fb9b1e5ed6d685611a12e7bb67c
Alex <alexander.tymchuk@gmail.com>
2020-07-23 19:02:37 +0300

    PMM-6272 remove the redundant forawesome package

--- pmm-app/src/pmm-settings/components/MetricsResolution/MetricsResolution.tsx
+++ pmm-app/src/pmm-settings/components/MetricsResolution/MetricsResolution.tsx
@@ -25,9 +25,12 @@ export const MetricsResolution: FC<MetricsResolutionProps> = ({ metricsResolutio
   const [resolution, setResolution] = useState(getResolutionValue(metricsResolutions).key);
   const [updatedResolutions, updateResolutions] = useState(initialResolutions);
   const [loading, setLoading] = useState(false);
-  const { metrics: { action, label, tooltip }, tooltipLinkText } = Messages;
-  // eslint-disable-next-line max-len
-  const onChangeInput = (key: string, value: string) => updateResolutions({ ...updatedResolutions, [key]: value });
+  const {
+    metrics: { action, label, tooltip },
+    tooltipLinkText,
+  } = Messages;
+  const onChangeInput = (key: string, value: string) =>
+    updateResolutions({ ...updatedResolutions, [key]: value });
   const applyChanges = () => {
     console.log('UPDATE SETTINGS');
     updateSettings({ metrics_resolutions: addUnits(updatedResolutions) }, setLoading);
@@ -35,11 +38,11 @@ export const MetricsResolution: FC<MetricsResolutionProps> = ({ metricsResolutio
 
   useEffect(() => {
     if (resolution !== 'custom') {
-      // eslint-disable-next-line max-len
-      updateResolutions(removeUnits(defaultResolutions[resolutionsOptions.findIndex((r) => r.key === resolution)]));
+      updateResolutions(
+        removeUnits(defaultResolutions[resolutionsOptions.findIndex(r => r.key === resolution)])
+      );
     }
   }, [resolution]);
-
 
   return (
     <div className={styles.resolutionsWrapper}>
@@ -58,14 +61,14 @@ export const MetricsResolution: FC<MetricsResolutionProps> = ({ metricsResolutio
         name="resolutions"
         dataQa="metrics-resolution-radio-button-group"
         className={styles.resolutionsRadioButtonGroup}
-        onChange={(key) => setResolution(key)}
+        onChange={key => setResolution(key)}
       />
       <input
         type="number"
         className={styles.resolutionInput}
         value={updatedResolutions.lr}
         disabled={resolution !== 'custom'}
-        onChange={(e) => onChangeInput('lr', e.target.value)}
+        onChange={e => onChangeInput('lr', e.target.value)}
         data-qa="metrics-resolution-lr-input"
       />
       <input
@@ -73,7 +76,7 @@ export const MetricsResolution: FC<MetricsResolutionProps> = ({ metricsResolutio
         className={styles.resolutionInput}
         value={updatedResolutions.mr}
         disabled={resolution !== 'custom'}
-        onChange={(e) => onChangeInput('mr', e.target.value)}
+        onChange={e => onChangeInput('mr', e.target.value)}
         data-qa="metrics-resolution-mr-input"
       />
       <input
@@ -81,7 +84,7 @@ export const MetricsResolution: FC<MetricsResolutionProps> = ({ metricsResolutio
         className={styles.resolutionInput}
         value={updatedResolutions.hr}
         disabled={resolution !== 'custom'}
-        onChange={(e) => onChangeInput('hr', e.target.value)}
+        onChange={e => onChangeInput('hr', e.target.value)}
         data-qa="metrics-resolution-hr-input"
       />
       <Button
