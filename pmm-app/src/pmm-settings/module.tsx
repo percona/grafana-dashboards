42bbb566047a89afd94f0396545dc1215b49707a
Tiago Mota Santos <tiago.mota@percona.com>
2020-07-23 04:09:01 +0100

    PMM-6272 Remove panel styles import

--- pmm-app/src/pmm-settings/module.tsx
+++ pmm-app/src/pmm-settings/module.tsx
@@ -1,7 +1,6 @@
 import { PanelPlugin } from '@grafana/data';
 import '../shared/styles.scss';
 import '../shared/style.less';
-import './panel.scss';
 import { SettingsPanel } from './Settings';
 
 export const plugin = new PanelPlugin(SettingsPanel);
