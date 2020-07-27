4e0aba5e8dccb28a7af8147d30ed176df9752ce5
yurkovychv <vasyl.yurkovych@percona.com>
2020-07-27 18:33:35 +0300

    SAAS-263 cleanup remote instances tests

--- pmm-app/tests/pages/remoteInstancesPage.js
+++ pmm-app/tests/pages/remoteInstancesPage.js
@@ -9,6 +9,12 @@ module.exports = {
 
   // insert your locators and methods here
   // setting locators
+  services: {
+    mongodb: 'mongodb_remote_new',
+    mysql: 'mysql_remote_new',
+    postgresql: 'postgresql_remote_new',
+    proxysql: 'proxysql_remote_new'
+  },
   url: 'graph/d/pmm-add-instance/pmm-add-instance?orgId=1',
   addMySQLRemoteURL: 'graph/d/pmm-add-instance/pmm-add-instance?instance_type=mysql',
   rds: {
@@ -18,43 +24,43 @@ module.exports = {
     'Cluster': 'rds56-cluster'
   },
   fields: {
-    pageHeaderText: 'PMM Add Instance',
-    iframe: '//div[@class=\'panel-content\']//iframe',
-    remoteInstanceTitleLocator: '//section[@class=\'content-wrapper\']/h3',
-    remoteInstanceTitle: 'How to Add an Instance',
-    addInstancesList: '//nav[@class=\'navigation\']',
-    addMongoDBRemote: '//a[text()[contains(.,\'Add a Remote MongoDB Instance\')]]',
-    addMySqlRemote: '//a[text()[contains(.,\'Add a Remote MySQL Instance\')]]',
-    addPostgreSQLRemote: '//a[text()[contains(.,\'Add a Remote PostgreSQL Instance\')]]',
-    addProxySQLRemote: '//a[text()[contains(.,\'Add a Remote ProxySQL Instance\')]]',
-    hostName: '//input[contains(@placeholder,\'*Hostname\')]',
-    serviceName: '//input[@placeholder=\'Service name (default: Hostname)\']',
-    portNumber: '//input[contains(@placeholder, \'Port\')]',
-    userName: '//input[contains(@placeholder, \'Username\')]',
-    password: '//input[contains(@placeholder, \'Password\')]',
-    environment: '//input[contains(@placeholder, \'Environment\')]',
-    cluster: '//input[contains(@placeholder, \'Cluster\')]',
-    replicationSet: '//input[contains(@placeholder, \'Replication set\')]',
-    addService: '#addInstance',
-    skipTLS: '//input[@name=\'tls_skip_verify\']',
-    useTLS: '//input[@name=\'tls\']',
-    usePgStatStatements: '//input[@name=\'qan_postgresql_pgstatements_agent\']',
-    useQANMongoDBProfiler: '//input[@name=\'qan_mongodb_profiler\']',
-    usePerformanceSchema: '//input[@name=\'qan_mysql_perfschema\']',
-    usePerformanceSchema2: '//input[@name=\'qan_mysql_perfschema\']/following-sibling::span[2]',
-    skipTLSL: '//input[@name=\'tls_skip_verify\']/following-sibling::span[2]',
-    skipConnectionCheck: '//input[@name=\'skip_connection_check\']/following-sibling::span[2]',
-    availabilityZone: '//input[@placeholder="*Availability Zone"]',
-    addInstanceDiv: '//div[@class=\'view\']',
+    accessKeyInput: '//input[@name="aws_access_key"]',
     addAWSRDSMySQLbtn: "//a[text()[contains(.,'Add an AWS RDS MySQL or Aurora MySQL Instance')]]",
-    accessKeyInput: '//input[@name=\'aws_access_key\']',
-    secretKeyInput: '//input[@name=\'aws_secret_key\']',
+    addInstanceDiv: '//div[@class="view"]',
+    addInstancesList: '//nav[@class="navigation"]',
+    addMongoDBRemote: '//a[text()[contains(.,"Add a Remote MongoDB Instance")]]',
+    addMySqlRemote: '//a[text()[contains(.,"Add a Remote MySQL Instance")]]',
+    addPostgreSQLRemote: '//a[text()[contains(.,"Add a Remote PostgreSQL Instance")]]',
+    addProxySQLRemote: '//a[text()[contains(.,"Add a Remote ProxySQL Instance")]]',
+    addService: '#addInstance',
+    availabilityZone: '//input[@placeholder="*Availability Zone"]',
+    cluster: '//input[contains(@placeholder, "Cluster")]',
+    disableBasicMetrics: '//input[@name="disable_basic_metrics"]/following-sibling::span[2]',
+    disableEnhancedMetrics: '//input[@name="disable_enhanced_metrics"]/following-sibling::span[2]',
     discoverBtn: '#addInstance',
     discoveryResults: "tbody[role='rowgroup']",
-    discoveryRowWithId: '//tr/td[text()=\'',
+    discoveryRowWithId: '//tr/td[text()="',
+    environment: '//input[contains(@placeholder, "Environment")]',
+    hostName: '//input[contains(@placeholder,"*Hostname")]',
+    iframe: '//div[@class="panel-content"]//iframe',
+    pageHeaderText: 'PMM Add Instance',
+    password: '//input[contains(@placeholder, "Password")]',
+    portNumber: '//input[contains(@placeholder, "Port")]',
+    remoteInstanceTitle: 'How to Add an Instance',
+    remoteInstanceTitleLocator: '//section[@class="content-wrapper"]/h3',
+    replicationSet: '//input[contains(@placeholder, "Replication set")]',
+    secretKeyInput: '//input[@name="aws_secret_key"]',
+    serviceName: '//input[@placeholder="Service name (default: Hostname)"]',
+    skipConnectionCheck: '//input[@name="skip_connection_check"]/following-sibling::span[2]',
+    skipTLS: '//input[@name="tls_skip_verify"]',
+    skipTLSL: '//input[@name="tls_skip_verify"]/following-sibling::span[2]',
     startMonitoring: '/following-sibling::td/a',
-    disableEnhancedMetrics: '//input[@name=\'disable_enhanced_metrics\']/following-sibling::span[2]',
-    disableBasicMetrics: '//input[@name=\'disable_basic_metrics\']/following-sibling::span[2]',
+    usePerformanceSchema2: '//input[@name="qan_mysql_perfschema"]/following-sibling::span[2]',
+    usePerformanceSchema: '//input[@name="qan_mysql_perfschema"]',
+    usePgStatStatements: '//input[@name="qan_postgresql_pgstatements_agent"]',
+    useQANMongoDBProfiler: '//input[@name="qan_mongodb_profiler"]',
+    useTLS: '//input[@name="tls"]',
+    userName: '//input[contains(@placeholder, "Username")]',
   },
 
   waitUntilRemoteInstancesPageLoaded() {
@@ -89,7 +95,7 @@ module.exports = {
   fillRemoteFields(serviceName) {
     // eslint-disable-next-line default-case
     switch (serviceName) {
-      case 'mysql_remote_new':
+      case this.services.mysql:
         I.fillField(this.fields.hostName, process.env.REMOTE_MYSQL_HOST);
         I.fillField(this.fields.userName, process.env.REMOTE_MYSQL_USER);
         I.fillField(this.fields.password, process.env.REMOTE_MYSQL_PASSWORD);
@@ -101,15 +107,15 @@ module.exports = {
         I.fillField(this.fields.environment, 'remote-mysql');
         I.fillField(this.fields.cluster, 'remote-mysql-cluster');
         break;
-      case 'mongodb_remote_new':
-        I.fillField(this.fields.hostName, process.env.REMOTE_MONGODB_HOST);
-        I.fillField(this.fields.userName, process.env.REMOTE_MONGODB_USER);
-        I.fillField(this.fields.password, process.env.REMOTE_MONGODB_PASSWORD);
+      case this.services.mongodb:
+        I.fillField(this.fields.hostName, process.env.REMOTE_MONGODB_HOST || '34.200.242.226');
+        I.fillField(this.fields.userName, process.env.REMOTE_MONGODB_USER || 'admin');
+        I.fillField(this.fields.password, process.env.REMOTE_MONGODB_PASSWORD || '4Ju4nuTbLL};,4:F');
         I.fillField(this.fields.serviceName, serviceName);
         I.fillField(this.fields.environment, 'remote-mongodb');
         I.fillField(this.fields.cluster, 'remote-mongodb-cluster');
         break;
-      case 'postgresql_remote_new':
+      case this.services.postgresql:
         I.fillField(this.fields.hostName, process.env.REMOTE_POSTGRESQL_HOST);
         I.fillField(this.fields.userName, process.env.REMOTE_POSTGRESQL_USER);
         I.fillField(this.fields.password, process.env.REMOTE_POSTGRESSQL_PASSWORD);
@@ -117,7 +123,7 @@ module.exports = {
         I.fillField(this.fields.environment, 'remote-postgres');
         I.fillField(this.fields.cluster, 'remote-postgres-cluster');
         break;
-      case 'proxysql_remote_new':
+      case this.services.proxysql:
         I.fillField(this.fields.hostName, process.env.REMOTE_PROXYSQL_HOST);
         I.fillField(this.fields.userName, process.env.REMOTE_PROXYSQL_USER);
         I.fillField(this.fields.password, process.env.REMOTE_PROXYSQL_PASSWORD);
@@ -135,11 +141,11 @@ module.exports = {
     I.click(this.fields.skipTLSL);
     // eslint-disable-next-line default-case
     switch (serviceName) {
-      case 'mongodb_remote_new':
+      case this.services.mongodb:
         I.click(this.fields.useTLS);
         I.click(this.fields.useQANMongoDBProfiler);
         break;
-      case 'postgresql_remote_new':
+      case this.services.postgresql:
         I.click(this.fields.useTLS);
         I.click(this.fields.usePgStatStatements);
         break;
@@ -150,7 +156,6 @@ module.exports = {
     }
     I.click(this.fields.addService);
     I.waitForVisible(pmmInventoryPage.fields.agentsLink, 30);
-    I.waitForClickable(pmmInventoryPage.fields.agentsLink, 30);
 
     return pmmInventoryPage;
   },
@@ -159,7 +164,6 @@ module.exports = {
     I.click(this.fields.addAWSRDSMySQLbtn);
     I.waitForVisible(this.fields.accessKeyInput, 30);
     I.waitForVisible(this.fields.secretKeyInput, 30);
-    I.waitForClickable(this.fields.discoverBtn, 30);
   },
 
   discoverRDS() {
--- pmm-app/tests/verifyRemoteInstances_test.js
+++ pmm-app/tests/verifyRemoteInstances_test.js
@@ -1,77 +1,39 @@
-const remoteInstancesPage = require('./pages/remoteInstancesPage');
-const pmmInventoryPage = require('./pages/pmmInventoryPage');
+const page = require('./pages/remoteInstancesPage');
+let instances = new DataTable(['name']);
 
-Feature('to verify monitoried Remote Db instances');
+for (const i of Object.keys(page.services)) {
+  instances.add([i]);
+}
+
+Feature('Remote DB Instances');
 
 Before(async I => {
   I.Authorize();
 });
 
-Scenario(
-  'Verify Remote MySQL Instance Addition [critical] @not-pr-pipeline',
-  async (I, adminPage, remoteInstancesPage, pmmInventoryPage) => {
-    const mysql_service_name = 'mysql_remote_new';
-    I.amOnPage(remoteInstancesPage.url);
-    remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
-    remoteInstancesPage.openAddRemotePage('mysql');
-    remoteInstancesPage.fillRemoteFields(mysql_service_name);
-    remoteInstancesPage.createRemoteInstance(mysql_service_name);
-    pmmInventoryPage.verifyRemoteServiceIsDisplayed(mysql_service_name);
-    await pmmInventoryPage.verifyAgentHasStatusRunning(mysql_service_name);
-  }
-);
+Data(instances)
+  .Scenario(
+    'Verify Remote Instance Addition [critical] @not-pr-pipeline',
+    async (I, remoteInstancesPage, current) => {
+      const serviceName = remoteInstancesPage.services[current.name];
+      I.amOnPage(remoteInstancesPage.url);
+      remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
+      remoteInstancesPage.openAddRemotePage(current.name);
+      remoteInstancesPage.fillRemoteFields(serviceName);
+      remoteInstancesPage.createRemoteInstance(serviceName);
+    }
+  );
 
-Scenario(
-  'Verify is the remote instances are in Running Status [critical] @pmm-post-update @not-pr-pipeline',
-  async (I, adminPage, remoteInstancesPage, pmmInventoryPage) => {
-    const mysql_service_name = 'mysql_remote_new';
-    I.amOnPage(pmmInventoryPage.url);
-    pmmInventoryPage.verifyRemoteServiceIsDisplayed(mysql_service_name);
-    await pmmInventoryPage.verifyAgentHasStatusRunning(mysql_service_name);
-  }
-);
-
-xScenario(
-  'Verify Remote MongoDB Instance Addition [critical]',
-  async (I, adminPage, remoteInstancesPage, pmmInventoryPage) => {
-    const mongodb_service_name = 'mongodb_remote_new';
-    I.amOnPage(remoteInstancesPage.url);
-    remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
-    remoteInstancesPage.openAddRemotePage('mongodb');
-    remoteInstancesPage.fillRemoteFields(mongodb_service_name);
-    remoteInstancesPage.createRemoteInstance(mongodb_service_name);
-    pmmInventoryPage.verifyRemoteServiceIsDisplayed(mongodb_service_name);
-    await pmmInventoryPage.verifyAgentHasStatusRunning(mongodb_service_name);
-  }
-);
-
-Scenario(
-  'Verify Remote PostgreSQL Instance Addition [critical] @not-pr-pipeline',
-  async (I, adminPage, remoteInstancesPage, pmmInventoryPage) => {
-    const postgresql_service_name = 'postgresql_remote_new';
-    I.amOnPage(remoteInstancesPage.url);
-    remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
-    remoteInstancesPage.openAddRemotePage('postgresql');
-    remoteInstancesPage.fillRemoteFields(postgresql_service_name);
-    remoteInstancesPage.createRemoteInstance(postgresql_service_name);
-    pmmInventoryPage.verifyRemoteServiceIsDisplayed(postgresql_service_name);
-    await pmmInventoryPage.verifyAgentHasStatusRunning(postgresql_service_name);
-  }
-);
-
-Scenario(
-  'Verify Remote ProxySQL Instance Addition [critical] @not-pr-pipeline',
-  async (I, adminPage, remoteInstancesPage, pmmInventoryPage) => {
-    const proxysql_service_name = 'proxysql_remote_new';
-    I.amOnPage(remoteInstancesPage.url);
-    remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
-    remoteInstancesPage.openAddRemotePage('proxysql');
-    remoteInstancesPage.fillRemoteFields(proxysql_service_name);
-    remoteInstancesPage.createRemoteInstance(proxysql_service_name);
-    pmmInventoryPage.verifyRemoteServiceIsDisplayed(proxysql_service_name);
-    await pmmInventoryPage.verifyAgentHasStatusRunning(proxysql_service_name);
-  }
-);
+Data(instances)
+  .Scenario(
+    'Verify Remote Instance has Status Running [critical] @not-pr-pipeline',
+    async (I, remoteInstancesPage, pmmInventoryPage, current) => {
+      const serviceName = remoteInstancesPage.services[current.name];
+      I.amOnPage(pmmInventoryPage.url);
+      pmmInventoryPage.verifyRemoteServiceIsDisplayed(serviceName);
+      await pmmInventoryPage.verifyAgentHasStatusRunning(serviceName);
+    }
+  );
 
 Scenario(
   'PMM-T339 - Verify MySQL service is removed on PMM Inventory page @not-pr-pipeline',
