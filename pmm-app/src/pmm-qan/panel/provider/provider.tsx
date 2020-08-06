e75bbeae72804a2899ac2913bcfc185dc8065380
Roman Misyurin <roman.misyurin@percona.com>
2020-08-06 16:40:12 +0300

    PMM-6328 rework sparkline

--- pmm-app/src/pmm-qan/panel/provider/provider.tsx
+++ pmm-app/src/pmm-qan/panel/provider/provider.tsx
@@ -1,120 +1,131 @@
 import React, { useEffect, useState } from 'react';
 import { omit } from 'lodash';
-import {
-  parseURL, refreshGrafanaVariables, setLabels
-} from './provider.tools';
+import { parseURL, refreshGrafanaVariables, setLabels } from './provider.tools';
 import { QueryAnalyticsContext } from './provider.types';
 
 const initialState = {} as QueryAnalyticsContext;
 export const QueryAnalyticsProvider = React.createContext<QueryAnalyticsContext>(initialState);
 
-const actions = {
-  // eslint-disable-next-line max-len
-  setLabels: (value) => (state) => omit({ ...state, labels: setLabels(value), pageNumber: 1 }, ['queryId', 'querySelected']),
-  resetLabels: () => (state) => omit({ ...state, labels: {}, pageNumber: 1 }, ['queryId', 'querySelected']),
-  setActiveTab: (value) => (state) => ({ ...state, openDetailsTab: value }),
-  highlightSparkline: (value) => (state) => ({ ...state, highlightedCoords: value }),
-  setLoadingDetails: (value) => (state) => ({ ...state, loadingDetails: value }),
-  selectQuery: (value, totals) => (state) => ({
-    ...state,
-    queryId: value,
-    querySelected: true,
-    // openDetailsTab: 'details',
-    totals,
-  }),
-  addColumn: (value) => (state) => {
-    const columns = [...state.columns];
-
-    columns.push(value);
-
-    return {
-      ...state,
-      columns,
-    };
-  },
-  changeColumn: (value) => (state) => {
-    const columns = [...state.columns];
-
-    columns[columns.indexOf(value.oldColumn.simpleName)] = value.column;
-
-    return {
-      ...state,
-      columns,
-      orderBy:
-        value.oldColumn.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
-    };
-  },
-  swapMainColumn: (value) => (state) => {
-    const columns = [...state.columns];
-    const columnIndex = columns.indexOf(value.simpleName);
-    const currentColumn = columns[columnIndex];
-
-    // eslint-disable-next-line prefer-destructuring
-    columns[columnIndex] = columns[0];
-    columns[0] = currentColumn;
-
-    return {
-      ...state,
-      columns,
-    };
-  },
-  removeColumn: (value) => (state) => {
-    const columns = [...state.columns];
-
-    columns.splice(columns.indexOf(value.simpleName), 1);
-
-    return {
-      ...state,
-      columns,
-      orderBy: value.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
-    };
-  },
-  changePage: (value) => (state) => omit(
-    {
-      ...state,
-      pageNumber: value,
-    },
-    ['queryId', 'querySelected']
-  ),
-  changePageSize: (value) => (state) => omit(
-    {
-      ...state,
-      pageSize: value,
-      pageNumber: 1,
-    },
-    ['queryId', 'querySelected']
-  ),
-  changeSort: (value) => (state) => omit(
-    {
-      ...state,
-      orderBy: value,
-      pageNumber: 1,
-    },
-    ['queryId', 'querySelected']
-  ),
-  changeGroupBy: (value) => (state) => omit(
-    {
-      ...state,
-      groupBy: value,
-      querySelected: false,
-      pageNumber: 1,
-    },
-    ['queryId', 'querySelected']
-  ),
-  closeDetails: () => (state) => omit(
-    {
-      ...state,
-      loadingDetails: false,
-    },
-    ['queryId', 'querySelected']
-  ),
-  setFingerprint: (value) => (state) => ({
-    ...state,
-    fingerprint: value,
-  }),
-};
-
 export const UrlParametersProvider = ({ timeRange, children }) => {
+  const actions = {
+    // eslint-disable-next-line max-len
+    setLabels: (value) => (state) => omit({ ...state, labels: setLabels(value), pageNumber: 1 }, ['queryId', 'querySelected']),
+    resetLabels: () => (state) => omit({ ...state, labels: {}, pageNumber: 1 }, ['queryId', 'querySelected']),
+    selectTime: (value) => (state) => {
+      console.log('time selection', value);
+
+      return {
+        ...state,
+        from: value[0],
+        to: value[1],
+        rawTime: {
+          from: value[0],
+          to: value[1],
+        },
+      };
+    },
+    setActiveTab: (value) => (state) => ({ ...state, openDetailsTab: value }),
+    highlightSparkline: (value) => (state) => ({ ...state, highlightedCoords: value }),
+    setLoadingDetails: (value) => (state) => ({ ...state, loadingDetails: value }),
+    selectQuery: (value, totals) => (state) => ({
+      ...state,
+      queryId: value,
+      querySelected: true,
+      // openDetailsTab: 'details',
+      totals,
+    }),
+    addColumn: (value) => (state) => {
+      const columns = [...state.columns];
+
+      columns.push(value);
+
+      return {
+        ...state,
+        columns,
+      };
+    },
+    changeColumn: (value) => (state) => {
+      const columns = [...state.columns];
+
+      columns[columns.indexOf(value.oldColumn.simpleName)] = value.column;
+
+      return {
+        ...state,
+        columns,
+        orderBy:
+          value.oldColumn.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
+      };
+    },
+    swapMainColumn: (value) => (state) => {
+      const columns = [...state.columns];
+      const columnIndex = columns.indexOf(value.simpleName);
+      const currentColumn = columns[columnIndex];
+
+      // eslint-disable-next-line prefer-destructuring
+      columns[columnIndex] = columns[0];
+      columns[0] = currentColumn;
+
+      return {
+        ...state,
+        columns,
+      };
+    },
+    removeColumn: (value) => (state) => {
+      const columns = [...state.columns];
+
+      columns.splice(columns.indexOf(value.simpleName), 1);
+
+      return {
+        ...state,
+        columns,
+        orderBy: value.simpleName === state.orderBy.replace('-', '') ? `-${columns[0]}` : state.orderBy,
+      };
+    },
+    changePage: (value) => (state) => omit(
+      {
+        ...state,
+        pageNumber: value,
+      },
+      ['queryId', 'querySelected'],
+    ),
+    changePageSize: (value) => (state) => omit(
+      {
+        ...state,
+        pageSize: value,
+        pageNumber: 1,
+      },
+      ['queryId', 'querySelected'],
+    ),
+    changeSort: (value) => (state) => omit(
+      {
+        ...state,
+        orderBy: value,
+        pageNumber: 1,
+      },
+      ['queryId', 'querySelected'],
+    ),
+    changeGroupBy: (value) => (state) => omit(
+      {
+        ...state,
+        groupBy: value,
+        querySelected: false,
+        pageNumber: 1,
+      },
+      ['queryId', 'querySelected'],
+    ),
+    closeDetails: () => (state) => omit(
+      {
+        ...state,
+        loadingDetails: false,
+      },
+      ['queryId', 'querySelected'],
+    ),
+    setFingerprint: (value) => (state) => ({
+      ...state,
+      fingerprint: value,
+    }),
+  };
+
   const query = new URLSearchParams(window.location.search);
 
   const [panelState, setContext] = useState({
--- pmm-app/src/shared/components/Elements/Charts/Sparkline/Sparkline.tsx
+++ pmm-app/src/shared/components/Elements/Charts/Sparkline/Sparkline.tsx
@@ -1,327 +1,221 @@
-// @ts-nocheck
-
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import * as d3 from 'd3';
-import { area, axisBottom, curveStepAfter, scaleLinear } from 'd3';
+import React, { useEffect, useRef, useState } from 'react';
+import { scaleLinear } from 'd3';
 import * as moment from 'moment';
 import ReactTooltip from 'react-tooltip';
 import { humanize } from '../../../helpers/Humanization';
-import './Sparkline.scss';
 import { PolygonChartInterface } from './Sparkline.types';
-import { QueryAnalyticsProvider } from '../../../../../pmm-qan/panel/provider/provider';
+import {
+  findXRange,
+  findYRange,
+  getAdditionalPoint,
+  getMetricSparklineKey,
+  isMetricExists,
+} from './Sparkline.tools';
 
-const getMetricSparklineKey = (metricName) => {
-  switch (metricName) {
-    case 'load':
-      return 'load';
-    case 'num_queries':
-      return 'num_queries_per_sec';
-    case 'num_queries_with_warnings':
-      return 'num_queries_with_warnings_per_sec';
-    case 'num_queries_with_errors':
-      return 'num_queries_with_errors_per_sec';
-    default:
-      return `m_${metricName}_sum_per_sec`;
-  }
+const updateGraphs = (columnNumber) => {
+  const event = new CustomEvent('sync-graphs', { detail: columnNumber });
+
+  document.dispatchEvent(event);
 };
+
+const BAR_HEIGHT = 30;
+const GRAPH_WIDTH = 300;
 
 export const Sparkline = ({
   margin = 0,
-  width = 300,
-  height = 30,
+  width = GRAPH_WIDTH,
+  height = BAR_HEIGHT,
   metricName,
   data,
-  color = 'rgba(255, 239, 168, 0.8)',
+  color = '#d0c38b',
 }: PolygonChartInterface) => {
-
-  const [coords,setCoords] = useState(null)
-  useEffect(() => {
-    const handler = function(e) {
-      setCoords(e.detail)
-    }
-    document.addEventListener(
-      'build',
-      handler,
-      false,
-    );
-    return () => {
-      document.removeEventListener('build', handler)
-    }
-  }, []);
+  const [tooltip, setTooltip] = useState('');
 
   const xkey = 'timestamp';
   const ykey = getMetricSparklineKey(metricName);
-  const appLoadPolygonChart = [...data] || [];
+  let appLoadPolygonChart = [...data] || [];
 
-  const getAdditionalPoint = (last, previous) =>
-    new Date(
-      (+moment.utc(last) || 0) - ((+moment.utc(previous) || 0) - (+moment.utc(last) || 0)),
-    ).toISOString();
+  if (appLoadPolygonChart.length > 2) {
+    appLoadPolygonChart.push({
+      point: appLoadPolygonChart[appLoadPolygonChart.length - 1].point + 1,
+      timestamp: getAdditionalPoint(
+        appLoadPolygonChart[appLoadPolygonChart.length - 1].timestamp,
+        appLoadPolygonChart[appLoadPolygonChart.length - 2].timestamp,
+      ),
+    });
+  }
 
-  // Adding additional point for display purposes
-  // TODO: replace it with something better
-  appLoadPolygonChart.push({
-    point: appLoadPolygonChart[appLoadPolygonChart.length - 1].point + 1,
-    timestamp: getAdditionalPoint(
-      appLoadPolygonChart[appLoadPolygonChart.length - 1].timestamp,
-      appLoadPolygonChart[appLoadPolygonChart.length - 2].timestamp,
-    ),
-  });
+  appLoadPolygonChart = appLoadPolygonChart.sort((a, b) => {
+    if (a.point === undefined) {
+      return 1;
+    }
 
-  const [tooltip, setTooltip] = useState('');
-  const isMetricExists = (metric) => metric === 'NaN' || metric === undefined || metric === '';
+    if (b.point === undefined) {
+      return -1;
+    }
 
-  const findYRange = (array) => {
-    const values = array.map((arrayItem) => +arrayItem[ykey] || 0);
+    return b.point - a.point;
+  })
 
-    return [Math.max(...values) || 1, Math.min(...values) || 0];
-  };
+  const xAxisLength = width - 2 * margin;
+  const yAxisLength = height - 2 * margin;
 
-  const findXRange = (array) => {
-    const values = array.map((arrayItem) => +moment.utc(arrayItem[xkey]) || 0);
+  const [maxX, minX] = findXRange(appLoadPolygonChart, xkey);
+  const scaleX = scaleLinear()
+    .domain([minX, maxX])
+    .range([0, xAxisLength]);
 
-    return [Math.max(...values), Math.min(...values)];
-  };
-  // eslint-disable-next-line react/no-string-refs
-  const ref = useRef<HTMLDivElement>();
-  const [svg, setSvg] = useState()
-  const [focusBar, setFocusBar] = useState()
+  const [maxY, minY] = findYRange(appLoadPolygonChart, ykey);
+  const scaleY = scaleLinear()
+    .domain([maxY, minY])
+    .range([0, yAxisLength]);
 
+  const drawData = appLoadPolygonChart.map((item) => ({
+    x: scaleX(moment.utc(item.timestamp)),
+    y: scaleY(isMetricExists(item[ykey]) ? 0 : Math.max(maxY / 15, item[ykey])),
+  }))
 
-  useEffect(() => {
-    d3.select(ref.current)
-      .selectAll('*')
-      .remove();
-    // console.log(coords)
-    const innerSvg = d3
-      // eslint-disable-next-line react/no-string-refs
-      .select(ref.current)
-      .append('svg')
-      .attr('class', 'axis')
-      .attr('width', width)
-      .attr('height', height);
-    setSvg(innerSvg)
-    console.log('ref changed', svg);
-  }, [ref])
-  useEffect(() => {
-    drawGraph(ref);
-  },[svg]);
+  const sparklineCanvas = useRef<HTMLCanvasElement>();
 
   useEffect(() => {
-    const highlightBar = (coords) => {
-      if (focusBar) {
-        focusBar.remove()
-      }
-      console.log(coords)
-      if (!coords[0]) {
-        return
-      }
+    if (!sparklineCanvas.current) {
+      return;
+    }
 
-      const xAxisLength = width - 2 * margin;
-      const yAxisLength = height - 2 * margin;
+    const ctx = sparklineCanvas.current.getContext('2d');
+    const drawBar = (barIndex: number, color: string, minHeight?: boolean): void => {
+      ctx.fillStyle = color;
+      const height = drawData[barIndex].y;
+      const offsetX = drawData[barIndex].x;
+      const width = drawData[barIndex - 1]
+        ? drawData[barIndex].x - drawData[barIndex - 1].x
+        : GRAPH_WIDTH / drawData.length;
 
-      const [maxX, minX] = findXRange(appLoadPolygonChart);
-      const scaleX = scaleLinear()
-        .domain([minX, maxX])
-        .range([0, xAxisLength]);
-
-      const [maxY, minY] = findYRange(appLoadPolygonChart);
-      const scaleY = scaleLinear()
-        .domain([maxY, minY])
-        .range([0, yAxisLength]);
-
-      const drawData = appLoadPolygonChart.map((item) => ({
-        x: scaleX(moment.utc(item.timestamp)),
-        y: scaleY(isMetricExists(item[ykey]) ? 0 : Math.max(maxY / 15, item[ykey])) + margin || 0,
-      }));
-
-      const areaBar = area()
-        .curve(curveStepAfter)
-        .x((d) => d.x)
-        .y0(height - margin)
-        .y1((d) => d.y);
-
-      const g = svg.append('g');
-      const focusG = svg.append('g');
-      // .style('display', 'none');
-
-      g.append('path')
-        .attr('d', areaBar(drawData))
-        .style('fill', color);
-
-      const innerBar = focusG
-        .append('path')
-        .attr('class', 'active-rect')
-        .style('fill', 'red');
-
-      setFocusBar(innerBar)
-      const bisectDate = d3.bisector((d, x) => +moment.utc(d[xkey]).isBefore(x)).right;
-
-      const mouseDate = moment.utc(scaleX.invert(coords[0]));
-
-      const indexOfStartPoint = Math.min(
-        Math.max(bisectDate(appLoadPolygonChart, mouseDate), 0),
-        appLoadPolygonChart.length - 1,
+      ctx.fillRect(
+        offsetX,
+        minHeight ? Math.min(BAR_HEIGHT - 2, height) : height,
+        width,
+        minHeight ? 30 : BAR_HEIGHT - height,
       );
-      const hoveredPoint = appLoadPolygonChart[indexOfStartPoint];
-      const endPoint = appLoadPolygonChart[indexOfStartPoint - 1];
-      const focusPointsRange = [hoveredPoint, endPoint];
-      const activeArea: any = focusPointsRange.map((item) => ({
-        x: scaleX(moment.utc(item[xkey])) || 0,
-        y:
-          scaleY(isMetricExists(endPoint[ykey]) ? 0 : Math.max(maxY / 15, endPoint[ykey]) || 0) + margin || 0,
-      }));
-
-      innerBar.attr('d', areaBar(activeArea));
     };
 
-    console.log([coords, 0])
-
-    if (!coords) {
-      return
-    }
-    highlightBar([coords, 0]);
-  },[coords, svg]);
-
-  const drawGraph = (ref) => {
-    console.log('rerender')
-    if (!svg) {
-      console.log('no SVG!!!')
-      return
-    }
-
-    const xAxisLength = width - 2 * margin;
-    const yAxisLength = height - 2 * margin;
-
-    const [maxX, minX] = findXRange(appLoadPolygonChart);
-    const scaleX = scaleLinear()
-      .domain([minX, maxX])
-      .range([0, xAxisLength]);
-
-    const [maxY, minY] = findYRange(appLoadPolygonChart);
-    const scaleY = scaleLinear()
-      .domain([maxY, minY])
-      .range([0, yAxisLength]);
-
-    const drawData = appLoadPolygonChart.map((item) => ({
-      x: scaleX(moment.utc(item.timestamp)),
-      y: scaleY(isMetricExists(item[ykey]) ? 0 : Math.max(maxY / 15, item[ykey])) + margin || 0,
-    }));
-
-    const areaBar = area()
-      .curve(curveStepAfter)
-      .x((d) => d.x)
-      .y0(height - margin)
-      .y1((d) => d.y);
-
-    const g = svg.append('g');
-    const focusG = svg.append('g');
-    // .style('display', 'none');
-
-    g.append('path')
-      .attr('d', areaBar(drawData))
-      .style('fill', color);
-
-    // const focusBar = focusG
-    //   .append('path')
-    //   .attr('class', 'active-rect')
-    //   .style('fill', 'white');
-
-    // focusBar
-    //   .append('text')
-    //   .attr('id', 'focusText')
-    //   .attr('font-size', '10')
-    //   .attr('x', 1)
-    //   .attr('y', 8);
-
-    const bisectDate = d3.bisector((d, x) => +moment.utc(d[xkey]).isBefore(x)).right;
-
-    // const highlightBar = (coords) => {
-    //   if (!coords) {
-    //     return
-    //   }
-    //   const mouseDate = moment.utc(scaleX.invert(coords[0]));
-    //
-    //   const indexOfStartPoint = Math.min(
-    //     Math.max(bisectDate(appLoadPolygonChart, mouseDate), 0),
-    //     appLoadPolygonChart.length - 1,
-    //   );
-    //   const hoveredPoint = appLoadPolygonChart[indexOfStartPoint];
-    //   const endPoint = appLoadPolygonChart[indexOfStartPoint - 1];
-    //   const focusPointsRange = [hoveredPoint, endPoint];
-    //   const activeArea: any = focusPointsRange.map((item) => ({
-    //     x: scaleX(moment.utc(item[xkey])) || 0,
-    //     y:
-    //       scaleY(isMetricExists(endPoint[ykey]) ? 0 : Math.max(maxY / 15, endPoint[ykey]) || 0) + margin || 0,
-    //   }));
-    //
-    //   focusBar.attr('d', areaBar(activeArea));
-    // };
-
-    // highlightBar(coords)
-    svg.on('mousemove', () => {
-      const coords = d3.mouse(d3.event.currentTarget);
-      var evt = new CustomEvent('build', { detail: coords[0] });
-      // console.log('move',coords)
-      document.dispatchEvent(evt);
-      const mouseDate = moment.utc(scaleX.invert(coords[0]));
-
-      const indexOfStartPoint = Math.min(
-        Math.max(bisectDate(appLoadPolygonChart, mouseDate), 0),
-        appLoadPolygonChart.length - 1,
-      );
-      const hoveredPoint = appLoadPolygonChart[indexOfStartPoint];
-      const endPoint = appLoadPolygonChart[indexOfStartPoint - 1];
-      // const focusPointsRange = [hoveredPoint, endPoint];
-      // const activeArea: any = focusPointsRange.map((item) => ({
-      //   x: scaleX(moment.utc(item[xkey])) || 0,
-      //   y:
-      //     scaleY(isMetricExists(endPoint[ykey]) ? 0 : Math.max(maxY / 15, endPoint[ykey]) || 0) + margin || 0,
-      // }));
-
-      // focusBar.attr('d', areaBar(activeArea));
-
-
-      const value = isMetricExists(endPoint[ykey]) ? 0 : endPoint[ykey];
-      const dateToShow = moment(endPoint[xkey]).format('YYYY-MM-DD HH:mm:ss');
+    const createTooltip = (columnNumber) => {
+      const value = isMetricExists(appLoadPolygonChart[columnNumber][ykey])
+        ? 0
+        : appLoadPolygonChart[columnNumber][ykey];
+      const dateToShow = moment(appLoadPolygonChart[columnNumber][xkey]).format('YYYY-MM-DD HH:mm:ss');
 
       // eslint-disable-next-line max-len
-      const isTimeBased =
-        metricName.endsWith('_time') || metricName.endsWith('_wait') || metricName === 'load';
+      const isTimeBased = metricName.endsWith('_time') || metricName.endsWith('_wait') || metricName === 'load';
       const load = humanize.transform(value, 'number');
 
-      const dataTooltip = !value
-        ? `NA at ${dateToShow}`
-        : `${load} ${isTimeBased ? '' : '/ sec'} at ${dateToShow}`;
+      return !value ? `NA at ${dateToShow}` : `${load} ${isTimeBased ? '' : '/ sec'} at ${dateToShow}`;
+    };
 
-      setTooltip(dataTooltip);
+    sparklineCanvas.current.addEventListener('mousemove', (e) => {
+      const columnNumber = Math.floor(e.offsetX / (GRAPH_WIDTH / (drawData.length - 1));
+
+      setTooltip(createTooltip(columnNumber));
+
+      updateGraphs(columnNumber);
+
+      // ReactTooltip.show(sparklineCanvas);
+      ctx.clearRect(0, 0, 300, 30);
+      drawData.forEach((item, index) => {
+        drawBar(index, color);
+      });
+
+      drawBar(columnNumber, 'white', true);
     });
-    svg.on('mouseover', () => focusG.style('display', null));
-    svg.on('mouseout', () => {
-      // focusG.style('display', 'none');
-      var evt = new CustomEvent('build', { detail: NaN });
-      document.dispatchEvent(evt);
+
+    sparklineCanvas.current.addEventListener('mouseout', () => {
+      updateGraphs(NaN);
+      ctx.clearRect(0, 0, 300, 30);
+      drawData.forEach((item, index) => {
+        drawBar(index, color);
+      });
     });
 
-    // Create X axis
-    const xAxis = axisBottom(scaleX);
+    // sparklineCanvas.current.addEventListener('mousedown', (e) => {
+    //   const startPoint = e.offsetX;
+    //   let endPoint = null;
+    //
+    //   e.stopPropagation();
+    //   const mouseUpHandler = (e) => {
+    //     ctx.clearRect(startPoint, 0, endPoint - startPoint, BAR_HEIGHT);
+    //
+    //     const start = Math.floor(startPoint / (GRAPH_WIDTH / drawData.length));
+    //     const end = Math.floor(endPoint / (GRAPH_WIDTH / drawData.length));
+    //
+    //     const result = [appLoadPolygonChart[start], appLoadPolygonChart[end]]
+    //       .sort((a, b) => b.point - a.point)
+    //       .map((item) => item.timestamp);
+    //
+    //     console.log(result);
+    //     e.stopPropagation();
+    //     sparklineCanvas.current.removeEventListener('mouseup', mouseUpHandler);
+    //     sparklineCanvas.current.removeEventListener('mouseup', mouseMove);
+    //   };
+    //   const mouseMove = (e) => {
+    //     endPoint = e.offsetX;
+    //
+    //     // ctx.fillStyle = "rgba(150,150,150,0.3)";
+    //     // const height = drawData[barIndex].y;
+    //     // ctx.fillRect(startPoint, 0, endPoint - startPoint, BAR_HEIGHT);
+    //
+    //     e.stopPropagation();
+    //   };
+    //
+    //   sparklineCanvas.current.addEventListener('mouseup', mouseUpHandler);
+    //   sparklineCanvas.current.addEventListener('mousemove', mouseMove);
+    // });
+    ctx.clearRect(0, 0, 300, 30);
+    drawData.forEach((item, index) => {
+      drawBar(index, color);
+    });
 
-    svg
-      .append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(${margin},${height - margin - 1})`)
-      .call(xAxis);
-  };
+    const drawHighlighted = (e) => {
+      ctx.clearRect(0, 0, 300, 30);
+      drawData.forEach((item, index) => drawBar(index, color));
+
+      if (!e.detail) {
+        return;
+      }
+
+      const columnNumber = e.detail;
+
+      drawBar(columnNumber, 'blue');
+    };
+
+    document.addEventListener('sync-graphs', drawHighlighted, false);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      document.removeEventListener('sync-graphs', drawHighlighted);
+    };
+  }, [sparklineCanvas]);
 
   return (
     <>
-      <div ref={ref} className="d3-bar-chart-container" data-tip={tooltip} />
+      <canvas
+        ref={sparklineCanvas}
+        style={{ background: 'transparent' }}
+        width={GRAPH_WIDTH}
+        height={BAR_HEIGHT}
+        data-tip={tooltip}
+        data-for="test"
+      />
       <ReactTooltip
         data-qa="sparkline-tooltip"
         className="sparkline-tooltip"
         place="bottom"
         backgroundColor="#3274d9"
         arrowColor="#3274d9"
+        id="test"
+        getContent={() => {
+          // console.log('tooltip', tooltip)
+          return tooltip
+        }}
       />
     </>
   );
