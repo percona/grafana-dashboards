fcaec03dc0678a1885a467db7d22405715016c9e
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-08-07 14:00:09 +0200

    PMM-6372 update tests

--- pmm-app/src/pmm-settings/components/SignUp/SignUp.test.tsx
+++ pmm-app/src/pmm-settings/components/SignUp/SignUp.test.tsx
@@ -5,6 +5,15 @@ import { SignUp } from './SignUp';
 jest.mock('shared/components/helpers/notification-manager');
 
 describe('SignUp::', () => {
+  it('Should show a sign up form if an undefined email is passed', () => {
+    const root = mount(<SignUp userEmail={undefined} />);
+    const loggedInEmail = root.find('[data-qa="logged-in-email"]').at(0);
+    const submitFrom = root.find('[data-qa="sign-up-form"]').at(0);
+
+    expect(loggedInEmail).toHaveLength(0);
+    expect(submitFrom).toHaveLength(1);
+  });
+
   it('Should have the "Sign up" button disabled when first rendered', () => {
     const root = mount(<SignUp userEmail={undefined} />);
     const submitButton = root.find('[data-qa="sign-up-submit-button"]').at(0);
@@ -14,8 +23,8 @@ describe('SignUp::', () => {
 
   it('Should show a page saying that the user is logged in if a not undefined email is passed', () => {
     const root = mount(<SignUp userEmail="test@example.com" />);
-    const submitButton = root.find('[data-qa="sign-up-submit-button"]').at(0);
+    const loggedInEmail = root.find('[data-qa="logged-in-email"]').at(0);
 
-    expect(submitButton.props()).toHaveProperty('disabled');
+    expect(loggedInEmail).toHaveLength(1);
   });
 });
