c160a7337eb28b94d8e372381f5aed7303d6b197
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-08-08 11:15:56 +0200

    PMM-6372 update FieldAdapters.styles.ts

--- pmm-app/src/pmm-settings/components/SignUp/FieldAdapters/FieldAdapters.styles.ts
+++ pmm-app/src/pmm-settings/components/SignUp/FieldAdapters/FieldAdapters.styles.ts
@@ -5,9 +5,9 @@ import { stylesFactory } from '@grafana/ui';
 export const getStyles = stylesFactory((theme: GrafanaTheme) => ({
   errorMessage: css`
     color: ${(theme.colors as any).red};
-    font-size: 10px;
-    height: 10px;
-    line-height: 1;
-    margin-top: 3px;
+    font-size: ${theme.typography.size.xs};
+    height: ${theme.typography.size.xs};
+    line-height: ${theme.typography.lineHeight.xs};
+    margin-top: ${theme.spacing.xs};
   `,
 }));
