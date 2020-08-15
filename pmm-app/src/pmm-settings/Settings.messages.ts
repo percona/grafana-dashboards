f1480910e39cda89a874ae16299360df9363ad6c
Tiago Mota Santos <tiago.mota@percona.com>
2020-08-15 14:36:29 +0100

    PMM-6383 Change diagnostics button message

--- pmm-app/src/pmm-settings/Settings.messages.ts
+++ pmm-app/src/pmm-settings/Settings.messages.ts
@@ -26,7 +26,7 @@ export const Messages = {
     urlTooltip: 'The URL of the external Alertmanager to use.'
   },
   diagnostics: {
-    action: 'Download PMM Server Logs',
+    action: 'Download server diagnostics data',
     label: 'Diagnostics',
     tooltip: 'You can download server logs to make the problem detection simpler. Please include this file if you are submitting a bug report.',
   },
