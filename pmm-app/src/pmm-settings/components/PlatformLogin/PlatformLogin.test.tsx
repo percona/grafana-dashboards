acb1f808ce90d22fc3d275bc2ce449e8b6e37c27
Roman Misyurin <roman.misyurin@percona.com>
2020-08-19 00:24:49 +0300

    PMM-6373 add sign in actions

--- pmm-app/src/pmm-settings/components/PlatformLogin/PlatformLogin.test.tsx
+++ pmm-app/src/pmm-settings/components/PlatformLogin/PlatformLogin.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { SignUp } from './SignUp';
+import { PlatformLogin } from './PlatformLogin';
 import { LoggedIn } from './LoggedIn/LoggedIn';
 
 jest.mock('shared/components/helpers/notification-manager');
 
 describe('SignUp::', () => {
   it('Should show a sign up form if an undefined email is passed', () => {
-    const root = mount(<SignUp userEmail={undefined} />);
+    const root = mount(<PlatformLogin userEmail={undefined} />);
     const loggedInEmail = root.find(LoggedIn).at(0);
     const submitFrom = root.find('[data-qa="sign-up-form"]').at(0);
 
@@ -16,14 +16,14 @@ describe('SignUp::', () => {
   });
 
   it('Should have the "Sign up" button disabled when first rendered', () => {
-    const root = mount(<SignUp userEmail={undefined} />);
+    const root = mount(<PlatformLogin userEmail={undefined} />);
     const submitButton = root.find('[data-qa="sign-up-submit-button"]').at(0);
 
     expect(submitButton.props()).toHaveProperty('disabled');
   });
 
   it('Should show a page saying that the user is logged in if a not undefined email is passed', () => {
-    const root = mount(<SignUp userEmail="test@example.com" />);
+    const root = mount(<PlatformLogin userEmail="test@example.com" />);
     const loggedInEmail = root.find(LoggedIn).at(0);
 
     expect(loggedInEmail).toHaveLength(1);
