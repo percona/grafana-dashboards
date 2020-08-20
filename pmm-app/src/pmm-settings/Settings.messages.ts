15495e610149b8d8d1b62a91177a7cf0ee59e1ba
Roman Misyurin <roman.misyurin@percona.com>
2020-08-20 18:25:40 +0300

    PMM-6373 review fixes

--- pmm-app/src/pmm-settings/Settings.messages.ts
+++ pmm-app/src/pmm-settings/Settings.messages.ts
@@ -61,7 +61,7 @@ export const Messages = {
     advanced: 'Advanced settings',
     ssh: 'SSH key',
     alertManager: 'Alertmanager integration',
-    signUp: 'Percona Platform',
+    perconaPlatform: 'Percona Platform',
   },
   tooltipLinkText: 'Read more'
 };
