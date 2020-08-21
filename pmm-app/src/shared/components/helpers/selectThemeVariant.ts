668d5aeb3b95255187b9e86deb993c9bb7b99c6b
Roman Misyurin <roman.misyurin@percona.com>
2020-08-21 20:58:24 +0300

    styles

---
+++ pmm-app/src/shared/components/helpers/selectThemeVariant.ts
@@ -0,0 +1,21 @@
+import { selectThemeVariant } from '@grafana/ui';
+
+interface Themes {
+  mainTextColor: string;
+}
+export const getThemeParameters = (theme): Themes => {
+  const mainTextColor = selectThemeVariant(
+    // @ts-ignore
+    { light: theme.colors.text, dark: 'rgba(255, 255, 255, 0.8)' },
+    theme.type,
+  );
+  const themeBasicColor = selectThemeVariant({ light: 'blue', dark: 'rgba(255, 255, 255, 0.8)' }, theme.type);
+  const tableBackgroundColor = selectThemeVariant({ light: 'rgb(247, 247, 249)', dark: '#161719' }, theme.type);
+  // @ts-ignore
+  const tableBorderColor = selectThemeVariant({ light: theme.colors.gray85, dark: '#292929' }, theme.type);
+  const tableHeaderBackground = selectThemeVariant({ light: 'rgb(247, 247, 249)', dark: '#3D3D3D' }, theme.type);
+
+  return {
+    mainTextColor: themeBasicColor,
+  };
+};
