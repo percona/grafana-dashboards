19cd4c4714aa43b7804a0c33fe386694e4954800
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-08-21 17:55:03 +0200

    PMM-6373 revert test for LoggedIn

--- pmm-app/src/pmm-settings/components/PlatformLogin/LoggedIn/LoggedIn.test.tsx
+++ pmm-app/src/pmm-settings/components/PlatformLogin/LoggedIn/LoggedIn.test.tsx
@@ -7,7 +7,7 @@ jest.mock('shared/components/helpers/notification-manager');
 describe('LoggedIn::', () => {
   it('Should show the passed email correctly', () => {
     const testEmail = 'test@email';
-    const root = shallow(<LoggedIn email={testEmail} getSettings={jest.fn()} />);
+    const root = shallow(<LoggedIn email={testEmail} />);
 
     expect(root.find('[data-qa="logged-in-email"]').text()).toEqual(testEmail);
   });
