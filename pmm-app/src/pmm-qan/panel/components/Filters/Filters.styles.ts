89d8b784bca4641b15b320ba0075fd3d33357490
Alex Tymchuk <alexander.tymchuk@percona.com>
2020-08-21 14:59:13 +0300

    PMM-5126 remove redundant styles

--- pmm-app/src/pmm-qan/panel/components/Filters/Filters.styles.ts
+++ pmm-app/src/pmm-qan/panel/components/Filters/Filters.styles.ts
@@ -10,7 +10,6 @@ export const styles = {
   filtersContentArea: css``,
   filtersField: css`
     width: 100%;
-    // border: 1px solid rgba(255, 255, 255, 0.8) !important;
   `,
   filtersHeader: css`
     display: flex;
