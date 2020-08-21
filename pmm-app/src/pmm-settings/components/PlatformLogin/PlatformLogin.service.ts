f6162af91c11b7ba1da3ef78fe0084e04249c4e3
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-08-21 14:21:22 +0200

    -m

--- pmm-app/src/pmm-settings/components/PlatformLogin/PlatformLogin.service.ts
+++ pmm-app/src/pmm-settings/components/PlatformLogin/PlatformLogin.service.ts
@@ -3,9 +3,12 @@ import { Credentials } from './types';
 
 export const PlatformLoginService = {
   signUp(credentials: Credentials): Promise<void> {
-    return apiRequest.post<any, Credentials>('/v1/Platform/SignUp', credentials);
+    return apiRequest.post<void, Credentials>('/v1/Platform/SignUp', credentials);
   },
   signIn(credentials: Credentials): Promise<void> {
-    return apiRequest.post<any, Credentials>('/v1/Platform/SignIn', credentials);
+    return apiRequest.post<void, Credentials>('/v1/Platform/SignIn', credentials);
+  },
+  signOut(): Promise<void> {
+    return apiRequest.post<void, {}>('/v1/Platform/SignOut', {});
   },
 };
