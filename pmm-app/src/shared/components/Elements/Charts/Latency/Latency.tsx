3d9ae9a561b3a6ebaced2ba737ae00526f6c570b
Roman Misyurin <roman.misyurin@percona.com>
2020-08-22 21:40:09 +0300

    PMM-6200 cleanup

--- pmm-app/src/shared/components/Elements/Charts/Latency/Latency.tsx
+++ pmm-app/src/shared/components/Elements/Charts/Latency/Latency.tsx
@@ -1,64 +1,54 @@
 // @ts-nocheck
-import React, { Component } from 'react';
+import React, {
+  useEffect, useRef, useState
+} from 'react';
 import * as d3 from 'd3';
 import { scaleLog } from 'd3';
 import Tooltip from 'antd/es/tooltip';
 import { humanize } from '../../../helpers/Humanization';
-import './Latency.scss';
+import { getStyles } from './Latency.styles';
 
-interface LatencyChartState {
-  tooltip: string | undefined;
-}
+export const Latency = (props) => {
+  const styles = getStyles();
+  let measurement;
+  const width = 200;
+  const { className, data = [] } = props;
+  const [tooltip, setTooltip] = useState('');
+  const graphContainer = useRef(null);
 
-export class Latency extends Component<any, LatencyChartState> {
-  private width: number;
+  useEffect(() => {
+    if (!graphContainer.current) {
+      return;
+    }
 
-  private data: any[] | any;
+    drawChart(data);
+  }, []);
 
-  private measurement: string | undefined;
-
-  private dataTooltip: string | undefined;
-
-  constructor(props: any) {
-    super(props as any);
-    this.width = 200;
-    this.data = props.data || [];
-    this.state = {
-      tooltip: '',
-    };
-  }
-
-  componentDidMount() {
-    this.drawChart(this.data);
-    this.setState({ tooltip: this.dataTooltip });
-  }
-
-  drawChart(data) {
+  const drawChart = (data) => {
     // eslint-disable-next-line react/no-string-refs
-    const chart = d3.select(this.refs.graphContainer);
+    const chart = d3.select(graphContainer.current);
 
     chart.selectAll('*').remove();
-
     const svg = chart
       .append('svg')
       .attr('height', '20')
-      .attr('width', this.width);
+      .attr('width', width);
 
     const x = scaleLog()
       .domain([0.00001, 10000])
-      .range([2, this.width - 2])
+      .range([2, width - 2])
       .clamp(true)
       .nice();
 
     const {
       min = 0, max = 0, avg = 0, p99 = 0
     } = data;
-    const minStr = `⌜ Min: ${humanize.transform(min, this.measurement)}`;
-    const maxStr = `⌟ Max: ${humanize.transform(max, this.measurement)}`;
-    const avgStr = `◦ Avg: ${humanize.transform(avg, this.measurement)}`;
-    const p99Str = `${p99 ? `• 99%: ${humanize.transform(p99, this.measurement)}` : ''}`;
+    const minStr = `⌜ Min: ${humanize.transform(min, measurement)}`;
+    const maxStr = `⌟ Max: ${humanize.transform(max, measurement)}`;
+    const avgStr = `◦ Avg: ${humanize.transform(avg, measurement)}`;
+    const p99Str = `${p99 ? `• 99%: ${humanize.transform(p99, measurement)}` : ''}`;
 
-    this.dataTooltip = `${minStr}\n${maxStr}\n${avgStr}\n${p99Str}`.trim();
+    setTooltip(`${minStr}\n${maxStr}\n${avgStr}\n${p99Str}`.trim());
 
     const g = svg.append('g');
 
@@ -68,7 +58,7 @@ export class Latency extends Component<any, LatencyChartState> {
       .attr('x1', '0')
       .attr('stroke-dasharray', '1, 1')
       .attr('y1', '13px')
-      .attr('x2', this.width)
+      .attr('x2', width)
       .attr('y2', '13px');
 
     // hrLine
@@ -110,28 +100,22 @@ export class Latency extends Component<any, LatencyChartState> {
         .attr('cx', `${x(p99)}`)
         .attr('cy', '13px');
     }
-  }
+  };
 
-  render() {
-    /* eslint-disable react/no-string-refs */
-    const { className } = this.props;
-    const { tooltip } = this.state;
-
-    return (
-      <>
-        <Tooltip
-          getPopupContainer={() => document.querySelector('#antd') || document.body}
-          placement="left"
-          overlayClassName="latency-tooltip"
-          title={(
-            <pre style={{ backgroundColor: 'transparent', border: 'none' }}>
-              <span>{tooltip}</span>
-            </pre>
-          )}
-        >
-          <div className={`${className || ''} d3-bar-chart-container`} ref="graphContainer" data-tip="" />
-        </Tooltip>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Tooltip
+        getPopupContainer={() => document.querySelector('#antd') || document.body}
+        placement="left"
+        overlayClassName={styles.latencyTooltip}
+        title={(
+          <pre style={{ backgroundColor: 'transparent', border: 'none' }}>
+            <span>{tooltip}</span>
+          </pre>
+        )}
+      >
+        <div className={`${className || ''} d3-bar-chart-container`} ref={graphContainer} data-tip="" />
+      </Tooltip>
+    </>
+  );
+};
