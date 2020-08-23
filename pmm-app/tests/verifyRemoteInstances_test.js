8dba17d9b0b973ab96bcf54076a076f77bef976f
Ivana Brecherová <63704168+IvanaBrecherova@users.noreply.github.com>
2020-08-23 15:59:15 +0200

    PMM-6458-Add UI tests for Inventory Sorting (#911)
    * PMM-T371 Preporation
    
    * get array of values
    
    * PMM-T371 Done
    
    * fix
    
    * PMM-T371 done for all tabs
    
    * fix comments
    
    * Move test to new file
    
    * refactoring of loops
    
    * Fix PR  comment
    
    * PMM-6458 simplify the sort test
    
    * PMM-6458 break to stop the loop if error
    
    * PMM-6458 fix the scope modifier
    
    * PMM-6458 better failure message
    
    * Fix
    
    * Fix lint issues
    
    Co-authored-by: Alex <alexander.tymchuk@gmail.com>
    Co-authored-by: Puneet Kala <puneet.0191@gmail.com>
    Co-authored-by: Alexander Tymchuk <alexander.tymchuk@percona.com>

--- pmm-app/tests/verifyRemoteInstances_test.js
+++ pmm-app/tests/verifyRemoteInstances_test.js
@@ -13,136 +13,26 @@ Before(async (I) => {
 });
 
 // TODO: unskip the mongodb tests after resolving a creds issue
-Data(instances.filter((instance) => instance.name !== 'mongodb'))
-  .Scenario(
-    'Verify Remote Instance Addition [critical] @not-pr-pipeline',
-    async (I, remoteInstancesPage, current) => {
-      const serviceName = remoteInstancesPage.services[current.name];
+Data(instances.filter((instance) => instance.name !== 'mongodb')).Scenario(
+  'Verify Remote Instance Addition [critical] @not-pr-pipeline',
+  async (I, remoteInstancesPage, current) => {
+    const serviceName = remoteInstancesPage.services[current.name];
 
-      I.amOnPage(remoteInstancesPage.url);
-      remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
-      remoteInstancesPage.openAddRemotePage(current.name);
-      remoteInstancesPage.fillRemoteFields(serviceName);
-      remoteInstancesPage.createRemoteInstance(serviceName);
-    }
-  );
-
-Data(instances.filter((instance) => instance.name !== 'mongodb'))
-  .Scenario(
-    'Verify Remote Instance has Status Running [critical] @not-pr-pipeline',
-    async (I, remoteInstancesPage, pmmInventoryPage, current) => {
-      const serviceName = remoteInstancesPage.services[current.name];
-
-      I.amOnPage(pmmInventoryPage.url);
-      pmmInventoryPage.verifyRemoteServiceIsDisplayed(serviceName);
-      await pmmInventoryPage.verifyAgentHasStatusRunning(serviceName);
-    }
-  );
-
-Scenario(
-  'PMM-T339 - Verify MySQL service is removed on PMM Inventory page @not-pr-pipeline',
-  async (I, addInstanceAPI, pmmInventoryPage) => {
-    const serviceType = 'MySQL';
-    const serviceName = 'ServiceToDelete';
-
-    await addInstanceAPI.apiAddInstance(serviceType, serviceName);
-    I.amOnPage(pmmInventoryPage.url);
-    const serviceId = pmmInventoryPage.getServicesId(serviceName);
-
-    pmmInventoryPage.selectService(serviceName);
-    I.click(pmmInventoryPage.fields.deleteButton);
-    I.click(pmmInventoryPage.fields.proceedButton);
-    pmmInventoryPage.serviceExists(serviceName, false);
-    pmmInventoryPage.selectService(serviceName);
-    pmmInventoryPage.deleteWithForceOpt();
-    pmmInventoryPage.serviceExists(serviceName, true);
-    I.click(pmmInventoryPage.fields.agentsLink);
-    await pmmInventoryPage.getCountOfAgents(serviceId);
-    I.click(pmmInventoryPage.fields.nodesLink);
-    pmmInventoryPage.checkNodeExists(serviceName);
-  }
+    I.amOnPage(remoteInstancesPage.url);
+    remoteInstancesPage.waitUntilRemoteInstancesPageLoaded();
+    remoteInstancesPage.openAddRemotePage(current.name);
+    remoteInstancesPage.fillRemoteFields(serviceName);
+    remoteInstancesPage.createRemoteInstance(serviceName);
+  },
 );
 
-Scenario(
-  'PMM-T340 - Verify node with agents, services can be removed on PMM Inventory page @not-pr-pipeline',
-  async (I, addInstanceAPI, pmmInventoryPage) => {
-    const serviceType = 'MySQL';
-    const serviceName = 'NodeToDelete';
-
-    await addInstanceAPI.apiAddInstance(serviceType, serviceName);
-    I.amOnPage(pmmInventoryPage.url);
-    const serviceId = pmmInventoryPage.getServicesId(serviceName);
-
-    I.waitForVisible(pmmInventoryPage.fields.nodesLink, 30);
-    I.click(pmmInventoryPage.fields.nodesLink);
-    pmmInventoryPage.selectService(serviceName);
-    pmmInventoryPage.deleteWithForceOpt();
-    I.click(pmmInventoryPage.fields.pmmServicesSelector);
-    pmmInventoryPage.serviceExists(serviceName, true);
-    I.click(pmmInventoryPage.fields.agentsLink);
-    await pmmInventoryPage.getCountOfAgents(serviceId);
-  }
-);
-
-Scenario(
-  'PMM-T342 - Verify pmm-server node cannot be removed from PMM Inventory page @not-pr-pipeline',
-  async (I, pmmInventoryPage) => {
-    const node = 'pmm-server';
+Data(instances.filter((instance) => instance.name !== 'mongodb')).Scenario(
+  'Verify Remote Instance has Status Running [critical] @not-pr-pipeline',
+  async (I, remoteInstancesPage, pmmInventoryPage, current) => {
+    const serviceName = remoteInstancesPage.services[current.name];
 
     I.amOnPage(pmmInventoryPage.url);
-    I.waitForVisible(pmmInventoryPage.fields.nodesLink, 30);
-    I.click(pmmInventoryPage.fields.nodesLink);
-    pmmInventoryPage.selectService(node);
-    pmmInventoryPage.deleteWithForceOpt();
-    pmmInventoryPage.checkNodeExists(node);
-  }
-);
-
-Scenario(
-  'PMM-T343 - Verify agent can be removed on PMM Inventory page @not-pr-pipeline',
-  async (I, pmmInventoryPage) => {
-    const agentType = 'MySQL exporter';
-
-    I.amOnPage(pmmInventoryPage.url);
-    I.waitForVisible(pmmInventoryPage.fields.nodesLink, 20);
-    I.click(pmmInventoryPage.fields.nodesLink);
-    const countOfNodesBefore = await pmmInventoryPage.getNodeCount();
-
-    I.waitForVisible(pmmInventoryPage.fields.agentsLink, 20);
-    I.click(pmmInventoryPage.fields.agentsLink);
-    const serviceId = await pmmInventoryPage.getAgentServiceID(agentType);
-    const agentId = await pmmInventoryPage.getAgentID(agentType);
-
-    pmmInventoryPage.selectAgent(agentType);
-    I.click(pmmInventoryPage.fields.deleteButton);
-    I.click(pmmInventoryPage.fields.proceedButton);
-    pmmInventoryPage.existsByid(agentId, true);
-    I.click(pmmInventoryPage.fields.nodesLink);
-    const countOfNodesAfter = await pmmInventoryPage.getNodeCount();
-
-    pmmInventoryPage.verifyNodesCount(countOfNodesBefore, countOfNodesAfter);
-    I.click(pmmInventoryPage.fields.pmmServicesSelector);
-    pmmInventoryPage.existsByid(serviceId, false);
-  }
-);
-
-Scenario(
-  'PMM-T345 - Verify removing pmm-agent on PMM Inventory page removes all associated agents @not-pr-pipeline',
-  async (I, pmmInventoryPage) => {
-    const agentID = 'pmm-server';
-    const agentType = 'PMM Agent';
-
-    I.amOnPage(pmmInventoryPage.url);
-    I.waitForVisible(pmmInventoryPage.fields.agentsLink, 20);
-    I.click(pmmInventoryPage.fields.agentsLink);
-    pmmInventoryPage.selectAgentByID(agentID);
-    pmmInventoryPage.deleteWithForceOpt();
-    pmmInventoryPage.existsByid(agentID, false);
-    pmmInventoryPage.selectAgent(agentType);
-    const agentIDToDelete = await pmmInventoryPage.getAgentID(agentType);
-
-    pmmInventoryPage.deleteWithForceOpt();
-    pmmInventoryPage.existsByid(agentIDToDelete, true);
-    await pmmInventoryPage.checkAllNotDeletedAgents(agentID);
-  }
+    pmmInventoryPage.verifyRemoteServiceIsDisplayed(serviceName);
+    await pmmInventoryPage.verifyAgentHasStatusRunning(serviceName);
+  },
 );
