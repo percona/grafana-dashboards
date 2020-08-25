4e2b578ac6cd653476d5590c2103a1a0d3192921
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-08-25 19:44:57 +0200

    PMM-6276 Update Tabs.styles.ts

--- pmm-app/src/pmm-inventory/Tabs/Tabs.styles.ts
+++ pmm-app/src/pmm-inventory/Tabs/Tabs.styles.ts
@@ -8,6 +8,28 @@ export const styles = {
   `,
   tableWrapper: css`
     padding: 10px;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+
+    thead tr th {
+      position: sticky;
+      top: 0;
+      z-index: 1;
+    }
+  `,
+  table: css`
+    height: 100%;
+
+    & > :first-child {
+      height: 100%;
+      overflow-y: auto;
+      border: none;
+    }
+  `,
+  tableInnerWrapper: css`
+    flex: 1;
+    overflow: hidden;
   `,
   destructiveButton: css`
     background: rgba(0, 0, 0, 0) linear-gradient(rgb(224, 47, 68) 0%, rgb(196, 22, 42) 100%) repeat scroll 0%
