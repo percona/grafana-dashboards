a3cfbe61eec65627dbecef5f4c6e0942e7ef33f9
Roman Misyurin <roman.misyurin@percona.com>
2020-09-01 13:13:59 +0300

    PMM-6569 add scrollbar to Details

--- pmm-app/src/pmm-qan/panel/QueryAnalytics.styles.ts
+++ pmm-app/src/pmm-qan/panel/QueryAnalytics.styles.ts
@@ -6,46 +6,48 @@ import { getPmmTheme } from 'shared/components/helpers/getPmmTheme';
 export const getStyles = stylesFactory((theme: GrafanaTheme) => {
   const parameters = getPmmTheme(theme);
 
-
   return {
     overviewHeader: css`
-    display: flex;
-    justify-content: flex-end;
-    padding: 13px 2px 5px 0px;
-    height: 50px;
-  `,
+      display: flex;
+      justify-content: flex-end;
+      padding: 13px 2px 5px 0px;
+      height: 50px;
+    `,
     overviewFooter: css`
-    display: flex;
-    justify-content: flex-start;
-    padding: 13px 2px 5px 0px;
-    height: 50px;
-  `,
+      display: flex;
+      justify-content: flex-start;
+      padding: 13px 2px 5px 0px;
+      height: 50px;
+    `,
     splitterWrapper: css`
-    height: 1000px;
-    position: relative;
-  `,
+      height: 1200px;
+      position: relative;
+    `,
+    detailsWrapper: css`
+      height: 100%;
+    `,
     paginationWrapper: css`
-    display: flex;
-    align-items: center;
+      display: flex;
+      align-items: center;
 
-    .ant-pagination-item-link,
-     .ant-select-selection,
-     svg,
-     a {
-      background-color: ${parameters.table.backgroundColor} !important;
-      color: ${parameters.mainTextColor} !important;
-    }
-  `,
+      .ant-pagination-item-link,
+      .ant-select-selection,
+      svg,
+      a {
+        background-color: ${parameters.table.backgroundColor} !important;
+        color: ${parameters.mainTextColor} !important;
+      }
+    `,
     showTotal: css`
-    color: ${parameters.mainTextColor};
-  `,
+      color: ${parameters.mainTextColor};
+    `,
     tableWrapper: css`
-    min-height: 1000px;
-  `,
+      min-height: 1000px;
+    `,
     getContainerWrapper: (size) => css`
-    width: ${(size || 1500) - 260}px;
-    height: 100%;
-    position: relative;
-  `,
+      width: ${(size || 1500) - 260}px;
+      height: 100%;
+      position: relative;
+    `,
   };
 });
