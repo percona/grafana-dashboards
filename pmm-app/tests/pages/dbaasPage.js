324562e278c06366a8bf99d304178b6507d4f143
Ivana Wzientková <ivana.wzientkova@percona.com>
2020-09-11 12:34:59 +0200

    Done T426

--- pmm-app/tests/pages/dbaasPage.js
+++ pmm-app/tests/pages/dbaasPage.js
@@ -1,8 +1,19 @@
+const { I } = inject();
+
 module.exports = {
   url: 'graph/d/pmm-dbaas/dbaas?orgId=1',
+  positiveAlertText: 'Cluster was successfully added',
   fields: {
     addKubernetesClusterButton: "//span[contains(text(), 'Add new Kubernetes Cluster')]",
     modalWindow: '$modal-body',
     closeButton: '$modal-close-button',
+    kubernetesClusterNameInput: '$kubernetes-cluster-name-field',
+    kubeconfigFileInput: '$kubernetes-kubeconfig-field',
+    kubernetesAddButton: '$kubernetes-add-cluster-button',
+  },
+
+  checkAddedCluster(cluserName) {
+    const clusterLocator = `//td[contains(text(), '${cluserName}')]`;
+    I.seeElement(clusterLocator);
   },
 };
--- pmm-app/tests/verifyDBaaS_test.js
+++ pmm-app/tests/verifyDBaaS_test.js
@@ -5,10 +5,22 @@ Before(async (I) => {
 });
 
 Scenario('PMM-T426 - Verify adding new Kubernetes cluster@not-pr-pipeline', async (I, dbaasPage) => {
+  const clusterName = 'Kubernetes_Testing_Cluster';
   I.amOnPage(dbaasPage.url);
   I.waitForVisible(dbaasPage.fields.addKubernetesClusterButton, 30);
   I.click(dbaasPage.fields.addKubernetesClusterButton);
   I.seeElement(dbaasPage.fields.modalWindow);
   I.click(dbaasPage.fields.closeButton);
   I.dontSeeElement(dbaasPage.fields.modalWindow);
+  I.click(dbaasPage.fields.addKubernetesClusterButton);
+  I.seeElement(dbaasPage.fields.modalWindow);
+  I.pressKey('Escape');
+  I.dontSeeElement(dbaasPage.fields.modalWindow);
+  //need add close by clicking out of the modal window
+  I.click(dbaasPage.fields.addKubernetesClusterButton);
+  I.fillField(dbaasPage.fields.kubernetesClusterNameInput, clusterName);
+  I.fillField(dbaasPage.fields.kubeconfigFileInput, 'Kubernetes_Config_Test');
+  I.click(dbaasPage.fields.kubernetesAddButton);
+  I.see(dbaasPage.positiveAlertText);
+  dbaasPage.checkAddedCluster(clusterName);
 });
