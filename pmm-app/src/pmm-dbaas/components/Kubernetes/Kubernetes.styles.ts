6a6651ab752f25801db2a933e516665a36cb7d92
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2020-09-22 14:25:25 +0300

    PMM-6492 add clusters (#942)
    * PMM-6492 add dbaas panel
    
    * PMM-6469 Refactor process promise results to shared
    
    * PMM-6469 Update DBaaS panel and dashboard
    
    * PMM-6469 Kubernetes cluster inventory (list and delete)
    
    * PMM-6469 Fix checkbox styles and add unit tests
    
    * PMM-6469 Fix lint errors
    
    * PMM-6469 Remove unused datasource and collapse
    
    * PMM-6469 Refactor toModelList
    
    * PMM-6469 Add and use pluralize function
    
    * PMM-6469 Export promises helper types and add filterRejected
    
    * PMM-6469 Add promises helper tests
    
    * PMM-6469 Fix rejected promises error
    
    * PMM-6469 Remove mass delete clusters
    
    * PMM-6469 Remove PMM tag from DBaaS
    
    * PMM-6469 Memoize styles
    
    * PMM-6469 Make Table text compatible with light theme
    
    * PMM-6492 add modal to kubernetes clusters
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 fix review issues
    
    * PMM-6492 rebase & upgrade stubs
    
    * PMM-6492 UX improvements
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 review improvements
    
    * PMM-6492 add modal to kubernetes clusters
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 disable button
    
    * PMM-6492 fix lint issue
    
    * PMM-6573 Switch DBaaS (#964)
    
    * PMM-6492 add dbaas panel
    
    * PMM-6469 Refactor process promise results to shared
    
    * PMM-6469 Update DBaaS panel and dashboard
    
    * PMM-6469 Kubernetes cluster inventory (list and delete)
    
    * PMM-6469 Fix checkbox styles and add unit tests
    
    * PMM-6469 Fix lint errors
    
    * PMM-6469 Remove unused datasource and collapse
    
    * PMM-6469 Refactor toModelList
    
    * PMM-6469 Add and use pluralize function
    
    * PMM-6469 Export promises helper types and add filterRejected
    
    * PMM-6469 Add promises helper tests
    
    * PMM-6469 Fix rejected promises error
    
    * PMM-6469 Remove mass delete clusters
    
    * PMM-6469 Remove PMM tag from DBaaS
    
    * PMM-6469 Memoize styles
    
    * PMM-6469 Make Table text compatible with light theme
    
    * PMM-6492 add modal to kubernetes clusters
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 fix review issues
    
    * PMM-6492 rebase & upgrade stubs
    
    * PMM-6492 UX improvements
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 review improvements
    
    * PMM-6492 add modal to kubernetes clusters
    
    * PMM-6492 add cluster window added
    
    * PMM-6492 disable button
    
    * PMM-6492 fix lint issue
    
    * PMM-6573 Add DBaaS read-only option to settings
    
    * PMM-6573 Fix dbaas settings property
    
    Co-authored-by: Roman Misyurin <roman.misyurin@percona.com>
    
    Co-authored-by: Tiago Mota Santos <tiago.mota@percona.com>
    Co-authored-by: Tiago Mota Santos <tiago.a.mota@hotmail.com>

---
+++ pmm-app/src/pmm-dbaas/components/Kubernetes/Kubernetes.styles.ts
@@ -0,0 +1,20 @@
+import { css } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+
+export const getStyles = (theme: GrafanaTheme) => ({
+  actionPanel: css`
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: ${theme.spacing.sm};
+  `,
+  actionsColumn: css`
+    display: flex;
+    justify-content: center;
+  `,
+  tableWrapper: css`
+    padding: ${theme.spacing.md};
+  `,
+  deleteModalContent: css`
+    margin-bottom: ${theme.spacing.xl};
+  `,
+});
---
+++ pmm-app/src/pmm-dbaas/components/Kubernetes/KubernetesInventory.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { KubernetesInventory } from './KubernetesInventory';
+import { kubernetesStub, deleteActionStub } from './__mocks__/kubernetesStubs';
+
+jest.mock('shared/components/helpers/notification-manager');
+jest.mock('./Kubernetes.hooks');
+
+describe('KubernetesInventory::', () => {
+  it('renders table correctly', () => {
+    const root = mount(<KubernetesInventory />);
+    const rows = root.find('tr');
+
+    expect(rows.length).toBe(kubernetesStub.length + 1);
+  });
+  it('cancels delete cluster', () => {
+    const root = mount(<KubernetesInventory />);
+    const openDeleteModalButton = root.find('[data-qa="open-delete-modal-button"]').find('button').at(0);
+
+    openDeleteModalButton.simulate('click');
+
+    const cancelButton = root.find('[data-qa="cancel-delete-kubernetes-button"]').find('button');
+
+    cancelButton.simulate('click');
+
+    expect(root.find('tr').length).toBe(kubernetesStub.length + 1);
+    expect(deleteActionStub).toHaveBeenCalledTimes(0);
+  });
+  it('deletes cluster correctly', () => {
+    const root = mount(<KubernetesInventory />);
+    const openDeleteModalButton = root.find('[data-qa="open-delete-modal-button"]').find('button').at(0);
+
+    openDeleteModalButton.simulate('click');
+
+    const deleteButton = root.find('[data-qa="delete-kubernetes-button"]').find('button');
+
+    deleteButton.simulate('click');
+
+    expect(deleteActionStub).toHaveBeenCalled();
+  });
+});
