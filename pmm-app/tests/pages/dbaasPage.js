4cf29ee59d06b80fadabae77eb531a22b944fc72
Ivana Brecherová <63704168+IvanaBrecherova@users.noreply.github.com>
2020-09-23 12:22:30 +0200

    PMM-6626 DBaaS Cluster UI tests (#965)
    * New classes for DBaasPage and preporation for T426
    
    * Done T426
    
    * PMM-T427 - not done yet
    
    * PMM-T427 Done
    
    * PMM-T428 and PMM-T431 done
    
    * fix unused
    
    * Added check for deleted cluster
    
    * PMM-T427 Done
    
    * formating fixed

---
+++ pmm-app/tests/pages/dbaasPage.js
@@ -0,0 +1,51 @@
+const { I } = inject();
+
+module.exports = {
+  url: 'graph/d/pmm-dbaas/dbaas?orgId=1',
+  addedAlertMessage: 'Cluster was successfully added',
+  confirmDeleteText: 'Are you sure that you want to permanently delete this cluster?',
+  deletedAlertMessage: 'Cluster successfully deleted',
+  fields: {
+    addKubernetesClusterButton: "//span[contains(text(), 'Add new Kubernetes Cluster')]",
+    addKubernetesClusterButtonInTable:
+      "//div[@data-qa='table-no-data']//span[contains(text(), 'Add new Kubernetes Cluster')]",
+    modalWindow: '$modal-body',
+    closeButton: '$modal-close-button',
+    disabledAddButton: '//button[@data-qa="kubernetes-add-cluster-button" and @disabled]',
+    kubernetesClusterNameInput: '$kubernetes-cluster-name-field',
+    kubeconfigFileInput: '$kubernetes-kubeconfig-field',
+    kubernetesAddButton: '$kubernetes-add-cluster-button',
+    requiredField: "//div[contains(text(), 'Required field')]",
+    proceedButton: '$delete-kubernetes-button',
+  },
+
+  checkCluster(cluserName, deleted) {
+    const clusterLocator = `//td[contains(text(), '${cluserName}')]`;
+    if (deleted) {
+      I.dontSeeElement(clusterLocator);
+    } else {
+      I.waitForVisible(clusterLocator, 30);
+    }
+  },
+
+  seeErrorForAddedCluster(clusterName) {
+    const message = `Kubernetes Cluster with Name "${clusterName}" already exists.`;
+    I.waitForText(message, 10);
+  },
+
+  addKubernetesCluster(clusterName, config) {
+    I.click(this.fields.addKubernetesClusterButton);
+    I.fillField(this.fields.kubernetesClusterNameInput, clusterName);
+    I.fillField(this.fields.kubeconfigFileInput, config);
+    I.click(this.fields.kubernetesAddButton);
+  },
+
+  deleteCluster(cluserName) {
+    const deleteLocator = `//td[contains(text(), '${cluserName}')]//parent::tr//button[@data-qa='open-delete-modal-button']`;
+    I.waitForVisible(deleteLocator, 30);
+    I.click(deleteLocator);
+    I.waitForText(this.confirmDeleteText, 10);
+    I.click(this.fields.proceedButton);
+    I.waitForText(this.deletedAlertMessage, 10);
+  },
+};
