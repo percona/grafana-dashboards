0172ec79d1d8afaf003bad266aaccd044d075e59
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2020-09-23 09:57:33 +0100

    PMM-6515 Add links from qan filters to dashboards (#972)
    * PMM-6515 Add links from qan filters to dashboards
    
    * PMM-6515 Use enum for databases
    
    * PMM-6515 Fix get service type
    
    * PMM-6515 Fix current value

--- pmm-app/src/pmm-qan/panel/components/Filters/Filters.constants.ts
+++ pmm-app/src/pmm-qan/panel/components/Filters/Filters.constants.ts
@@ -1,6 +1,11 @@
+import { getServiceType } from './Filters.tools';
+import { Databases } from '../Details/Details.types';
+
 export const FILTERS_BODY_HEIGHT = 600;
 export const FILTERS_HEADER_SIZE = 50;
 export const FILTERS_MARGIN_BOTTOM = 20;
+
+const subURL = '/graph/d/';
 
 export const FILTERS_GROUPS = [
   {
@@ -10,10 +15,34 @@ export const FILTERS_GROUPS = [
   {
     name: 'Cluster',
     dataKey: 'cluster',
+    getDashboardURL: (value: string) => {
+      const serviceType = getServiceType(value, 'cluster');
+      let dashboardURL = '';
+
+      if (serviceType === Databases.mysql) {
+        dashboardURL = `pxc-cluster-summary/pxc-galera-cluster-summary?var-cluster=${value}`;
+      } else if (serviceType === Databases.mongodb) {
+        dashboardURL = `mongodb-cluster-summary/mongodb-cluster-summary?var-cluster=${value}`;
+      }
+
+      return dashboardURL ? `${subURL}${dashboardURL}` : '';
+    },
   },
   {
     name: 'Replication Set',
     dataKey: 'replication_set',
+    getDashboardURL: (value: string) => {
+      const serviceType = getServiceType(value, 'replication_set');
+      let dashboardURL = '';
+
+      if (serviceType === Databases.mysql) {
+        dashboardURL = `mysql-replicaset-summary/mysql-replication-summary?var-replication_set=${value}`;
+      } else if (serviceType === Databases.mongodb) {
+        dashboardURL = `mongodb-replicaset-summary/mongodb-replset-summary?var-replset=${value}`;
+      }
+
+      return dashboardURL ? `${subURL}${dashboardURL}` : '';
+    },
   },
   {
     name: 'Database',
@@ -26,10 +55,20 @@ export const FILTERS_GROUPS = [
   {
     name: 'Node Name',
     dataKey: 'node_name',
+    getDashboardURL: (value: string) => (
+      `${subURL}node-instance-summary/node-summary?var-node_name=${value}`
+    ),
   },
   {
     name: 'Service Name',
     dataKey: 'service_name',
+    getDashboardURL: (value: string) => {
+      const serviceType = getServiceType(value, 'service_name');
+
+      return serviceType
+        ? `${subURL}${serviceType}-instance-summary/${serviceType}-instance-summary?var-service_name=${value}`
+        : '';
+    },
   },
   {
     name: 'Client Host',
