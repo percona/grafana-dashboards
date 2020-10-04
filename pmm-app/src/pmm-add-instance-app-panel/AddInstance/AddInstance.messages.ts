6b0c298722dcececab86b7e3e0cff732a0fdc923
Roman Misyurin <roman.misyurin@percona.com>
2020-10-05 01:41:57 +0300

    PMM-6666 add pg_stat_monitor checkbox

---
+++ pmm-app/src/pmm-add-instance-app-panel/AddInstance/AddInstance.messages.ts
@@ -0,0 +1,9 @@
+export const Messages = {
+  form: {
+    trackingOptions: {
+      none: 'Don\'t track',
+      pgStatements: 'Pg Stat Statements',
+      pgMonitor: 'PG Stat Monitor',
+    },
+  },
+};
