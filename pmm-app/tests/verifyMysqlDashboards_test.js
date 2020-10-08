22267ef0fbdb1005b89a697afd1012c262643756
Puneet Kala <puneet.0191@gmail.com>
2020-10-08 21:26:02 +0530

    PMM-6707
    Possible Issue with Time

--- pmm-app/tests/verifyMysqlDashboards_test.js
+++ pmm-app/tests/verifyMysqlDashboards_test.js
@@ -17,7 +17,7 @@ Scenario(
     I.click(adminPage.fields.metricTitle);
     adminPage.peformPageDown(5);
     dashboardPage.verifyMetricsExistence(dashboardPage.mysqlInstanceSummaryDashboard.metrics);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
+    await dashboardPage.verifyThereAreNoGraphsWithNA(1);
     await dashboardPage.verifyThereAreNoGraphsWithoutData(5);
   },
 );
