f0323e648dbae4fc852da1b7b1d9501fc4cda0ea
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2020-10-09 17:37:15 +0100

    PMM-5972 Create XtraDB cluster (#981)
    * PMM-5972 Add platform-core package
    
    * PMM-5972 Add select field adapter
    
    * PMM-5972 Keep consistency with platform-core
    
    * PMM-5972 Refactor useKubernetes to DBaaS
    
    * PMM-5972 Create XtraDB cluster
    
    * PMM-5972 Fix lint errors
    
    * PMM-5972 Fix indentation
    
    * PMM-5972 Add interfaces
    
    * PMM-5972 Perfomance optimization with useCallback
    
    * PMM-5972 Refactor comment
    
    * PMM-5972 Add missing commas
    
    * PMM-5972 Refactor validators and disabled
    
    * PMM-5972 Use textarea from platform core
    
    * PMM-5975 List DB clusters (#985)
    
    * PMM-5975 Add database types to core
    
    * PMM-5975 Add status component
    
    * PMM-5975 Add list db clusters
    
    Co-authored-by: Alexander Tymchuk <alexander.tymchuk@percona.com>
    Co-authored-by: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Co-authored-by: Roman Misyurin <lunaticusgreen@users.noreply.github.com>

--- pmm-app/src/pmm-dbaas/DBaaS.tsx
+++ pmm-app/src/pmm-dbaas/DBaaS.tsx
@@ -1,19 +1,40 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useMemo } from 'react';
 import { TabsBar, TabContent, Tab } from '@grafana/ui';
 import { KubernetesInventory } from './components/Kubernetes/KubernetesInventory';
+import { XtraDB } from './components/XtraDB/XtraDB';
+import { useKubernetes } from './components/Kubernetes/Kubernetes.hooks';
 import { Messages } from './DBaaS.messages';
 import { TabKeys } from './DBaaS.types';
-
-const tabs = [
-  {
-    label: Messages.tabs.kubernetes,
-    key: TabKeys.kubernetes,
-    component: <KubernetesInventory key={TabKeys.kubernetes} />
-  },
-];
+import { styles } from './DBaaS.styles';
 
 export const DBaaSPanel: FC = () => {
   const [activeTab, setActiveTab] = useState(TabKeys.kubernetes);
+  const [kubernetes, deleteKubernetes, addKubernetes, kubernetesLoading] = useKubernetes();
+  const tabs = useMemo(
+    () => [
+      {
+        label: Messages.tabs.kubernetes,
+        key: TabKeys.kubernetes,
+        component: <KubernetesInventory
+          key={TabKeys.kubernetes}
+          kubernetes={kubernetes}
+          deleteKubernetes={deleteKubernetes}
+          addKubernetes={addKubernetes}
+          loading={kubernetesLoading}
+        />
+      },
+      {
+        label: Messages.tabs.xtradb,
+        key: TabKeys.xtradb,
+        disabled: kubernetes.length === 0,
+        component: <XtraDB
+          key={TabKeys.xtradb}
+          kubernetes={kubernetes}
+        />
+      },
+    ],
+    [kubernetes, kubernetesLoading]
+  );
 
   return (
     <div>
@@ -23,6 +44,7 @@ export const DBaaSPanel: FC = () => {
             key={index}
             label={tab.label}
             active={tab.key === activeTab}
+            style={tab.disabled ? styles.disabled : undefined}
             onChangeTab={() => setActiveTab(tab.key)}
           />
         ))}
