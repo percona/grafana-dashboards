a7c288fa976cb556d2eab4d7c50b892068eae8c7
Roman Misyurin <roman.misyurin@percona.com>
2020-10-15 15:10:10 +0300

    PMM-6542 cleanup styles

--- pmm-app/src/pmm-add-instance-app-panel/components/AddInstance/AddInstance.styles.ts
+++ pmm-app/src/pmm-add-instance-app-panel/components/AddInstance/AddInstance.styles.ts
@@ -2,53 +2,49 @@ import { stylesFactory } from '@grafana/ui';
 import { GrafanaTheme } from '@grafana/data';
 import { css } from 'emotion';
 
-export const getStyles = stylesFactory((theme: GrafanaTheme) => {
-  console.log(theme);
+export const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  navigationButton: css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: ${theme.spacing.sm};
+    border-radius: ${theme.border.radius.md};
+    width: 240px;
+    height: 170px;
+    text-align: center;
+    background-color: transparent;
+    border: 1px dashed ${theme.colors.border2};
 
-  return {
-    navigationButton: css`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      margin: 10px;
-      border-radius: 10px;
-      width: 240px;
-      height: 170px;
-      text-align: center;
-      background-color: transparent;
-      border: 1px dashed ${theme.colors.border2};
-
-      :hover {
-        cursor: pointer;
-        background-color: ${theme.colors.dropdownOptionHoverBg};
-        border: 1px solid ${theme.colors.border2};
-      }
-    `,
-    navigationPanel: css`
-      display: flex;
-      flex-direction: row;
-      justify-content: start;
-      flex-wrap: wrap;
-      max-width: 800px;
-      width: 100%;
-      overflow: hidden;
-    `,
-    content: css`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    `,
-    addInstance: css`
-      margin-top: 10px;
-    `,
-    addInstanceTitle: css`
-      margin-top: 5px;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      width: 65%;
-      height: 1.5em;
-      white-space: nowrap;
-    `,
-  };
-});
+    :hover {
+      cursor: pointer;
+      background-color: ${theme.colors.dropdownOptionHoverBg};
+      border: 1px solid ${theme.colors.border2};
+    }
+  `,
+  navigationPanel: css`
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    flex-wrap: wrap;
+    max-width: 800px;
+    width: 100%;
+    overflow: hidden;
+  `,
+  content: css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  addInstance: css`
+    margin-top: ${theme.spacing.sm};
+  `,
+  addInstanceTitle: css`
+    margin-top: ${theme.spacing.sm};
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 65%;
+    height: 1.5em;
+    white-space: nowrap;
+  `,
+}));
--- pmm-app/src/pmm-add-instance-app-panel/components/AddInstance/AddInstance.test.tsx
+++ pmm-app/src/pmm-add-instance-app-panel/components/AddInstance/AddInstance.test.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import {
+  shallow, mount, ShallowWrapper, ReactWrapper,
+} from 'enzyme';
 import AddInstance, { SelectInstance } from './AddInstance';
+import { instanceList } from './AddInstance.constants';
 
 describe('AddInstance page:: ', () => {
   it('should render with correct amount of links correct', () => {
-    const wrapper = shallow(<AddInstance onSelectInstanceType={() => {}} />);
+    const wrapper: ShallowWrapper = shallow(<AddInstance onSelectInstanceType={() => {}} />);
 
-
-    expect(wrapper?.find(SelectInstance).length).toEqual(5);
+    expect(wrapper.find(SelectInstance).length).toEqual(instanceList.length);
   });
 
   it('should invoke callback with right key', () => {
     const onSelectInstanceType = jest.fn();
 
-    const wrapper = mount(<AddInstance onSelectInstanceType={onSelectInstanceType} />);
+    const wrapper: ReactWrapper = mount(<AddInstance onSelectInstanceType={onSelectInstanceType} />);
 
     expect(onSelectInstanceType).toBeCalledTimes(0);
 
-    console.log(wrapper?.find('[data-qa="rds-instance"]'));
-
-    wrapper?.find('[data-qa="rds-instance"]').simulate('click');
+    wrapper.find('[data-qa="rds-instance"]').simulate('click');
 
     expect(onSelectInstanceType).toBeCalledTimes(1);
     expect(onSelectInstanceType.mock.calls[0][0]).toStrictEqual({ type: 'rds' });
