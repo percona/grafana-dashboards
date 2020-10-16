04ba59cf599188f94bb7b646c6a97cd1f7aca1bd
Roman Misyurin <roman.misyurin@percona.com>
2020-10-16 12:42:28 +0300

    PMM-6542 rename add instance panel

---
+++ pmm-app/src/pmm-add-instance/components/AddInstance/AddInstance.styles.ts
@@ -0,0 +1,54 @@
+import { stylesFactory } from '@grafana/ui';
+import { GrafanaTheme } from '@grafana/data';
+import { css } from 'emotion';
+
+export const getStyles = stylesFactory((theme: GrafanaTheme) => {
+  const { border, colors, spacing } = theme;
+
+  return {
+    navigationButton: css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: ${spacing.sm};
+      border-radius: ${border.radius.md};
+      width: 240px;
+      height: 170px;
+      text-align: center;
+      background-color: transparent;
+      border: ${border.width.sm} dashed ${colors.border2};
+
+      :hover {
+        cursor: pointer;
+        background-color: ${colors.dropdownOptionHoverBg};
+        border: ${border.width.sm} solid ${colors.border2};
+      }
+    `,
+    navigationPanel: css`
+      display: flex;
+      flex-direction: row;
+      justify-content: start;
+      flex-wrap: wrap;
+      max-width: 800px;
+      width: 100%;
+      overflow: hidden;
+    `,
+    content: css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `,
+    addInstance: css`
+      margin-top: ${spacing.sm};
+    `,
+    addInstanceTitle: css`
+      margin-top: ${spacing.sm};
+      text-overflow: ellipsis;
+      overflow: hidden;
+      width: 65%;
+      height: 1.5em;
+      white-space: nowrap;
+    `,
+  };
+});
