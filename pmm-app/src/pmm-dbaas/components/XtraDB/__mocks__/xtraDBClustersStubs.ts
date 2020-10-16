d3773e476fc1324e32fdc3226ef2bfa52c98ce37
Tiago Mota Santos <tiago.mota@percona.com>
2020-10-16 09:12:54 +0100

    PMM-5993 Add missing properties to XtraDB stubs

--- pmm-app/src/pmm-dbaas/components/XtraDB/__mocks__/xtraDBClustersStubs.ts
+++ pmm-app/src/pmm-dbaas/components/XtraDB/__mocks__/xtraDBClustersStubs.ts
@@ -7,11 +7,17 @@ export const xtraDBClustersStub: XtraDBCluster[] = [
     kubernetesClusterName: 'Kubernetes Cluster 1',
     clusterName: 'dbcluster1',
     databaseType: Databases.mysql,
+    clusterSize: 3,
+    memory: 1024,
+    cpu: 1,
   },
   {
     kubernetesClusterName: 'Kubernetes Cluster 2',
     clusterName: 'dbcluster2',
     databaseType: Databases.mysql,
+    clusterSize: 7,
+    memory: 2048,
+    cpu: 4,
   },
 ];
 
