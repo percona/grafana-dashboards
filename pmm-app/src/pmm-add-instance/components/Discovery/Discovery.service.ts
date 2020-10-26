d48c52d4002ff77502ad3e109e20e6ff1dbaff58
Roman Misyurin <roman.misyurin@percona.com>
2020-10-27 01:12:03 +0300

    PMM-6793 TS fixes

--- pmm-app/src/pmm-add-instance/components/Discovery/Discovery.service.ts
+++ pmm-app/src/pmm-add-instance/components/Discovery/Discovery.service.ts
@@ -2,13 +2,13 @@ import { apiRequestManagement } from 'shared/components/helpers/api';
 
 class DiscoveryService {
   static async discoveryRDS({ aws_access_key, aws_secret_key }) {
-    return {
-      rds_instances: [{ address: 'da:123' }, { address: 'da:123' }],
-    };
-    // return apiRequestManagement.post<any, any>('/RDS/Discover', {
-    //   aws_access_key,
-    //   aws_secret_key,
-    // });
+    // return {
+    //   rds_instances: [{ address: 'da:123' }, { address: 'da:123' }],
+    // };
+    return apiRequestManagement.post<any, any>('/RDS/Discover', {
+      aws_access_key,
+      aws_secret_key,
+    });
   }
 }
 
