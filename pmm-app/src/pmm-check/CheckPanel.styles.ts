3a671b0d185868ab72e8752552c97c8771564106
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-27 19:42:10 +0100

    SAAS-409 update src/pmm-check/CheckPanel.styles.ts

--- pmm-app/src/pmm-check/CheckPanel.styles.ts
+++ pmm-app/src/pmm-check/CheckPanel.styles.ts
@@ -13,3 +13,11 @@ export const spinner = css`
   align-items: center;
   justify-content: center;
 `;
+
+export const tabBar = css`
+  height: 42px;
+`;
+
+export const tabContent = css`
+  height: calc(100% - 42px);
+`;
