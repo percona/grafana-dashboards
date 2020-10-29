6fbf5339a4b2bdbb61a0771b9e2202fdc04e2474
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-29 18:03:55 +0100

    SAAS-410 update AllChecksTab/types.ts

--- pmm-app/src/pmm-check/components/AllChecksTab/types.ts
+++ pmm-app/src/pmm-check/components/AllChecksTab/types.ts
@@ -1,3 +1,10 @@
+import { CheckDetails } from 'pmm-check/types';
+
 export type ChangeCheck = (checkName: string, enabled: boolean) => Promise<void>;
 
 export type FetchChecks = () => Promise<void>;
+
+export interface CheckTableRowProps {
+  check: CheckDetails;
+  onSuccess: (check: CheckDetails) => void;
+}
