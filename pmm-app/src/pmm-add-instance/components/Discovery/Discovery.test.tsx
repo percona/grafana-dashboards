95794de463371a7ca03ad76e34f5d090889c6d33
Roman Misyurin <roman.misyurin@percona.com>
2020-11-06 14:09:01 +0300

    PMM-6793 test fix

--- pmm-app/src/pmm-add-instance/components/Discovery/Discovery.test.tsx
+++ pmm-app/src/pmm-add-instance/components/Discovery/Discovery.test.tsx
@@ -11,7 +11,7 @@ describe('Discovery instance:: ', () => {
     const root = mount(<Discovery selectInstance={selectInstance} />);
 
     expect(root.find('input[data-qa="aws_access_key-text-input"]').length).toBe(1);
-    expect(root.find('input[data-qa="aws_secret_key-text-input"]').length).toBe(1);
+    expect(root.find('input[data-qa="aws_secret_key-password-input"]').length).toBe(1);
     expect(root.find('button[data-qa="credentials-search-button"]').length).toBe(1);
   });
 });
