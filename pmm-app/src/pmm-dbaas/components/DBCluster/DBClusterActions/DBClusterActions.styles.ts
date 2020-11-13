0d265f5fd6d464782142a44b4e652bb7ec479fc4
Tiago Mota Santos <tiago.mota@percona.com>
2020-11-13 17:58:30 +0000

    PMM-6843 Continously request db clusters

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterActions/DBClusterActions.styles.ts
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterActions/DBClusterActions.styles.ts
@@ -4,5 +4,9 @@ export const styles = {
   actionsColumn: css`
     display: flex;
     justify-content: center;
+    /* TODO: remove once updated platform-core */
+    div[data-qa="dropdown-menu-container"] {
+      z-index: 1;
+    }
   `,
 };
