bb75a51035dbee87f86dc0415dd27e1ab59043c6
Roman Misyurin <roman.misyurin@percona.com>
2020-11-17 16:17:51 +0300

    PMM-6851-fix-service

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBCluster.hooks.ts
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBCluster.hooks.ts
@@ -2,19 +2,16 @@ import { useState, useEffect } from 'react';
 import { processPromiseResults, FulfilledPromiseResult } from 'shared/components/helpers/promises';
 import { Databases } from 'shared/core';
 import { Kubernetes } from '../Kubernetes/Kubernetes.types';
-import { DBCluster, GetDBClustersAction, DBClusterPayload } from './DBCluster.types';
+import {
+  DBCluster, GetDBClustersAction, DBClusterPayload,
+} from './DBCluster.types';
 import { isClusterChanging } from './DBCluster.utils';
 import { DBClusterServiceFactory } from './DBClusterService.factory';
 
 const RECHECK_INTERVAL = 30000;
-const DATABASES = [
-  Databases.mysql,
-  Databases.mongodb,
-];
+const DATABASES = [Databases.mysql, Databases.mongodb];
 
-export const useDBClusters = (
-  kubernetes: Kubernetes[],
-): [DBCluster[], GetDBClustersAction, boolean] => {
+export const useDBClusters = (kubernetes: Kubernetes[]): [DBCluster[], GetDBClustersAction, boolean] => {
   const [dbClusters, setDBClusters] = useState<DBCluster[]>([]);
   const [loading, setLoading] = useState(false);
   let timer: NodeJS.Timeout;
@@ -68,13 +65,8 @@ const getClusters = async (kubernetes: Kubernetes[], databaseType: Databases): P
 
     const clusters: DBClusterPayload[] = (r as FulfilledPromiseResult).value?.clusters ?? [];
 
-    const resultClusters = clusters.map(
-      (cluster) => dbClusterService.toModel(
-        cluster,
-        kubernetes[index].kubernetesClusterName,
-        databaseType,
-      ),
-    );
+    // eslint-disable-next-line max-len
+    const resultClusters = clusters.map((cluster) => dbClusterService.toModel(cluster, kubernetes[index].kubernetesClusterName, databaseType));
 
     return acc.concat(resultClusters);
   }, []);
