1e626176b47e08d257a24f4f7d353337edfda889
Tiago Mota Santos <tiago.mota@percona.com>
2020-11-19 09:13:35 +0000

    PMM-6843 Bump platform-core version

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterActions/DBClusterActions.styles.ts
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterActions/DBClusterActions.styles.ts
@@ -4,9 +4,5 @@ export const styles = {
   actionsColumn: css`
     display: flex;
     justify-content: center;
-    /* TODO: remove once updated platform-core */
-    div[data-qa="dropdown-menu-container"] {
-      z-index: 1;
-    }
   `,
 };
