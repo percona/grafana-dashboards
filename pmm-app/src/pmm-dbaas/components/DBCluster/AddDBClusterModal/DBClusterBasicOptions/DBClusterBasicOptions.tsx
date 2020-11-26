c1d5fe0e164da4347c66c55e109e3c5382c865ec
Roman Misyurin <roman.misyurin@percona.com>
2020-11-26 21:39:26 +0300

    PMM-7014 add installed kubernetes operator checks

--- pmm-app/src/pmm-dbaas/components/DBCluster/AddDBClusterModal/DBClusterBasicOptions/DBClusterBasicOptions.tsx
+++ pmm-app/src/pmm-dbaas/components/DBCluster/AddDBClusterModal/DBClusterBasicOptions/DBClusterBasicOptions.tsx
@@ -1,16 +1,18 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Field } from 'react-final-form';
 import { TextInputField, validators } from '@percona/platform-core';
-import { Databases } from 'shared/core';
+import { DATABASE_LABELS, Databases } from 'shared/core';
 import { SelectFieldAdapter } from 'shared/components/Form/FieldAdapters/FieldAdapters';
 import { Messages } from 'pmm-dbaas/DBaaS.messages';
 import { DBClusterBasicOptionsProps } from './DBClusterBasicOptions.types';
 import { DATABASE_OPTIONS } from '../../DBCluster.constants';
 import { AddDBClusterFields } from '../AddDBClusterModal.types';
 import { DBClusterTopology } from '../DBClusterAdvancedOptions/DBClusterAdvancedOptions.types';
+import { OptionContent } from '../../OptionContent/OptionContent';
 import { kubernetesClusterName } from './DBClusterBasicOptions.utils';
 
-export const DBClusterBasicOptions: FC<DBClusterBasicOptionsProps> = ({ kubernetesOptions, form }) => {
+export const DBClusterBasicOptions: FC<DBClusterBasicOptionsProps> = ({ kubernetes, form }) => {
+  const DATABASE_TYPES = [Databases.mongodb, Databases.mysql];
   const { required } = validators;
   const { change } = form;
   const onChangeDatabase = useCallback((databaseType) => {
@@ -19,6 +21,57 @@ export const DBClusterBasicOptions: FC<DBClusterBasicOptionsProps> = ({ kubernet
     }
 
     change(AddDBClusterFields.databaseType, databaseType);
+  }, []);
+
+  const kubernetesOptions = kubernetes.map(({ kubernetesClusterName }) => {
+    const installedOperators = {
+      mysql: true,
+      mongodb: false,
+    };
+
+    const availableTypes = DATABASE_TYPES.filter((databaseType) => installedOperators[databaseType]);
+    const disabledTypes = DATABASE_TYPES.filter((databaseType) => !installedOperators[databaseType]);
+
+    const getOptionContent = (listOfOperators, kubernetesClusterName) => {
+      return (
+        <OptionContent
+          title={kubernetesClusterName}
+          description={disabledTypes.length ? 'Operators must be installed to use database type' : ''}
+          tags={availableTypes.map((databaseType) => DATABASE_LABELS[databaseType])}
+          disabledTags={disabledTypes.map((databaseType) => DATABASE_LABELS[databaseType])}
+        />
+      );
+    };
+
+    return {
+      value: kubernetesClusterName,
+      label: getOptionContent(installedOperators, kubernetesClusterName),
+      installedOperators,
+    };
+  });
+
+  const [databaseOptions, setDatabaseOptions] = useState(DATABASE_OPTIONS);
+  const onChangeCluster = useCallback((selectedKubernetes) => {
+    const selectedDatabase = form.getState().values[AddDBClusterFields.databaseType]?.value
+    const availableDatabaseOptions = [];
+
+    DATABASE_TYPES.forEach((databaseType) => {
+      if (!selectedKubernetes.installedOperators[databaseType]) {
+        availableDatabaseOptions.push({
+          value: Databases[databaseType],
+          label: DATABASE_LABELS[databaseType],
+        });
+
+        if (selectedDatabase === Databases[databaseType])
+          change(AddDBClusterFields.databaseType, {
+            value: undefined,
+            label: undefined,
+          });
+      }
+    });
+
+    setDatabaseOptions(availableDatabaseOptions);
+    change(AddDBClusterFields.kubernetesCluster, selectedKubernetes);
   }, []);
 
   return (
@@ -35,12 +88,15 @@ export const DBClusterBasicOptions: FC<DBClusterBasicOptionsProps> = ({ kubernet
         options={kubernetesOptions}
         component={SelectFieldAdapter}
         validate={required}
+        onChange={onChangeCluster}
       />
       <Field
+        // disabled={!form.getState().values[AddDBClusterFields.kubernetesCluster]}
+        disabled={!databaseOptions.length}
         dataQa="dbcluster-database-type-field"
         name={AddDBClusterFields.databaseType}
         label={Messages.dbcluster.addModal.fields.databaseType}
-        options={DATABASE_OPTIONS}
+        options={databaseOptions}
         component={SelectFieldAdapter}
         validate={required}
         onChange={onChangeDatabase}
