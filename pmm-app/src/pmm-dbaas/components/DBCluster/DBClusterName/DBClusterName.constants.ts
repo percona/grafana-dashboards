95972218a7646ccf609e65d11ebe906a22f522e7
Roman Misyurin <roman.misyurin@percona.com>
2020-11-28 00:54:00 +0300

    PMM-7032 fix cluster variable value

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterName/DBClusterName.constants.ts
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterName/DBClusterName.constants.ts
@@ -1,6 +1,9 @@
+/* eslint implicit-arrow-linebreak: 0 */
 import { Databases } from 'shared/core';
 
 export const DASHBOARD_URL_MAP = {
-  [Databases.mysql]: '/graph/d/pxc-cluster-summary/pxc-galera-cluster-summary?var-cluster=',
-  [Databases.mongodb]: '/graph/d/mongodb-cluster-summary/mongodb-cluster-summary?var-cluster=',
+  [Databases.mysql]: (clusterName: string) =>
+    `/graph/d/pxc-cluster-summary/pxc-galera-cluster-summary?var-cluster=${clusterName}-pxc`,
+  [Databases.mongodb]: (clusterName: string) =>
+    `/graph/d/mongodb-cluster-summary/mongodb-cluster-summary?var-cluster=${clusterName}`,
 };
