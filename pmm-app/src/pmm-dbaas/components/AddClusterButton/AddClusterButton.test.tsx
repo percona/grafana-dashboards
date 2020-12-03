3b4e41c2efc76e01678958b37b46dec36b51888b
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2020-12-03 16:03:31 +0300

    PMM-7019 fix single mode field (#1056)
    * PMM-7019 fix single mode field
    
    * PMM-7019 rework url warning message
    
    * PMM-7019 use grafana themes

--- pmm-app/src/pmm-dbaas/components/AddClusterButton/AddClusterButton.test.tsx
+++ pmm-app/src/pmm-dbaas/components/AddClusterButton/AddClusterButton.test.tsx
@@ -1,49 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { dataQa } from '@percona/platform-core';
 import { AddClusterButton } from './AddClusterButton';
 
 describe('AddClusterButton::', () => {
   it('renders correctly and calls action', () => {
     const action = jest.fn();
-    const root = mount(
-      <AddClusterButton
-        label="test"
-        action={action}
-      />,
-    );
+    const root = mount(<AddClusterButton label="test" action={action} />);
     const button = root.find('button');
 
     button.simulate('click');
 
     expect(button.text()).toEqual('test');
     expect(action).toHaveBeenCalled();
-    expect(root.contains(dataQa('add-cluster-button-warning'))).toBeFalsy();
   });
-  it('renders correctly with warning', () => {
-    const action = jest.fn();
-    const root = mount(
-      <AddClusterButton
-        label="test"
-        action={action}
-        warningMessage="Test warning"
-        showWarning
-      />,
-    );
-    const warning = root.find(dataQa('add-cluster-button-warning'));
 
-    expect(warning).toBeTruthy();
-    expect(warning.text()).toContain('Test warning');
-  });
   it('disables button correctly', () => {
     const action = jest.fn();
-    const root = mount(
-      <AddClusterButton
-        label="test"
-        action={action}
-        disabled
-      />,
-    );
+    const root = mount(<AddClusterButton label="test" action={action} disabled />);
     const button = root.find('button');
 
     button.simulate('click');
