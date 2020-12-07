8ebc6db26a476ada355d9b99cbd6eab155d05120
Ivana Wzientková <ivana.wzientkova@percona.com>
2020-12-07 15:53:33 +0100

    Added PMM-T507

--- pmm-app/tests/verifyVMDashboards_test.js
+++ pmm-app/tests/verifyVMDashboards_test.js
@@ -5,7 +5,7 @@ Before(async (I) => {
 });
 
 Scenario(
-    'Will be added later(name) @not-ui-pipeline @nightly @not-pr-pipeline',
+    'PMM-T506 Verify metrics on VictoriaMetrcis dashboard @not-ui-pipeline @nightly @not-pr-pipeline',
     async (I, dashboardPage, adminPage) => {
         I.amOnPage(dashboardPage.victoriaMetricsDashboard.url);
         dashboardPage.waitForDashboardOpened();
@@ -15,5 +15,19 @@ Scenario(
         dashboardPage.verifyMetricsExistence(dashboardPage.victoriaMetricsDashboard.metrics);
         await dashboardPage.verifyThereAreNoGraphsWithNA(0);
         await dashboardPage.verifyThereAreNoGraphsWithoutData(0);
+    },
+  );
+
+Scenario(
+    'PMM-T507 Verify metrics on VM Agents Overview Dashboard @not-ui-pipeline @nightly @not-pr-pipeline',
+    async (I, dashboardPage, adminPage) => {
+        I.amOnPage(dashboardPage.victoriaMetricsAgentsOverviewDashboard.url);
+        dashboardPage.waitForDashboardOpened();
+        await dashboardPage.expandEachDashboardRow();
+        I.click(adminPage.fields.metricTitle);
+        adminPage.peformPageDown(5);
+        dashboardPage.verifyMetricsExistence(dashboardPage.victoriaMetricsAgentsOverviewDashboard.metrics);
+      //  await dashboardPage.verifyThereAreNoGraphsWithNA(0);
+      //  await dashboardPage.verifyThereAreNoGraphsWithoutData(0);
     },
   );
