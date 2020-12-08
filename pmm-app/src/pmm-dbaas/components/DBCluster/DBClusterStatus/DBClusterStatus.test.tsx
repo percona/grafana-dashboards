eb582d65a34996b3576f3b7bd5266511e48d92ae
Roman Misyurin <roman.misyurin@percona.com>
2020-12-09 00:43:23 +0300

    PMM-7084 add error message icon

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.test.tsx
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.test.tsx
@@ -5,42 +5,29 @@ import { DBClusterStatus } from './DBClusterStatus';
 
 describe('DBClusterStatus::', () => {
   it('renders correctly when active', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.ready}
-        errorMessage="Should not render error"
-      />,
-    );
+    const root = shallow(<DBClusterStatus status={Status.ready} errorMessage="Should not render error" />);
     const span = root.find('span');
 
     expect(root.find('[data-qa="cluster-status-active"]')).toBeTruthy();
+    expect(root.find('[data-qa="cluster-status-error-message"]')).toBeFalsy();
     expect(span.prop('className')).toContain('active');
-    expect(span.prop('title')).toEqual('');
   });
   it('renders correctly when failed', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.failed}
-        errorMessage="Test error"
-      />,
-    );
+    const root = shallow(<DBClusterStatus status={Status.failed} errorMessage="Test error" />);
     const span = root.find('span');
+    const errorMessage = root.find('[data-qa="cluster-status-error-message"]');
 
     expect(root.find('[data-qa="cluster-status-failse"]')).toBeTruthy();
+    expect(errorMessage).toBeTruthy();
+    expect(errorMessage.prop('title')).toEqual('Test error');
     expect(span.prop('className')).toContain('failed');
-    expect(span.prop('title')).toEqual('Test error');
   });
   it('renders correctly when changing', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.changing}
-        errorMessage="Should not render error"
-      />,
-    );
+    const root = shallow(<DBClusterStatus status={Status.changing} errorMessage="Should not render error" />);
     const span = root.find('span');
 
     expect(root.find('[data-qa="cluster-status-pending"]')).toBeTruthy();
+    expect(root.find('[data-qa="cluster-status-error-message"]')).toBeFalsy();
     expect(span.prop('className')).not.toContain('active');
-    expect(span.prop('title')).toEqual('');
   });
 });
--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.tsx
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.tsx
@@ -1,32 +1,37 @@
 import React, { FC, useMemo } from 'react';
 import { cx } from 'emotion';
-import { useStyles } from '@grafana/ui';
+import { Icon, useStyles } from '@grafana/ui';
 import { Messages } from 'pmm-dbaas/DBaaS.messages';
 import { DBClusterStatusProps } from './DBClusterStatus.types';
 import { getStyles } from './DBClusterStatus.styles';
 import { DBClusterStatus as Status } from '../DBCluster.types';
 import { STATUS_DATA_QA } from './DBClusterStatus.constants';
 
-export const DBClusterStatus: FC<DBClusterStatusProps> = ({
-  status,
-  errorMessage,
-}) => {
-  const styles = useStyles(getStyles);
+export const DBClusterStatus: FC<DBClusterStatusProps> = ({ status, errorMessage }) => {
+  const styles = useStyles(getStyles);08
   const statusError = status === Status.failed || status === Status.invalid;
-  const statusStyles = useMemo(() => ({
-    [styles.statusActive]: status === Status.ready,
-    [styles.statusFailed]: statusError,
-  }), [status]);
+  const statusStyles = useMemo(
+    () => ({
+      [styles.statusActive]: status === Status.ready,
+      [styles.statusFailed]: statusError,
+    }),
+    [status],
+  );
 
   return (
     <div className={styles.clusterStatusWrapper}>
-      <span
-        title={statusError ? errorMessage : ''}
-        className={cx(styles.status, statusStyles)}
-        data-qa={`cluster-status-${STATUS_DATA_QA[status]}`}
-      >
+      <span className={cx(styles.status, statusStyles)} data-qa={`cluster-status-${STATUS_DATA_QA[status]}`}>
         {Messages.dbcluster.table.status[status]}
       </span>
+      {statusError && errorMessage ? (
+        <span
+          title={statusError ? errorMessage : ''}
+          className={cx(styles.statusIcon)}
+          data-qa="cluster-status-error-message"
+        >
+          <Icon name="info-circle" />
+        </span>
+      ) : null}
     </div>
   );
 };
