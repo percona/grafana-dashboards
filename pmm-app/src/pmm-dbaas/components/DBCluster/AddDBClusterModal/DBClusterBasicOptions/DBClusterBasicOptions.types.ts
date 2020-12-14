0e0ea0838748be914d6f952b4fa2af900f7b2d61
Roman Misyurin <roman.misyurin@percona.com>
2020-12-14 15:25:19 +0300

    PMM-7014 fix kubernetes types

--- pmm-app/src/pmm-dbaas/components/DBCluster/AddDBClusterModal/DBClusterBasicOptions/DBClusterBasicOptions.types.ts
+++ pmm-app/src/pmm-dbaas/components/DBCluster/AddDBClusterModal/DBClusterBasicOptions/DBClusterBasicOptions.types.ts
@@ -1,7 +1,9 @@
 import { SelectableValue } from '@grafana/data';
 import { FormApi } from 'final-form';
+import { Kubernetes } from '../../../Kubernetes/Kubernetes.types';
 
 export interface DBClusterBasicOptionsProps {
   kubernetesOptions: SelectableValue[];
+  kubernetes: Kubernetes[];
   form: FormApi;
 }
