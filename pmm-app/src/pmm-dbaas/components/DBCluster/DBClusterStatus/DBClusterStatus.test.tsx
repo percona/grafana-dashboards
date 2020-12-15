83d6d06aff2071e9f26f9f431ce4ea5311e97662
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2020-12-15 13:52:17 +0300

    PMM-7084 db cluster status error message icon (#1069)
    * PMM-7084 add error message icon
    
    * PMM-7084 fix tests
    
    * PMM-7084 fix tests
    
    * PMM-7084 add multirow errors render

--- pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.test.tsx
+++ pmm-app/src/pmm-dbaas/components/DBCluster/DBClusterStatus/DBClusterStatus.test.tsx
@@ -5,42 +5,41 @@ import { DBClusterStatus } from './DBClusterStatus';
 
 describe('DBClusterStatus::', () => {
   it('renders correctly when active', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.ready}
-        errorMessage="Should not render error"
-      />,
-    );
+    const root = shallow(<DBClusterStatus status={Status.ready} errorMessage="Should not render error" />);
     const span = root.find('span');
 
     expect(root.find('[data-qa="cluster-status-active"]')).toBeTruthy();
+    expect(root.find('[data-qa="cluster-status-error-message"]').length).toBe(0);
     expect(span.prop('className')).toContain('active');
-    expect(span.prop('title')).toEqual('');
   });
   it('renders correctly when failed', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.failed}
-        errorMessage="Test error"
-      />,
-    );
-    const span = root.find('span');
+    const root = shallow(<DBClusterStatus status={Status.failed} errorMessage="Test error" />);
+    const span = root.find('[data-qa="cluster-status-failed"]').find('span');
+    const errorMessage = root.find('[data-qa="cluster-status-error-message"]');
 
-    expect(root.find('[data-qa="cluster-status-failse"]')).toBeTruthy();
+    expect(root.find('[data-qa="cluster-status-failed"]')).toBeTruthy();
+    expect(errorMessage).toBeTruthy();
+    expect(errorMessage.prop('title')).toEqual('Test error');
     expect(span.prop('className')).toContain('failed');
-    expect(span.prop('title')).toEqual('Test error');
   });
+
+  it('renders correctly when have multirow message', () => {
+    const root = shallow(<DBClusterStatus status={Status.failed} errorMessage="Test error;Second line error" />);
+    const span = root.find('[data-qa="cluster-status-failed"]').find('span');
+    const errorMessage = root.find('[data-qa="cluster-status-error-message"]');
+
+    expect(root.find('[data-qa="cluster-status-failed"]')).toBeTruthy();
+    expect(errorMessage).toBeTruthy();
+    expect(errorMessage.prop('title')).toEqual('Test error\nSecond line error');
+    expect(span.prop('className')).toContain('failed');
+  });
+
   it('renders correctly when changing', () => {
-    const root = shallow(
-      <DBClusterStatus
-        status={Status.changing}
-        errorMessage="Should not render error"
-      />,
-    );
+    const root = shallow(<DBClusterStatus status={Status.changing} errorMessage="Should not render error" />);
     const span = root.find('span');
 
     expect(root.find('[data-qa="cluster-status-pending"]')).toBeTruthy();
+    expect(root.find('[data-qa="cluster-status-error-message"]').length).toBe(0);
     expect(span.prop('className')).not.toContain('active');
-    expect(span.prop('title')).toEqual('');
   });
 });
