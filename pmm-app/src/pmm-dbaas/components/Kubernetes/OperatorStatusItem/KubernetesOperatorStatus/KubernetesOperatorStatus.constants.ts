e1d227295a36827b319d2d07127c851e1c023a1e
Roman Misyurin <roman.misyurin@percona.com>
2020-12-25 01:08:36 +0300

    PMM-7221 change operator install links

--- pmm-app/src/pmm-dbaas/components/Kubernetes/OperatorStatusItem/KubernetesOperatorStatus/KubernetesOperatorStatus.constants.ts
+++ pmm-app/src/pmm-dbaas/components/Kubernetes/OperatorStatusItem/KubernetesOperatorStatus/KubernetesOperatorStatus.constants.ts
@@ -2,8 +2,8 @@ import { Databases } from '../../../../../shared/core';
 import { KubernetesOperatorStatus } from './KubernetesOperatorStatus.types';
 
 export const OPERATORS_DOCS_URL = {
-  [Databases.mysql]: 'https://www.percona.com/doc/kubernetes-operator-for-pxc/index.html',
-  [Databases.mongodb]: 'https://www.percona.com/doc/kubernetes-operator-for-psmongodb/index.html',
+  [Databases.mysql]: 'https://per.co.na/x0wBC4',
+  [Databases.mongodb]: 'https://per.co.na/03Clok',
 };
 
 export const STATUS_DATA_QA = {
