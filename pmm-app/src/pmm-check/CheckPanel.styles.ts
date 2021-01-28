e8649cc9a75732271643a7b6eed17e7c6d92aa60
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-01-28 22:42:18 +0100

    PMM-7329 add settings link to CheckPanel

--- pmm-app/src/pmm-check/CheckPanel.styles.ts
+++ pmm-app/src/pmm-check/CheckPanel.styles.ts
@@ -1,23 +1,45 @@
 import { css } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+import { selectThemeVariant, stylesFactory } from '@grafana/ui';
 
-export const panel = css`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  margin-top: 1em;
-`;
+export const getStyles = stylesFactory((theme: GrafanaTheme) => {
+  const borderColor = selectThemeVariant(
+    { light: theme.palette.gray85, dark: theme.palette.dark7 },
+    theme.type,
+  );
 
-export const spinner = css`
-  display: flex;
-  height: 10em;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const tabBar = css`
-  height: 42px;
-`;
-
-export const tabContent = css`
-  height: calc(100% - 42px);
-`;
+  return {
+    panel: css`
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      margin-top: 1em;
+    `,
+    spinner: css`
+      display: flex;
+      height: 10em;
+      align-items: center;
+      justify-content: center;
+    `,
+    tabBar: css`
+      height: 42px;
+    `,
+    tabContent: css`
+      height: calc(100% - 42px);
+    `,
+    empty: css`
+      display: flex;
+      width: 100%;
+      height: 160px;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid ${borderColor};
+    `,
+    link: css`
+      color: ${theme.colors.linkExternal};
+      &:hover {
+        color: ${theme.colors.textBlue};
+      }
+    `,
+  };
+});
