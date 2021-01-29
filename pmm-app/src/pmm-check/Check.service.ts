1e65fbb6e2abc87af57ebe87c099dff822be2438
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-01-29 17:25:00 +0100

    PMM-7422 add silence info in Check service

--- pmm-app/src/pmm-check/Check.service.ts
+++ pmm-app/src/pmm-check/Check.service.ts
@@ -20,9 +20,9 @@ export const makeApiUrl: (segment: string) => string = (segment) => `${API.ALERT
  * A service-like object to store the API methods
  */
 export const CheckService = {
-  async getActiveAlerts(): Promise<ActiveCheck[] | undefined> {
+  async getActiveAlerts(includeSilenced = false): Promise<ActiveCheck[] | undefined> {
     const data = await apiRequest.get<Alert[], AlertRequestParams>(makeApiUrl('alerts'), {
-      params: { active: true, silenced: false, filter: 'stt_check=1' },
+      params: { active: true, silenced: includeSilenced, filter: 'stt_check=1' },
     });
 
     return Array.isArray(data) && data.length ? processData(data as Alert[]) : undefined;
@@ -56,13 +56,20 @@ export const CheckService = {
 export const processData = (data: Alert[]): ActiveCheck[] => {
   const result: Record<
     string,
-    Array<{ summary: string; description: string; severity: string; labels: { [key: string]: string } }>
+    Array<{
+      summary: string;
+      description: string;
+      severity: string;
+      labels: { [key: string]: string },
+      silenced: boolean;
+     }>
   > = data
     .filter((alert) => !!alert.labels.stt_check)
     .reduce((acc, alert) => {
       const {
         labels,
         annotations: { summary, description },
+        status: { state },
       } = alert;
       const serviceName = labels.service_name;
 
@@ -75,6 +82,7 @@ export const processData = (data: Alert[]): ActiveCheck[] => {
         description,
         severity: labels.severity,
         labels,
+        silenced: state === 'suppressed',
       };
 
       acc[serviceName] = (acc[serviceName] ?? []).concat(item);
@@ -106,6 +114,7 @@ export const processData = (data: Alert[]): ActiveCheck[] => {
       .map((val) => ({
         description: `${val.summary}${val.description ? `: ${val.description}` : ''}`,
         labels: val.labels ?? [],
+        silenced: val.silenced,
       }))
       .sort((a, b) => {
         const aSeverity = a.labels.severity;
