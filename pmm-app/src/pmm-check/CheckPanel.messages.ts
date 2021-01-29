d924bb805b394b41d9752f292d4e5eca6b9da384
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-01-29 18:17:09 +0100

    PMM-7422 update messages

--- pmm-app/src/pmm-check/CheckPanel.messages.ts
+++ pmm-app/src/pmm-check/CheckPanel.messages.ts
@@ -2,4 +2,6 @@ export const Messages = {
   failedChecksTitle: 'Failed Checks',
   allChecksTitle: 'All Checks',
   silenced: 'Silenced',
+  sttDisabled: 'Security Threat Tool is disabled. You can enable it in',
+  pmmSettings: 'PMM Settings.',
 };
