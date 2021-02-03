ac33239dada25c560749f82e30e2dfe43f6d3521
puneet0191 <puneet.0191@gmail.com>
2021-02-03 11:56:49 +0530

    PMM-7374 Adding minor registration tests

--- pmm-app/tests/pages/dbaasPage.js
+++ pmm-app/tests/pages/dbaasPage.js
@@ -5,21 +5,26 @@ module.exports = {
   addedAlertMessage: 'Cluster was successfully registered',
   confirmDeleteText: 'Are you sure that you want to unregister this cluster?',
   deletedAlertMessage: 'Cluster successfully unregistered',
+  configurationCopiedMessage: 'Copied',
   fields: {
     addKubernetesClusterButton: '$kubernetes-new-cluster-button',
-    addKubernetesClusterButtonInTable:
-      '//div[@data-qa=\'table-no-data\']//span[contains(text(), \'Register new Kubernetes Cluster\')]',
-    modalWindow: '$modal-body',
+    addKubernetesClusterButtonInTable: '//div[@data-qa=\'table-no-data\']//span[contains(text(), \'Register new Kubernetes Cluster\')]',
+    actionsLocator: (clusterName) => `//td[contains(text(), "${clusterName}")]//parent::tr//button[@data-qa="dropdown-menu-toggle"]`,
     closeButton: '$modal-close-button',
+    clusterConfigurationText: '//div[@data-qa=\'pmm-overlay-wrapper\']//pre',
+    copyToClipboardButton: '//span[contains(text(), \'Copy to clipboard\')]',
     disabledAddButton: '//button[@data-qa="kubernetes-add-cluster-button" and @disabled]',
-    kubernetesClusterNameInput: '$name-text-input',
+    forceUnreigsterCheckBox: '.checkbox-container__checkmark',
     kubeconfigFileInput: '$kubeConfig-textarea-input',
     kubernetesAddButton: '$kubernetes-add-cluster-button',
-    requiredField: '//div[contains(text(), \'Required field\')]',
+    kubernetesClusterNameInput: '$name-text-input',
+    modalWindow: '$modal-body',
+    modalCloseButton: '$modal-close-button',
+    modalContent: '$modal-content',
     proceedButton: '$delete-kubernetes-button',
+    requiredField: '//div[contains(text(), \'Required field\')]',
     unregisterButton: '//div[@data-qa=\'dropdown-menu-menu\']//span[1]',
     viewClusterConfiguration: '//div[@data-qa=\'dropdown-menu-menu\']//span[2]',
-    forceUnreigsterCheckBox: '.checkbox-container__checkmark',
   },
 
   checkCluster(cluserName, deleted) {
@@ -45,11 +50,9 @@ module.exports = {
     I.click(this.fields.kubernetesAddButton);
   },
 
-  unregisterCluster(cluserName) {
-    const actionsLocator = `//td[contains(text(), '${cluserName}')]//parent::tr//button[@data-qa='dropdown-menu-toggle']`;
-
-    I.waitForVisible(actionsLocator, 30);
-    I.click(actionsLocator);
+  unregisterCluster(clusterName) {
+    I.waitForVisible(this.fields.actionsLocator(clusterName), 30);
+    I.click(this.fields.actionsLocator(clusterName));
     I.waitForElement(this.fields.unregisterButton, 30);
     I.click(this.fields.unregisterButton);
     I.waitForText(this.confirmDeleteText, 10);
--- pmm-app/tests/verifyDBaaS_test.js
+++ pmm-app/tests/verifyDBaaS_test.js
@@ -7,7 +7,7 @@ Before(async (I) => {
 });
 
 Scenario(
-  'PMM-T426 - Verify adding new Kubernetes cluster minikube, PMM-T428 - Verify adding new Kubernetes cluster with same name, PMM-T431 -Verify unregistering Kubernetes cluster @not-pr-pipeline @dbaas',
+  'PMM-T426 - Verify adding new Kubernetes cluster minikube, PMM-T428 - Verify adding new Kubernetes cluster with same name, PMM-T546, PMM-T431 -Verify unregistering Kubernetes cluster @not-pr-pipeline @dbaas',
   async (I, dbaasPage) => {
     const clusterName = 'Kubernetes_Testing_Cluster_Minikube';
 
@@ -28,7 +28,7 @@ Scenario(
     // PMM-T428 - starting here
     dbaasPage.registerKubernetesCluster(clusterName, process.env.kubeconfig_minikube);
     dbaasPage.seeErrorForAddedCluster(clusterName);
-    // PMM-T431 - starting here
+    // PMM-T431, PMM-T546 - starting here, unregister cluster using unregister option
     dbaasPage.unregisterCluster(clusterName);
     dbaasPage.checkCluster(clusterName, true);
   },
@@ -59,4 +59,40 @@ Scenario('PMM-T427 - Verify elements on PMM DBaaS page @not-pr-pipeline @dbaas',
   I.amOnPage(dbaasPage.url);
   I.waitForVisible(dbaasPage.fields.addKubernetesClusterButton, 30);
   I.waitForVisible(dbaasPage.fields.addKubernetesClusterButtonInTable, 30);
+});
+
+Scenario('PMM-T547 Verify user is able to view config of registered Kubernetes cluster on Kubernetes Cluster Page @not-pr-pipeline @dbaas', async (I, dbaasPage, dbaasAPI) => {
+  const clusterName = 'Kubernetes_Testing_Cluster';
+
+  await dbaasAPI.apiRegisterCluster(process.env.kubeconfig_minikube, clusterName);
+  I.amOnPage(dbaasPage.url);
+  dbaasPage.checkCluster(clusterName, false);
+  I.waitForElement(dbaasPage.fields.actionsLocator(clusterName), 30);
+  I.click(dbaasPage.fields.actionsLocator(clusterName));
+  I.waitForElement(dbaasPage.fields.viewClusterConfiguration, 30);
+  I.click(dbaasPage.fields.viewClusterConfiguration);
+  I.waitForElement(dbaasPage.fields.modalContent, 30);
+  I.seeElement(dbaasPage.fields.copyToClipboardButton);
+  I.click(dbaasPage.fields.copyToClipboardButton);
+  I.waitForText(dbaasPage.configurationCopiedMessage, 30);
+  const configuration = await I.grabTextFrom(dbaasPage.fields.clusterConfigurationText);
+
+  assert.ok(configuration === process.env.kubeconfig_minikube, `The configuration shown is not equal to the expected Cluster configuration, ${configuration}`);
+  await dbaasAPI.apiUnregisterCluster(clusterName);
+});
+
+Scenario('Verify user is able to add same cluster config with different Name @not-pr-pipeline @dbaas', async (I, dbaasPage, dbaasAPI) => {
+  const clusterName1 = 'Kubernetes_Testing_Cluster_1';
+  const clusterName2 = 'Kubernetes_Testing_Cluster_2';
+
+  await dbaasAPI.apiRegisterCluster(process.env.kubeconfig_minikube, clusterName1);
+  await dbaasAPI.apiRegisterCluster(process.env.kubeconfig_minikube, clusterName2);
+  I.amOnPage(dbaasPage.url);
+  dbaasPage.checkCluster(clusterName1, false);
+  dbaasPage.checkCluster(clusterName2, false);
+  await dbaasAPI.apiUnregisterCluster(clusterName1);
+  await dbaasAPI.apiUnregisterCluster(clusterName2);
+  I.refreshPage();
+  dbaasPage.checkCluster(clusterName2, true);
+  dbaasPage.checkCluster(clusterName1, true);
 });
