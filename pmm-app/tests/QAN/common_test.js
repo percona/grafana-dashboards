6f81dccc08a45001b1a97fc185c3d37f05728f0e
Puneet Kala <puneet.0191@gmail.com>
2021-02-12 16:52:50 +0530

    Revert "Nightly-job (#1130)" (#1139)
    This reverts commit 00b0bf7b5a55fa9200676f91f3656bc84b481523.

--- pmm-app/tests/QAN/common_test.js
+++ pmm-app/tests/QAN/common_test.js
@@ -15,8 +15,11 @@ Scenario(
     await qanPagination.verifyPagesAndCount(25);
     I.waitForVisible(qanFilters.elements.environmentLabel, 30);
     await qanOverview.verifyRowCount(27);
-    I.waitForVisible(qanFilters.fields.filterBy, 30);
     qanFilters.applyFilter('mysql');
+    I.waitForVisible(qanFilters.fields.filterBy, 30);
+    I.waitForVisible(qanOverview.fields.searchBy, 30);
+    I.fillField(qanOverview.fields.searchBy, 'insert');
+    I.pressKey('Enter');
     I.waitForVisible(qanOverview.elements.querySelector, 30);
     I.click(qanOverview.elements.querySelector);
     I.waitForVisible(qanOverview.getColumnLocator('Lock Time'), 30);
@@ -29,7 +32,10 @@ Scenario(
     const cellValue = qanDetails.getMetricsCellLocator('Query Time', 3);
 
     qanOverview.waitForOverviewLoaded();
-    qanFilters.applyFilter('ps-dev');
+    qanFilters.applyFilter('mysql');
+    I.waitForVisible(qanOverview.fields.searchBy, 30);
+    I.fillField(qanOverview.fields.searchBy, 'insert');
+    I.pressKey('Enter');
     I.waitForElement(qanOverview.elements.querySelector, 30);
     qanOverview.selectRow(1);
     I.waitForVisible(cellValue, 30);
