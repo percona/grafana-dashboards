0b181c60c3908c9a880f85d4a3d5522af60f9a6c
Puneet Kala <puneet.0191@gmail.com>
2021-03-01 12:43:52 +0530

    Change Default Time RANGE

--- pmm-app/tests/pages/dashboardPage.js
+++ pmm-app/tests/pages/dashboardPage.js
@@ -113,7 +113,7 @@ module.exports = {
     ],
   },
   processDetailsDashboard: {
-    url: 'graph/d/node-cpu-process/processes-details?from=now-30m&to=now',
+    url: 'graph/d/node-cpu-process/processes-details?from=now-45m&to=now',
   },
   nodeSummaryDashboard: {
     url: 'graph/d/node-instance-summary/node-summary?orgId=1&refresh=5m',
