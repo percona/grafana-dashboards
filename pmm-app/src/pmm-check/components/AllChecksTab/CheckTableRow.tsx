8484b2916a19b37d7c02494cc48326420b9a35c9
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-03-31 22:40:58 +0200

    PMM-7772 update CheckTableRow

--- pmm-app/src/pmm-check/components/AllChecksTab/CheckTableRow.tsx
+++ pmm-app/src/pmm-check/components/AllChecksTab/CheckTableRow.tsx
@@ -4,15 +4,18 @@ import { ButtonWithSpinner } from 'shared/components/Form';
 import { CheckService } from 'pmm-check/Check.service';
 import { Messages } from './AllChecksTab.messages';
 import { CheckTableRowProps } from './types';
+import { Interval } from 'pmm-check/types';
 import { ChangeCheckIntervalModal } from './ChangeCheckIntervalModal';
 import { getStyles } from './CheckTableRow.styles';
+
+const formatInterval = (interval: Interval): typeof Interval => Interval[interval];
 
 export const CheckTableRow: FC<CheckTableRowProps> = ({ check, onSuccess }) => {
   const styles = useStyles(getStyles)
   const [changeCheckPending, setChangeCheckPending] = useState(false);
   const [checkIntervalModalVisible, setCheckIntervalModalVisible] = useState(false);
   const {
-    name, summary, description, disabled,
+    name, summary, description, disabled, interval,
   } = check;
 
   const handleChangeCheckInterval = () => {
@@ -36,25 +39,26 @@ export const CheckTableRow: FC<CheckTableRowProps> = ({ check, onSuccess }) => {
 
   return (
     <>
-    <tr key={name}>
-      <td>{summary}</td>
-      <td>{description}</td>
-      <td>{disabled ? Messages.disabled : Messages.enabled}</td>
-      <td>
-        <div className={styles.actionsWrapper}>
-          <ButtonWithSpinner
-            variant={disabled ? 'primary' : 'destructive'}
-            size="sm"
-            isLoading={changeCheckPending}
-            onClick={changeCheck}
-          >
-            {disabled ? Messages.enable : Messages.disable}
-          </ButtonWithSpinner>
-          <IconButton title={Messages.changeIntervalButtonTitle} name="history" onClick={handleChangeCheckInterval} />
-        </div>
-      </td>
-    </tr>
-    <ChangeCheckIntervalModal isVisible={checkIntervalModalVisible} setVisible={setCheckIntervalModalVisible} checkName={name} />
+      <tr key={name}>
+        <td>{summary}</td>
+        <td>{description}</td>
+        <td>{disabled ? Messages.disabled : Messages.enabled}</td>
+        <td>{formatInterval(interval)}</td>
+        <td>
+          <div className={styles.actionsWrapper}>
+            <ButtonWithSpinner
+              variant={disabled ? 'primary' : 'destructive'}
+              size="sm"
+              isLoading={changeCheckPending}
+              onClick={changeCheck}
+            >
+              {disabled ? Messages.enable : Messages.disable}
+            </ButtonWithSpinner>
+            <IconButton title={Messages.changeIntervalButtonTitle} name="history" onClick={handleChangeCheckInterval} />
+          </div>
+        </td>
+      </tr>
+      <ChangeCheckIntervalModal isVisible={checkIntervalModalVisible} setVisible={setCheckIntervalModalVisible} checkName={name} />
     </>
   );
 };
