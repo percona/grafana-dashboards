94bc07a30eaf5e2b0702ba9b61f2810dcd565312
adivinho <vadim.yalovets@percona.com>
2021-06-15 11:54:29 +0300

    PMM-7784 Add function for parameters renaming

--- misc/convert-dash.py
+++ misc/convert-dash.py
@@ -7,7 +7,9 @@ import json
 datasources = [['${DS_METRICS}','Metrics'], 
                ['${DS_PTSUMMARY}','PTSummary'], 
                ['${DS_CLICKHOUSE}','ClickHouse'],
-               ['${DS_PROMETHEUS_ALERTMANAGER}','Prometheus AlertManager']]
+               ['${DS_PROMETHEUS_ALERTMANAGER}','Prometheus AlertManager'],
+               ['${DS_PROMETHEUS}','Metrics'],
+               ['Prometheus','Metrics']]
 
 
 def drop_some_internal_elements(dashboard):
@@ -72,7 +74,13 @@ def check_formulas(dashboard, currentVariableName, newVariableName):
                         if 'expr' in target:
                             expr = dashboard['panels'][panel_index]['targets'][target_index]['expr']
                             if (expr.find(currentVariableName) != -1):
-                                print ' >>>> %s' % expr 
+                                print ' >>>> %s' % expr
+                                prompt = ' Replace variable %s to %s (Y/N)? [N]: ' % (currentVariableName, newVariableName)
+                                user_input = raw_input(prompt).upper()
+                                if user_input:
+                                    if user_input == 'Y':
+                                        dashboard['panels'][panel_index]['targets'][target_index]['expr'] = expr.replace(currentVariableName, newVariableName)
+                                        print ' >>>> Done! %s\n' % dashboard['panels'][panel_index]['targets'][target_index]['expr']
 
                 if 'panels' in panel:
                         if (len(dashboard['panels'][panel_index]['panels']) > 0):
@@ -82,7 +90,13 @@ def check_formulas(dashboard, currentVariableName, newVariableName):
                                         if 'expr' in target:
                                             expr = dashboard['panels'][panel_index]['panels'][panelIn_index]['targets'][target_index]['expr']
                                             if (expr.find(currentVariableName) != -1):
-                                                print ' >>>> %s' % expr 
+                                                print ' >>>> %s' % expr
+                                                prompt = ' Replace variable %s to %s (Y/N)? [N]: ' % (currentVariableName, newVariableName)
+                                                user_input = raw_input(prompt).upper()
+                                                if user_input:
+                                                    if user_input == 'Y':
+                                                        dashboard['panels'][panel_index]['panels'][panelIn_index]['targets'][target_index]['expr'] = expr.replace(currentVariableName, newVariableName)
+                                                        print ' >>>> Done! %s\n' % dashboard['panels'][panel_index]['panels'][panelIn_index]['targets'][target_index]['expr']
 
         if 'templating' in element:
             for list_index, lists in enumerate(dashboard['templating']['list']):
@@ -91,11 +105,29 @@ def check_formulas(dashboard, currentVariableName, newVariableName):
                         if (expr.find(currentVariableName) != -1):
                             print ' >>>> %s' % expr 
                             prompt = ' Replace variable %s to %s (Y/N)? [N]: ' % (currentVariableName, newVariableName)
-                            user_input = raw_input(prompt)
+                            user_input = raw_input(prompt).upper()
                             if user_input:
                                 if user_input == 'Y':
                                     dashboard['templating']['list'][list_index]['query'] = expr.replace(currentVariableName, newVariableName)
-                                    print ' >>>> Done! %s' % dashboard['templating']['list'][list_index]['query'] 
+                                    print ' >>>> Done! %s\n' % dashboard['templating']['list'][list_index]['query'] 
+
+    return dashboard
+
+
+def fix_parameters(dashboard):
+    while 1:
+        prompt = '\nDoes it required to rename parameters in formulas (Y/N)? [N]: '
+        user_input = raw_input(prompt).upper()
+        if user_input:
+            if user_input == 'Y':
+                prompt = 'Please enter parameter name for renaming: '
+                currentParameterName = raw_input(prompt)
+                prompt = 'Please enter new name for parameter %s: ' % (currentParameterName)
+                newParameterName = raw_input(prompt)
+                print '\nCollecting formulas ...'
+                check_formulas(dashboard, currentParameterName, newParameterName)
+        else:
+            break
 
     return dashboard
 
@@ -109,7 +141,7 @@ def fix_variables(dashboard):
                         print ('\nVariable: %s' % (dashboard['templating']['list'][panel_index]['name']))
                         print ('Next expression is used for collecting variable: %s' % (dashboard['templating']['list'][panel_index]['query']))
                         prompt = 'Modify (Y/N)? [N]: ' 
-                        user_input = raw_input(prompt)
+                        user_input = raw_input(prompt).upper()
                         if user_input:
                             if user_input == 'Y':
                                 prompt = 'Please enter new name for variable %s: ' % (currentVariableName)
@@ -127,7 +159,7 @@ def main():
         dashboard = json.loads(dashboard_file.read())
 
     # registered cleanupers.
-    CLEANUPERS = [drop_some_internal_elements, fix_datasource, fix_variables]
+    CLEANUPERS = [drop_some_internal_elements, fix_datasource, fix_variables, fix_parameters]
 
     for func in CLEANUPERS:
         dashboard = func(dashboard)
