c89ea515caf9683af9df74956958d9d1ebcaf15d
Tiago Mota Santos <tiago.mota@percona.com>
2021-10-19 12:46:55 +0100

    PMM-8590 Change API

--- pmm-app/src/pmm-qan/panel/components/Details/Plan/Plan.tsx
+++ pmm-app/src/pmm-qan/panel/components/Details/Plan/Plan.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import sqlFormatter from 'sql-formatter';
 import { Icon, Tooltip, useStyles } from '@grafana/ui';
 import { Overlay } from 'shared/components/Elements/Overlay/Overlay';
 import { getStyles } from './Plan.styles';
@@ -18,7 +17,7 @@ export const Plan: FC = () => {
         {plan ? (
           <>
             <Highlight key={plan?.id || ''} language="sql">
-              {sqlFormatter.format(plan?.plan || '')}
+              {plan?.plan || ''}
             </Highlight>
             <div className={styles.tooltipWrapper}>
               <Tooltip placement="left" theme="info" content={`${Messages.planId} ${plan?.id}`}>
--- pmm-app/src/pmm-qan/panel/components/Details/Plan/Plan.types.ts
+++ pmm-app/src/pmm-qan/panel/components/Details/Plan/Plan.types.ts
@@ -1,24 +1,13 @@
-import { QueryDimension } from 'pmm-qan/panel/provider/provider.types';
-
 export interface QueryPlan {
   id: string;
   plan: string;
 }
 
 export interface QueryPlanRequest {
-  filter_by: string;
-  group_by: QueryDimension;
-  period_start_from: string;
-  period_start_to: string;
-  labels: any;
-  totals: boolean;
+  queryid?: string;
 }
 
 export interface QueryPlanResponse {
-  query_plans: QueryPlanAPI[];
-}
-
-export interface QueryPlanAPI {
   planid: string;
   query_plan: string;
 }
