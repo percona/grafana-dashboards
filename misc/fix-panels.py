a4ec800bbddde49d3193080058d629e51255064b
Lutz Reinhardt <lr1980@users.noreply.github.com>
2021-12-10 14:28:39 +0100

    fix print statement for python3

--- misc/fix-panels.py
+++ misc/fix-panels.py
@@ -12,22 +12,22 @@ GRAFANA_PLUGINS_DR = '/srv/grafana/plugins/'
 
 def rename_panels():
     for app in ['panels']:
-        print '  * Renaming %r' % (app,)
+        print('  * Renaming %r' % (app,))
         if os.path.exists(GRAFANA_PLUGINS_DR):
             panels_list = os.listdir(GRAFANA_PLUGINS_DR)
             for panel in panels_list:
                 panel_path = os.path.join(GRAFANA_PLUGINS_DR, panel, 'dist/plugin.json')
                 if os.path.exists(panel_path):
-                    print '   * %r -> ' % (panel,),
+                    print('   * %r -> ' % (panel,),)
                     with open(panel_path, 'r') as f:
                         panel_params = json.loads(f.read())
-                        print '%r' % (panel_params['id'],)
+                        print('%r' % (panel_params['id'],))
                         if (not(os.path.exists(os.path.join(GRAFANA_PLUGINS_DR, panel_params['id'])))):
-                            print '    * Renaming panel'
+                            print('    * Renaming panel')
                             shutil.copytree(os.path.join(GRAFANA_PLUGINS_DR, panel), os.path.join(GRAFANA_PLUGINS_DR, panel_params['id']))
                         if (panel != panel_params['id']):
-                            print '    * Cleaning up leftovers'
+                            print('    * Cleaning up leftovers')
                             shutil.rmtree(os.path.join(GRAFANA_PLUGINS_DR, panel))
 
-print "Grafana database directory: %s" % (GRAFANA_PLUGINS_DR,)
+print("Grafana database directory: %s" % (GRAFANA_PLUGINS_DR,))
 rename_panels()
