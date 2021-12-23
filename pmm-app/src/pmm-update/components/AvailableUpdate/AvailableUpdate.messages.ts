0c8f27974eb2111efe57c0bc1c2a96de5e19e3cb
Tiago Mota Santos <tiago.mota@percona.com>
2021-12-23 09:21:34 +0000

    PMM-9049 Update tooltip text

--- pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.messages.ts
+++ pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.messages.ts
@@ -2,7 +2,7 @@ export const Messages = {
   availableVersion: 'Available version',
   built: 'Built on',
   tooltip: `Releases may not be available on the build date as we run extensive tests 
-  depending on the nature of change in each release, you may notice a few days delay 
-  between build date and available date`,
+  (according to the change) in each release. There may be a few days delay between the 
+  build and the available dates`,
   whatsNew: "What's new", // eslint-disable-line
 };
