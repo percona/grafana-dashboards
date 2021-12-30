4a1deb706547ac294392e94b4b823cd25ddc7ade
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2021-12-30 10:39:10 +0000

    PMM-9049 Build date (#1294)
    * PMM-9049 Build date
    
    * PMM-9049 Use var for font weight
    
    * PMM-9049 Update tooltip text
    
    * PMM-9049 Clarify current and available version dates
    
    * PMM-9049 Version tooltips
    
    * PMM-9049 Fix tests and linting
    
    * PMM-9049 Remove unused var
    
    * PMM-9049 Fix tooltips
    
    * PMM-9049 Fix tooltip
    
    * Update pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.messages.ts
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update pmm-app/src/pmm-update/components/CurrentVersion/CurrentVersion.messages.ts
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    Co-authored-by: Denys Kondratenko <denisok@users.noreply.github.com>

--- pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.tsx
+++ pmm-app/src/pmm-update/components/AvailableUpdate/AvailableUpdate.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react';
-import { LinkButton } from '@grafana/ui';
+import {
+  useStyles, Icon, LinkButton, Tooltip,
+} from '@grafana/ui';
 
 import { AvailableUpdateProps } from 'pmm-update/types';
 import { useToggleOnAltClick } from 'pmm-update/hooks';
 import { Messages } from './AvailableUpdate.messages';
-import * as styles from './AvailableUpdate.styles';
+import { getStyles } from './AvailableUpdate.styles';
 
 export const AvailableUpdate: FC<AvailableUpdateProps> = ({ nextVersionDetails }) => {
+  const styles = useStyles(getStyles);
   const [showFullVersion, handleToggleShowFullVersion] = useToggleOnAltClick(false);
 
   const {
@@ -14,32 +17,42 @@ export const AvailableUpdate: FC<AvailableUpdateProps> = ({ nextVersionDetails }
   } = nextVersionDetails;
 
   return (
-    <section className={styles.availableUpdate} onClick={handleToggleShowFullVersion}>
+    <section
+      data-testid="update-latest-section"
+      className={styles.availableUpdate}
+      onClick={handleToggleShowFullVersion}
+    >
       <p>
         {Messages.availableVersion}
         :&nbsp;
-      </p>
-      <p className={styles.availableUpdateVersion}>
-        <span data-testid="update-latest-version">{showFullVersion ? nextFullVersion : nextVersion}</span>
-        {' '}
+        <span
+          data-testid="update-latest-version"
+          className={styles.latestVersion}
+        >
+          {showFullVersion ? nextFullVersion : nextVersion}
+        </span>
         <span data-testid="update-latest-release-date" className={styles.releaseDate}>
           (
           {nextVersionDate}
           )
+          <Tooltip
+            content={Messages.tooltip}
+            data-testid="update-published-date-info"
+          >
+            <Icon name="info-circle" className={styles.infoIcon} />
+          </Tooltip>
         </span>
         {newsLink && (
-          <>
-            {' '}
-            <LinkButton
-              className={styles.whatsNewLink}
-              rel="noreferrer"
-              href={newsLink}
-              target="_blank"
-              variant="link"
-            >
-              {Messages.whatsNew}
-            </LinkButton>
-          </>
+        <LinkButton
+          data-testid="update-news-link"
+          className={styles.whatsNewLink}
+          rel="noreferrer"
+          href={newsLink}
+          target="_blank"
+          variant="link"
+        >
+          {Messages.whatsNew}
+        </LinkButton>
         )}
       </p>
     </section>
