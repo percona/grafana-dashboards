1bc2fba79ea29850cee5e03349edad49a5c7f432
Alex Tymchuk <alexander.tymchuk@gmail.com>
2022-07-29 01:50:15 +0300

    PMM-10036 cleaner entrypoint

--- setup-page/entrypoint.sh
+++ setup-page/entrypoint.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-yum install wget -y
-wget https://rpm.nodesource.com/pub_16.x/el/7/x86_64/nodejs-16.14.2-1nodesource.x86_64.rpm
-yum clean all
-yum install -y nodejs-16.14.2-1nodesource.x86_64.rpm
+curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
+yum install -y nodejs
 node -v
-rm nodejs-16.14.2-1nodesource.x86_64.rpm
 
+test -d build && rm -rf build
 npm install --production && npm run build
 ls -la ./build
