b301c1c21e5a2c5f33fd4c6a8e0c2e0bc3b062ad
Fábio Silva <ffjs1993@gmail.com>
2022-09-09 12:18:04 +0100

    fix: replace tooltip lib (#1380)
    * fix: replace tooltip lib
    
    * deps: lock grafana version
    
    * deps: fix versions
    
    * fix: use Tippy on failed checks
    
    * fix: unit test
    
    * fix: QAN filters buttons
    
    * fix: plan tooltip
    
    * PMM-10452 Create default folder for alerts
    
    * Revert "PMM-10452 Create default folder for alerts"
    
    This reverts commit 257ce7890eb5cae8a34d9f1cce0bb0e9afc0b65d.
    
    * deps: update polystat panel
    
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>

--- pmm-app/src/shared/components/Elements/Charts/Sparkline/Sparkline.styles.ts
+++ pmm-app/src/shared/components/Elements/Charts/Sparkline/Sparkline.styles.ts
@@ -4,5 +4,19 @@ export const styles = {
   graphWrapper: css`
     background: transparent;
     borderBottom: 1px solid grey;
-`,
+  `,
+  tippy: css`
+    .tippy-content {
+      background-color: #3274d9;
+      padding: 3px;
+      border-radius: 3px;
+      font-size: 13px;
+      padding: 5px 10px;
+      opacity: 0.9
+    }
+
+    .tippy-arrow {
+      background-color: #3274d9;
+    }
+  `,
 };
