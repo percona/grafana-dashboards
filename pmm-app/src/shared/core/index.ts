7b4638a0a32c82c87f62b481514de80e908e4496
Fabio Silva <ffjs1993@gmail.com>
2022-10-27 15:59:33 +0100

    chore: remove @percona/platform-core

--- pmm-app/src/shared/core/index.ts
+++ pmm-app/src/shared/core/index.ts
@@ -1,3 +1,4 @@
 export * from './constants';
 export * from './types';
 export * from './Settings.service';
+export * from './logger';
---
+++ pmm-app/src/shared/core/test.utils.ts
@@ -0,0 +1,1 @@
+export const dataTestId = (selector: string) => `[data-testid="${selector}"]`;
