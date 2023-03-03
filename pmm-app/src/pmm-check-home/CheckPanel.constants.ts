ac1540e3f02e41483a2840e840a0c34dfd0825c6
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-03-03 14:26:06 +0100

    PMM-11345 fix wrong url (#1466)

--- pmm-app/src/pmm-check-home/CheckPanel.constants.ts
+++ pmm-app/src/pmm-check-home/CheckPanel.constants.ts
@@ -1,7 +1,7 @@
 import { CheckPanelOptions } from './types';
 
 export const PMM_SETTINGS_URL = '/graph/settings/advanced-settings';
-export const PMM_DATABASE_CHECKS_PANEL_URL = '/graph/pmm-database-checks';
+export const PMM_DATABASE_CHECKS_PANEL_URL = '/graph/advisors/insights';
 
 export const DEFAULTS: CheckPanelOptions = {
   title: 'Failed Checks',
