132aa99fd3ae160d9524d428290358307b327403
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-03-23 15:25:02 +0100

    PMM-10799 Missing comma.

--- pmm-app/src/pmm-qan/panel/components/Details/Table/TableContainer.hooks.ts
+++ pmm-app/src/pmm-qan/panel/components/Details/Table/TableContainer.hooks.ts
@@ -15,7 +15,7 @@ export const useTables = (example, explains, databaseType): any[] => {
         const parsedJSON = JSON.parse(jsonExplain.value);
 
         const tablesResult = [
-          get(parsedJSON, 'real_table_name')
+          get(parsedJSON, 'real_table_name'),
         ].filter(Boolean);
 
         setTables(tablesResult);
