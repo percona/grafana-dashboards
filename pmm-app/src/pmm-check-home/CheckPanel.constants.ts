e84e46229c065012f64d97177bd628c285f0a55e
Fábio Silva <ffjs1993@gmail.com>
2023-04-04 16:48:47 +0100

    PMM-11200 migrate pmm checks (#1481)
    * PMM-11200 remove pmm-check-panel
    
    * PMM-11200 rename to advisors
    
    * PMM-11200 remove --skipTest

--- pmm-app/src/pmm-check-home/CheckPanel.constants.ts
+++
@@ -1,8 +0,0 @@
-import { CheckPanelOptions } from './types';
-
-export const PMM_SETTINGS_URL = '/graph/settings/advanced-settings';
-export const PMM_DATABASE_CHECKS_PANEL_URL = '/graph/advisors/insights';
-
-export const DEFAULTS: CheckPanelOptions = {
-  title: 'Failed Checks',
-};
