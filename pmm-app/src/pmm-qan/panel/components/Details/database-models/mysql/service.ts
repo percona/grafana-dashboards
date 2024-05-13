f94a83f7f84c0ae22fa20696bbb21f531b42ea85
Yash Sartanpara <yash.sartanpara@3pillarglobal.com>
2024-05-13 14:51:23 +0530

    PMM-12710 fix lint issue

--- pmm-app/src/pmm-qan/panel/components/Details/database-models/mysql/service.ts
+++ pmm-app/src/pmm-qan/panel/components/Details/database-models/mysql/service.ts
@@ -3,5 +3,5 @@ import { apiRequestActions } from 'shared/components/helpers/api';
 export default {
   getMySQLActions(body) {
     return apiRequestActions.post<any, any>(':startServiceAction', body);
-  }
+  },
 };
