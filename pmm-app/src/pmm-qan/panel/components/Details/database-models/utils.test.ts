6e9e94b0ef9a475c86517072d91afb24cd9a6f0b
Ioana Maniga <ioana.maniga@3pillarglobal.com>
2025-05-15 15:40:51 +0300

    PMM-13963 fixed unit tests comments

--- pmm-app/src/pmm-qan/panel/components/Details/database-models/utils.test.ts
+++ pmm-app/src/pmm-qan/panel/components/Details/database-models/utils.test.ts
@@ -6,16 +6,19 @@ describe('stripPrefix', () => {
 
     expect(result).toBe('main-text');
   });
+
   it('should return original text if it does not start with prefix', () => {
     const result = stripPrefix('prefix/main-text', 'other-prefix/');
 
     expect(result).toBe('prefix/main-text');
   });
+
   it('should return original text if prefix is empty', () => {
     const result = stripPrefix('prefix/main-text', '');
 
     expect(result).toBe('prefix/main-text');
   });
+  
   it('should return empty string if both text and prefix are empty', () => {
     const result = stripPrefix('', '');
 
